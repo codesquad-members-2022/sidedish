9aaa17ecb330bf1c2f80ad8c86e6a9cf8aef77fa
sunullo <fpzk5656@gmail.com>
2022-04-18 12:08:30 +0900

    [feat] Back End Project initial

---
+++ be/src/test/java/kr/codesquad/sidedish/SidedishApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.codesquad.sidedish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidedishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
