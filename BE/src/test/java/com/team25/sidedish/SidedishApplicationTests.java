9b6a2203b1b982853649023575508e8b246d8f1b
Deok Yun Kim <59705184+dukcode@users.noreply.github.com>
2022-04-18 23:22:56 +0900

    build: 스프링 부트 초기 설정 (#4)
    - 의존성 Spring WEB, Validation, Lombok, Spring Data JDBC 추가
    - gitignore 설정

---
+++ BE/src/test/java/com/team25/sidedish/SidedishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team25.sidedish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidedishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
