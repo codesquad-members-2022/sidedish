b4d7a01bd57b1acdfb72185492963547cdea2a60
bibi <bibi6666667@kakao.com>
2022-04-18 17:02:09 +0900

    feat : Xcode 프로젝트 생성 및 불필요한 주석,메서드 삭제 #2

---
+++ sidedishApp/sidedishApp/SceneDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  SceneDelegate.swift
+//  sidedishApp
+//
+//  Created by Bibi on 2022/04/18.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
---
+++ sidedishApp/sidedishApp/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  sidedishApp
+//
+//  Created by Bibi on 2022/04/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
