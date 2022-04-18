598a5cf9d23a508c1d1dbf4415939b9c67f7ec5f
ByeoungJoon Jeon <juni8453@naver.com>
2022-04-18 17:46:41 +0900

    feat : Spring Boot 셋팅 및 gitignore 수정
    프로젝트를 위한 Spring Boot 를 셋팅하고, gitignore 파일을 수정했습니다.

---
+++ BE/BE-sidedish/src/main/java/team14/sidedish/SidedishApplication.java
@@ -0,0 +1,13 @@
+package team14.sidedish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidedishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidedishApplication.class, args);
+	}
+
+}
