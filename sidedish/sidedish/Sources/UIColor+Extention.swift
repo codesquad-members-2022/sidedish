4161f9681203b58029805ccc775fe499083bf338
Jihee hwang <hwangjihee94@gmail.com>
2022-04-18 17:43:13 +0900

    [feat] Extension UIColor

---
+++ sidedish/sidedish/Sources/UIColor+Extention.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+Extention.swift
+//  sidedish
+//
+//  Created by Jihee hwang on 2022/04/18.
+//
+
+import UIKit
+
+extension UIColor {
+    static let primaryDark = UIColor(red: 0, green: 102/255, blue: 214/255, alpha: 1)
+    static let primary = UIColor(red: 0, green: 122/255, blue: 1, alpha: 1)
+    static let primaryLight = UIColor(red: 128/255, green: 188/255, blue: 1, alpha: 1)
+    static let grey1 = UIColor(white: 79/255, alpha: 1)
+    static let grey2 = UIColor(white: 130/255, alpha: 1)
+    static let grey3 = UIColor(white: 224/255, alpha: 1)
+    static let grey4 = UIColor(white: 245/255, alpha: 1)
+}
