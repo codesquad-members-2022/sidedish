90ac27ea6841284bf733af4048e14a31fa205745
sanghee <anshee97@naver.com>
2022-04-18 11:51:41 +0900

    [CHORE] Add Project

---
+++ SideDish20/SideDish20/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideDish20
+//
+//  Created by 안상희 on 2022/04/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
