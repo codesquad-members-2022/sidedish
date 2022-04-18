bcdc8a7fab5782c4c465304244c945e3e84def7d
shingha <shingha1124@gmail.com>
2022-04-18 17:15:12 +0900

    [feature] Food 모델 정의

--- sidedish/sidedish/Sources/Present/Main/MainViewController.swift
+++ sidedish/sidedish/Sources/Present/Main/MainViewController.swift
@@ -11,7 +11,7 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        title = "asfdsafas"
         view.backgroundColor = .red
     }
 }
