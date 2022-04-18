b1d72354e09a2e05801d7ee82e33c9e9b70cedb8
KTH-96 <taehyun960@gmail.com>
2022-04-18 12:04:25 +0900

    init project

---
+++ be/src/main/java/sidedish/be/BeApplication.java
@@ -0,0 +1,13 @@
+package sidedish.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeApplication.class, args);
+	}
+
+}
