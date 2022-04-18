7791b614ac54f360f2d1c6591c38a1236b9b108e
hsjang <hsjang0405@gmail.com>
2022-04-18 17:27:47 +0900

    [BE] feat: initial project setting

---
+++ BE/src/main/java/sidedish/jbc/JbcApplication.java
@@ -0,0 +1,13 @@
+package sidedish.jbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbcApplication.class, args);
+	}
+
+}
