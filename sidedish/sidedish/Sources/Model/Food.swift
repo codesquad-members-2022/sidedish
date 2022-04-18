ccdb80e63d96ce53adb477e812cace808f376d83
shingha <shingha1124@gmail.com>
2022-04-18 17:15:12 +0900

    [feature] Food 모델 정의

---
+++ sidedish/sidedish/Sources/Model/Food.swift
@@ -0,0 +1,27 @@
+//
+//  Food.swift
+//  sidedish
+//
+//  Created by seongha shin on 2022/04/18.
+//
+
+import Foundation
+
+struct Food {
+    let hash: String
+    let image: URL
+    let title: String
+    let deliveryType: [String]
+    let description: String
+    let price: String
+    let salePrice: String
+    let badge: [String]
+    
+    enum CodingKeys: String, CodingKey {
+        case image, title, description, badge
+        case deliveryType = "delivery_type"
+        case hash = "detail_hash"
+        case price = "n_price"
+        case salePrice = "s_price"
+    }
+}
