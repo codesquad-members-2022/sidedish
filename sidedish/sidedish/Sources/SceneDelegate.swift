c1d86b010a9905b3154282958144df350e96a8ba
shingha <shingha1124@gmail.com>
2022-04-18 16:28:44 +0900

    폴더 구조 정리

---
+++ sidedish/sidedish/Sources/SceneDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  SceneDelegate.swift
+//  sidedish
+//
+//  Created by seongha shin on 2022/04/18.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
