5e6e58412b2b40306ef597eb344f6ad73abd7457
BumgeunSong <isp1195@gmail.com>
2022-04-18 17:59:19 +0900

    [Add] Update Project file

--- SideDishApp/SideDishApp/ViewController.swift
+++ SideDishApp/SideDishApp/ViewController.swift
@@ -6,14 +6,14 @@
 //
 
 import UIKit
+import Toast_Swift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
-
 
 }
 
