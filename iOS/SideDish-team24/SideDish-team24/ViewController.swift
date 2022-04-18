aa581977107da4366443cfdb804184db45658721
sungju-kim <wnwn0719@gmail.com>
2022-04-18 11:55:51 +0900

    Chore - 주석제거

--- iOS/SideDish-team24/SideDish-team24/ViewController.swift
+++ iOS/SideDish-team24/SideDish-team24/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  SideDish-team24
-//
-//  Created by dale on 2022/04/18.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
