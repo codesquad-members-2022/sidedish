d9ac0242b7b292ac9220538ad32c0664f0313827
shingha <shingha1124@gmail.com>
2022-04-18 16:58:50 +0900

    메인 뷰 출력

--- sidedish/sidedish/Sources/Present/Main/MainViewController.swift
+++ sidedish/sidedish/Sources/Present/Main/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  sidedish
 //
 //  Created by seongha shin on 2022/04/18.
@@ -7,10 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
     }
 }
