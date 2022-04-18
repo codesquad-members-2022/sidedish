5596e7e99918a2c4ef9f4e4acd53b3e14e741d28
sungju-kim <wnwn0719@gmail.com>
2022-04-18 11:45:39 +0900

    Docs: 프로젝트 생성

---
+++ iOS/SideDish-team24/SideDish-team24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideDish-team24
+//
+//  Created by dale on 2022/04/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
