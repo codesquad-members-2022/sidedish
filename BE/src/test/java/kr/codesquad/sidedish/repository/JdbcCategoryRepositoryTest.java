27a84b32b9d978d2f262b257146de7ad21f6c5a1
Minseok-Choi <cms05041@gmail.com>
2022-04-19 11:00:41 +0900

    Feat: Category 도메인, JdbcCategoryRepository 구현, 테스트

---
+++ BE/src/test/java/kr/codesquad/sidedish/repository/JdbcCategoryRepositoryTest.java
@@ -0,0 +1,34 @@
+package kr.codesquad.sidedish.repository;
+
+import kr.codesquad.sidedish.domain.Category;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJdbcTest
+class JdbcCategoryRepositoryTest {
+
+    @Autowired
+    private JdbcCategoryRepository jdbcCategoryRepository;
+
+
+    private Category category;
+
+    @BeforeEach
+    void setUp() {
+        category = new Category(null, "짱민", "짱");
+    }
+    @Test
+    void test() {
+
+        Category saved = jdbcCategoryRepository.save(category);
+        assertThat(saved.getId()).isEqualTo(1L);
+        assertThat(saved.getName()).isEqualTo("짱민");
+        assertThat(saved.getDescription()).isEqualTo("짱");
+
+    }
+
+}
