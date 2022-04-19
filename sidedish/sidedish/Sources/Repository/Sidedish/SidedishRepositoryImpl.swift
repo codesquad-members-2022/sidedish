40fdbce705d0a2c04c301bf400ec366f9882c765
shingha <shingha1124@gmail.com>
2022-04-19 14:09:21 +0900

    [refactor] API모델 이름 변경

--- sidedish/sidedish/Sources/Repository/Sidedish/SidedishRepositoryImpl.swift
+++ sidedish/sidedish/Sources/Repository/Sidedish/SidedishRepositoryImpl.swift
@@ -11,8 +11,11 @@ import Foundation
 class SidedishRepositoryImpl: NetworkRepository<SidedishTarget>, SidedishRepository {
     func loadMain() -> AnyPublisher<ApiResult<[Sidedish], SessionError>, Never> {
         request(.loadMain)
-            .map { $0.decode(SidedishResult.self) }
+            .map { $0.decode(SidedishAPIResult.self) }
             .map { result -> ApiResult<[Sidedish], SessionError> in
+                if let error = result.error {
+                    return ApiResult(value: nil, error: error)
+                }
                 if let result = result.value,
                    (200..<300).contains(result.statusCode) {
                     return ApiResult(value: result.body, error: nil)
