4cbc792cd0d6bc1a9e164d1198d4aebe0a3c10fa
sju02048 <sju02048@naver.com>
2022-04-19 17:47:40 +0900

    Chore: 초기개발환경
    Chore: CRA 초기구성
    
    Chore: eslint 구성
    
    Chore: prettier 구성

---
+++ FE/src/App.js
@@ -0,0 +1,22 @@
+function App() {
+	return (
+		<>
+			<div className="App">
+				<header></header>
+				<main>
+					<div className="Best">
+						<header className="Best__header"></header>
+					</div>
+					<div className="CategoryContainer">
+						<div className="Category"></div>
+						<div className="Category"></div>
+						<div className="Category"></div>
+					</div>
+				</main>
+			</div>
+			<div className="Modal"></div>
+		</>
+	);
+}
+
+export default App;
