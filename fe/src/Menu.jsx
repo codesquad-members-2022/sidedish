d2b7afd055a6182607058a1bc607d650baf5187e
Cola <bcad1591@gmail.com>
2022-04-19 16:31:49 +0900

    feat: Menu 컴포넌트 추가

---
+++ fe/src/Menu.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const StyledMenu = styled.li`
+  margin-right: 24px;
+`;
+
+const SubMenuList = styled.ul`
+  margin-top: 16px;
+
+  li:not(:last-child) {
+    margin-bottom: 8px;
+  }
+`;
+
+export const Menu = ({ menuData: { mainMenu, subMenuList }, isMouseEnter }) => {
+  return (
+    <StyledMenu>
+      <span>{mainMenu}</span>
+      {isMouseEnter ? (
+        <SubMenuList>
+          {subMenuList.map((subMenu, idx) => (
+            <li key={idx}>{subMenu}</li>
+          ))}
+        </SubMenuList>
+      ) : null}
+    </StyledMenu>
+  );
+};
