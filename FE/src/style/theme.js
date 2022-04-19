4e09998f3e4a2853839565c1e0e7352ff03c439f
Millie <jay.lee.2280@gmail.com>
2022-04-19 17:47:35 +0900

    Design: 공통 스타일 속성 정의 - #9
    font-size, font-weight, font-family, colors 정의

---
+++ FE/src/style/theme.js
@@ -0,0 +1,38 @@
+const fontSize = {
+  xLarge: '40px',
+  large: '20px',
+  medium: '16px',
+  small: '14px',
+  xSmall: '12px',
+};
+
+const fontWeight = {
+  logo: 900,
+  display: 700,
+  bold: 500,
+  regular: 400,
+};
+
+const fontFamily = {
+  default: 'Noto Sans KR',
+  logo: 'Outfit',
+};
+
+const colors = {
+  black: '#1B1B1B',
+  grey1: '#3F3F3F',
+  grey2: '#777777',
+  grey3: '#BCBCBC',
+  grey4: '#EBEBEB',
+  offWhite: '#F8F7F7',
+  white: '#FFFFFF',
+  green: '#6DD028',
+  orange: '#FF8E14',
+};
+
+export const theme = {
+  fontSize,
+  fontWeight,
+  fontFamily,
+  colors,
+};
