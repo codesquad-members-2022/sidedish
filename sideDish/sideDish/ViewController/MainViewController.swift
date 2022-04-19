1b4c17a107b8993bb8303610ce5fbf7bf3f99e6f
Jason <ggj00125@gmail.com>
2022-04-19 17:02:49 +0900

    ADD: dumyImage tempFood 파일추가

--- sideDish/sideDish/ViewController/MainViewController.swift
+++ sideDish/sideDish/ViewController/MainViewController.swift
@@ -10,9 +10,45 @@ import Toaster
 
 class MainViewController: UIViewController {
     
+    private var collectionView: UICollectionView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 1
+        layout.minimumInteritemSpacing = 1
+        layout.itemSize = CGSize(width: self.view.frame.width, height: (view.frame.size.height/4)-3)
+        
+        
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        guard let collectionView = collectionView else {
+            return
+        }
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(FoodCell.self, forCellWithReuseIdentifier: FoodCell.identifier)
+        
+        view.addSubview(collectionView)
+        collectionView.frame = view.bounds
+        
     }
-
+    
 }
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FoodCell.identifier, for: indexPath) as! FoodCell
+        
+        cell.configure(firLabel: "런팅특가", secLabel: "12,640 15,800", thiLabel: "감칠맛나는양념", fouLabel: "오리주물럭")
+        return cell
+        
+    }
+    
+}
+
