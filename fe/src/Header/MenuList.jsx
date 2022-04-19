78f59ee12f47b4513d3ab048390496b3d82a920b
Cola <bcad1591@gmail.com>
2022-04-20 03:51:38 +0900

    refactor: StyledMenuList -> Root로 이름 변경

--- fe/src/Header/MenuList.jsx
+++ fe/src/Header/MenuList.jsx
@@ -2,13 +2,13 @@ import { Menu } from './Menu';
 import { useState } from 'react';
 import styled from 'styled-components';
 
-const StyledMenuList = styled.ul`
+const Root = styled.ul`
   display: flex;
   cursor: pointer;
 `;
 
 export const MenuList = () => {
-  const [menuDatas, setMenuDatas] = useState([
+  const [menuDatas] = useState([
     { mainMenu: '든든한 메인요리', subMenuList: ['육류 요리', '해산물 요리'] },
     { mainMenu: '뜨끈한 국물요리', subMenuList: ['국/탕/찌개'] },
     {
@@ -28,13 +28,13 @@ export const MenuList = () => {
   };
 
   return (
-    <StyledMenuList
+    <Root
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
       {menuDatas.map((menuData, idx) => (
         <Menu key={idx} menuData={menuData} isMouseEnter={isMouseEnter} />
       ))}
-    </StyledMenuList>
+    </Root>
   );
 };
