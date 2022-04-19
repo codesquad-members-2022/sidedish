2a0ac475ae90c5179eab8555b551abfc7c2737ab
jm <jm.dev.study@gmail.com>
2022-04-19 23:27:33 +0900

    Feat: Main의 기획전과 관련된 UI 구현 - 기획전의 탭 클릭시 해당하는 Item이 보이도록 구현

---
+++ fe/src/MainTheme/ThemeTab.jsx
@@ -0,0 +1,183 @@
+import { useState } from 'react';
+import styled, { css } from 'styled-components';
+import TabList from './ThemeTabList';
+
+const TabBar = styled.ul`
+  display: flex;
+  list-style: none;
+  margin: 0;
+  padding: 0 0 0 80px;
+`;
+
+const TabBarItem = styled.li`
+  margin-right: 32px;
+  font-size: 20px;
+  line-height: 30px;
+  text-align: center;
+  cursor: pointer;
+
+  ${(props) =>
+    props.focus === props.id &&
+    css`
+      border-bottom: 2px solid #1b1b1b;
+    `}
+`;
+
+const items = [
+  {
+    id: 1,
+    title: '풍성한 고기 반찬',
+    data: [
+      {
+        id: 1,
+        img: 'https://random.imagecdn.app/412/412',
+        title: '오리 주물럭_반조리',
+        desc: '감칠맛 나는 매콤한 양념',
+        salePrice: 12640,
+        normalPrice: 15800,
+        tag: '런칭특가',
+      },
+      {
+        id: 2,
+        img: 'https://random.imagecdn.app/412/412',
+        title: '돼지 주물럭_반조리',
+        desc: '감칠맛 나는 매콤한 양념',
+        salePrice: 12640,
+        normalPrice: 15800,
+        tag: '이벤트특가',
+      },
+      {
+        id: 3,
+        img: 'https://random.imagecdn.app/412/412',
+        title: '달콤 오리 주물럭_반조리',
+        desc: '감칠맛 나는 달콤한 양념',
+        salePrice: 12640,
+        normalPrice: 15800,
+        tag: '런칭특가',
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: '편리한 반찬 세트',
+    data: [
+      {
+        id: 1,
+        img: 'https://random.imagecdn.app/412/412',
+        title: '간장 소갈비찜',
+        desc: '촉촉하게 벤 양념이 일품',
+        salePrice: 26010,
+        normalPrice: 28900,
+        tag: '이벤트특가',
+      },
+      {
+        id: 2,
+        img: 'https://random.imagecdn.app/412/412',
+        title: '매운 소갈비찜',
+        desc: '촉촉하게 벤 양념이 일품',
+        salePrice: 26010,
+        normalPrice: 28900,
+        tag: '런칭특가',
+      },
+      {
+        id: 3,
+        img: 'https://random.imagecdn.app/412/412',
+        title: '마라 소갈비찜',
+        desc: '촉촉하게 벤 양념이 일품',
+        salePrice: 26010,
+        normalPrice: 28900,
+        tag: '이벤트특가',
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: '맛있는 제철 요리',
+    data: [
+      {
+        id: 1,
+        img: 'https://random.imagecdn.app/412/412',
+        title: '간장 쭈꾸미',
+        desc: '쫄낏한 쭈구미',
+        // salePrice: 16900,
+        normalPrice: 16900,
+        tag: '이벤트특가',
+      },
+      {
+        id: 2,
+        img: 'https://random.imagecdn.app/412/412',
+        title: '매콤 쭈꾸미',
+        desc: '쫄낏한 쭈구미',
+        // salePrice: 16900,
+        normalPrice: 16900,
+        tag: '이벤트특가',
+      },
+      {
+        id: 3,
+        img: 'https://random.imagecdn.app/412/412',
+        title: '쭈꾸미',
+        desc: '쫄낏한 쭈구미',
+        // salePrice: 16900,
+        normalPrice: 16900,
+        tag: '이벤트특가',
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: '우리 아이 영양 반찬',
+    data: [
+      {
+        id: 1,
+        img: 'https://random.imagecdn.app/412/412',
+        title: '새콤달콤 오징어무침',
+        desc: '국내산 오징어를 새콤달콤하게',
+        salePrice: 6000,
+        normalPrice: 7500,
+        tag: '런칭특가',
+      },
+      {
+        id: 2,
+        img: 'https://random.imagecdn.app/412/412',
+        title: '새콤달콤 오징어무침',
+        desc: '국내산 오징어를 새콤달콤하게',
+        salePrice: 6000,
+        normalPrice: 7500,
+        tag: '런칭특가',
+      },
+      {
+        id: 3,
+        img: 'https://random.imagecdn.app/412/412',
+        title: '새콤달콤 오징어무침',
+        desc: '국내산 오징어를 새콤달콤하게',
+        salePrice: 6000,
+        normalPrice: 7500,
+        tag: '런칭특가',
+      },
+    ],
+  },
+];
+
+const ThemeTab = () => {
+  const [focus, setFocus] = useState(1);
+  const onClick = (e) => {
+    const id = e.currentTarget.id;
+    setFocus(Number(id));
+  };
+  const tabBarList = items.map((item) => {
+    return (
+      <TabBarItem key={item.id} id={item.id} onClick={onClick} focus={focus}>
+        <p>{item.title}</p>
+      </TabBarItem>
+    );
+  });
+
+  return (
+    <>
+      <TabBar>{tabBarList}</TabBar>
+      <TabList items={items[focus - 1]}></TabList>
+    </>
+  );
+};
+
+export default ThemeTab;
