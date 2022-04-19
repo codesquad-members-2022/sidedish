6b42f50f8de6f168d36bd74f622a31f86f049d25
Millie <jay.lee.2280@gmail.com>
2022-04-19 17:51:10 +0900

    Design: GlobalStyle에 reset css 작성 - #9
    index.js에 ThemeProvider, GlobalStyle 적용

--- FE/src/index.js
+++ FE/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import GlobalStyle from './style/global.js';
+import { theme } from './style/theme.js';
+import { ThemeProvider } from 'styled-components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ FE/src/style/global.js
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+h1 {
+  margin: 0;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+button,
+input,
+select,
+textarea {
+  background-color: transparent;
+  border: 0;
+
+  &:focus {
+    outline: none;
+    box-shadow: none;
+  }
+}
+
+a,
+button {
+  cursor: pointer;
+}
+
+ul,
+ol,
+li,
+menu {
+  padding-left: 0;
+  list-style: none;
+}
+`;
+
+export default GlobalStyle;
