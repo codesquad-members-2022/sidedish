53343f5e0282feb44b594f450981c2920b88af14
who-hoo <who.ho3ov@gmail.com>
2022-04-19 16:32:35 +0900

    Feat: 스프링부트 프로젝트 생성

---
+++ BE/src/test/java/com/team34/sidedish/SidedishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team34.sidedish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidedishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
