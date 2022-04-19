d5872bf378497f347bcc4eaee843fa5c6fdb15fc
seyeong <seyoung7555@naver.com>
2022-04-19 16:00:04 +0900

    feat: ViewService 계층 생성 및 기능 명세 (#5)

--- backend/src/main/java/com/codesquad/sidedish/item/controller/ViewController.java
+++ backend/src/main/java/com/codesquad/sidedish/item/controller/ViewController.java
@@ -2,20 +2,26 @@ package com.codesquad.sidedish.item.controller;
 
 import com.codesquad.sidedish.item.dto.CategoryItemsDto;
 import com.codesquad.sidedish.item.dto.DetailItemDto;
+import com.codesquad.sidedish.item.service.ViewService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ViewController {
+    private final ViewService viewService;
+
+    public ViewController(ViewService viewService) {
+        this.viewService = viewService;
+    }
 
     @GetMapping("/items")
     public CategoryItemsDto list() {
-        return null;
+        return viewService.findAll();
     }
 
     @GetMapping("/items/{itemId}")
     public DetailItemDto detail(@PathVariable Integer itemId) {
-        return null;
+        return viewService.findById(itemId);
     }
 }
---
+++ backend/src/main/java/com/codesquad/sidedish/item/service/ViewService.java
@@ -0,0 +1,17 @@
+package com.codesquad.sidedish.item.service;
+
+import com.codesquad.sidedish.item.dto.CategoryItemsDto;
+import com.codesquad.sidedish.item.dto.DetailItemDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ViewService {
+
+    public CategoryItemsDto findAll() {
+        return null;
+    }
+
+    public DetailItemDto findById(Integer itemId) {
+        return null;
+    }
+}
