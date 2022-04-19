4fd6a104c9cbc1d7107351c88ea7dc6ae2b9f807
Damagucci-Juice <jsr9874@gmail.com>
2022-04-19 22:04:40 +0900

    [#3] feat: VC에 CollectionView 추가 및 TopBar 문구 설정

---
+++ SideDish/SideDish/HomeViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var dishCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
