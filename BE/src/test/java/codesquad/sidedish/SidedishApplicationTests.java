b5c7cead19a5f36ceefe2a03e1a5bfa344f6dae5
Senglee Choi <tmdfl1027@gmail.com>
2022-04-19 11:56:13 +0900

    [#13] Chore: 프로젝트 초기 설정
    프로젝트 생성 후 EC2 인스턴스의 MySQL과 연동.

---
+++ BE/src/test/java/codesquad/sidedish/SidedishApplicationTests.java
@@ -0,0 +1,13 @@
+package codesquad.sidedish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidedishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
