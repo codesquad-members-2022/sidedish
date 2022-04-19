e1a072cf9fabd7232b78d2e9e3a3f195563c67c8
suuzin <8oozin@gmail.com>
2022-04-19 11:18:37 +0900

    fix : 깃오류

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,14 +1,9 @@
 import "./App.css";
-import Card from "./component/Card";
 
 function App() {
   return (
     <div className="App">
-      <div className="card-wrapper">
-        <Card />
-        <Card />
-        <Card />
-      </div>
+      <p> start </p>
     </div>
   );
 }
