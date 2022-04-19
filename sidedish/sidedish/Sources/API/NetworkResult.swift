cf89a6c122baf5437e8cc9c0ba1484a61cbc806e
shingha <shingha1124@gmail.com>
2022-04-19 12:28:03 +0900

    [feat] 메인메뉴 서버 데이터 파싱

--- sidedish/sidedish/Sources/API/NetworkResult.swift
+++ sidedish/sidedish/Sources/API/NetworkResult.swift
@@ -35,10 +35,22 @@ extension NetworkResult {
         }
         
         guard let data = data,
-              let decodableData = try? JSONDecoder().decode(T.self, from: data) else {
+              let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+              let statusCode = json["statusCode"] as? Int else {
             return ApiResult(value: nil, error: .pasingError)
         }
-        return ApiResult(value: decodableData, error: nil)
+    
+        if !(200..<300).contains(statusCode) {
+            return ApiResult(value: nil, error: .statusCodeError)
+        }
+        
+        guard let body = json["body"],
+              let bodyData = try? JSONSerialization.data(withJSONObject: body, options: .init()),
+              let dto = try? JSONDecoder().decode(T.self, from: bodyData) else {
+            return ApiResult(value: nil, error: .pasingError)
+        }
+        
+        return ApiResult(value: dto, error: nil)
     }
     
     func mapVoid() -> ApiResult<Void, SessionError> {
--- sidedish/sidedish/Sources/Model/Sidedish.swift
+++ sidedish/sidedish/Sources/Model/Sidedish.swift
@@ -14,14 +14,14 @@ struct Sidedish: Decodable {
     let deliveryType: [String]
     let description: String
     let price: String
-    let salePrice: String
-    let badge: [String]
+    let salePrice: String?
+    let badge: [String]?
     
     enum CodingKeys: String, CodingKey {
         case image, title, description, badge
         case deliveryType = "delivery_type"
         case hash = "detail_hash"
-        case price = "n_price"
-        case salePrice = "s_price"
+        case price = "s_price"
+        case salePrice = "n_price"
     }
 }
