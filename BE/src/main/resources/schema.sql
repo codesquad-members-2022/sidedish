996219cfaa42af62a44bb67da6d0b24e66638a74
leezzangmin <ckdals12345678@gmail.com>
2022-04-19 15:05:24 +0900

    Refactor: 스키마 수정

--- BE/src/main/resources/schema.sql
+++ BE/src/main/resources/schema.sql
@@ -1,35 +1,57 @@
-drop table if exists CATEGORY;
-CREATE TABLE CATEGORY(
-     id          BIGINT PRIMARY KEY AUTO_INCREMENT,
-     name        VARCHAR(30) not null,
-     description VARCHAR(500)
 
+CREATE TABLE MAIN_CATEGORY(
+      id          BIGINT PRIMARY KEY AUTO_INCREMENT,
+      name        VARCHAR(30) not null,
+      description VARCHAR(500)
 );
 
 CREATE TABLE SUB_CATEGORY(
-    id      BIGINT PRIMARY KEY AUTO_INCREMENT,
-
+     id          BIGINT PRIMARY KEY AUTO_INCREMENT,
+     name        VARCHAR(30) not null,
+     description VARCHAR(500),
+     main_category_id BIGINT,
+     FOREIGN KEY (main_category_id) references MAIN_CATEGORY(id)
 );
 
-
-drop table if exists DISH;
 CREATE TABLE DISH(
-     id              BIGINT PRIMARY KEY AUTO_INCREMENT,
-     category_id     BIGINT,
-     name            VARCHAR(30) not null,
-     stock           INT not null,
-     price           DOUBLE not null,
-     point_percent   DOUBLE NOT NULL,
-     delivery_type   VARCHAR(10) not null,
-     discount_policy VARCHAR(10) not null,
-     is_deleted      BOOLEAN default false, -- 도메인에 넣을지 말지?
-     foreign key (category_id) references CATEGORY(id)
+     id               BIGINT PRIMARY KEY AUTO_INCREMENT,
+     main_category_id BIGINT,
+     sub_category_id  BIGINT,
+     name             VARCHAR(30) not null,
+     description      varchar(100) not null,
+     stock            INT not null,
+     price            DOUBLE not null,
+     point_percent    DOUBLE NOT NULL,
+     delivery_type    VARCHAR(10) not null,
+     discount_policy  VARCHAR(10) not null,
+     is_deleted       BOOLEAN default false, -- 도메인에 넣을지 말지?
+     foreign key (main_category_id) references MAIN_CATEGORY(id),
+     foreign key (sub_category_id) references SUB_CATEGORY(id)
 );
 
-drop table if exists IMAGE;
 CREATE TABLE IMAGE(
       id      BIGINT PRIMARY KEY AUTO_INCREMENT,
       name    VARCHAR(50) not null,
       dish_id BIGINT,
       foreign key (dish_id) references DISH(id)
+);
+
+CREATE TABLE USER(
+    id BIGINT PRIMARY KEY,
+    name varchar(20) NOT NULL,
+    address varchar(100) NOT NULL
+);
+
+-- 장바구니 안됨
+CREATE TABLE ORDER(
+    id BIGINT PRIMARY KEY,
+    user_id BIGINT,
+    dish_id BIGINT,
+    shipping_address varchar(100) NOT NULL,
+    quantity BIGINT NOT NULL,
+    delivery_fee BIGINT NOT NULL,
+    total_price BIGINT NOT NULL,
+    order_datetime datetime NOT NULL,
+    foreign key (user_id) references USER(id),
+    foreign key (dish_id) references DISH(id)
 );
