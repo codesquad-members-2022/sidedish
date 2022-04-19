8f0fc8479439a7cf75cfa113d89db58b13ebcf6a
godhyeongman <88314186+godhyeongman@users.noreply.github.com>
2022-04-19 17:33:15 +0900

    design: 폰트 스타일 변경
    서브 카테고리 폰트 사이즈 변경
    서브 카테고리 호버시  하이라이트

--- fe/src/Menu.jsx
+++ fe/src/Menu.jsx
@@ -7,8 +7,17 @@ const StyledMenu = styled.li`
 const SubMenuList = styled.ul`
   margin-top: 16px;
 
+  li {
+    font-size: 14px;
+  }
+
   li:not(:last-child) {
     margin-bottom: 8px;
+  }
+
+  li:hover {
+    text-decoration: underline;
+    color: ${'#777777'};
   }
 `;
 
