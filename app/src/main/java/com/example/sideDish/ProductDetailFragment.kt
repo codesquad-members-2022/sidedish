573be0146fec16f6d7e981f6f47f6b4c761c0030
shim506 <shim506@naver.com>
2022-04-19 23:49:43 +0900

    feat: 상세페이지의 이미지 viewPager 구현
    - 동작의 빠른 확인을 위해 dummyData 와 glide 를 사용
    - viewpager 를 담는 레이아웃의 높이를 픽스값이용 -> 수정이 필요함

--- app/src/main/java/com/example/sideDish/ProductDetailFragment.kt
+++ app/src/main/java/com/example/sideDish/ProductDetailFragment.kt
@@ -1,12 +1,14 @@
 package com.example.sideDish
 
 import android.graphics.Paint
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.viewpager2.widget.ViewPager2
 
 class ProductDetailFragment : Fragment() {
     override fun onCreateView(
@@ -14,14 +16,30 @@ class ProductDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val root = inflater.inflate(R.layout.fragment_product_detail_fargment, container, false)
-
+        val textViewIndex = root.findViewById<TextView>(R.id.text_view_image_index)
         // 추후 bindingAdapter 로 이전 예정
         val originPriceTextView: TextView =
             root.findViewById<TextView>(R.id.text_view_origin_price)
         originPriceTextView.paintFlags =
             originPriceTextView.paintFlags.or(Paint.STRIKE_THRU_TEXT_FLAG);
 
+        val viewPager = root.findViewById<ViewPager2>(R.id.view_pager)
+        val dummyList = listOf<String>(
+            "http://public.codesquad.kr/jk/storeapp/data/main/1155_ZIP_P_0081_T.jpg",
+            "http://public.codesquad.kr/jk/storeapp/data/side/17_ZIP_P_6014_T.jpg",
+            "http://public.codesquad.kr/jk/storeapp/data/side/48_ZIP_P_5008_T.jpg",
+        )
+        viewPager.adapter = ImageSliderAdapter(requireContext(), dummyList)
+        viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+                // 추후 수정 필요
+                textViewIndex.text = "${position + 1}/${dummyList.size}"
+            }
+        })
         return root
     }
-
 }
