38938d2975ce71f7396c6bcd09b218e40a5fb2a7
Jason <ggj00125@gmail.com>
2022-04-19 10:40:47 +0900

    ADD: 프로젝트 및 CocoaPods 생성

---
+++ sideDish/sideDish/ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sideDish
+//
+//  Created by 이건행 on 2022/04/18.
+//
+
+import UIKit
+import toast
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
