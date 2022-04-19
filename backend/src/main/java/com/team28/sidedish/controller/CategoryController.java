7d031b9b985300261d7c6033000a3a8862308300
ak2j38 <k2j38@naver.com>
2022-04-20 00:22:33 +0900

    feat: 카테고리 목록 조회 Mock API 구현
    1. CategoryResponse 구현
    2. CategoryService 구현
    3. CategoryController 구현
    4. Swagger 기능 적용
    
    end

---
+++ backend/src/main/java/com/team28/sidedish/controller/CategoryController.java
@@ -0,0 +1,44 @@
+package com.team28.sidedish.controller;
+
+import com.team28.sidedish.controller.dto.CategoryResponse;
+import com.team28.sidedish.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Tag(name = "categories", description = "카테고리 API")
+@RestController
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @Operation(summary = "카테고리 목록 조회",
+            description = "모든 카테고리 목록을 조회합니다.",
+            responses = {
+                @ApiResponse(responseCode = "200",
+                        description = "카테고리 목록 조회 성공",
+                        content = {
+                            @Content(
+                                    mediaType = "application",
+                                    array = @ArraySchema(schema = @Schema(implementation = CategoryResponse.class))
+                            )
+                        })
+            }
+    )
+    @GetMapping("/categories")
+    public ResponseEntity<List<CategoryResponse>> getCategories() {
+        List<CategoryResponse> categories = categoryService.findAll();
+
+        return ResponseEntity.ok().body(categories);
+    }
+}
