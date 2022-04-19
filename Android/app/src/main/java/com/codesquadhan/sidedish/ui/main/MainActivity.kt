4d939d5218b9a8dc49123afa402b0210fb60fad2
Linus <zerolecture0922@gmail.com>
2022-04-19 12:30:26 +0900

    [AOS] feat: 메인 화면 음식 아이템뷰 완성 - 리스트 어댑터 구현 - 더미 데이터로 테스트 완료 - viewType 지정하여 header 와 food 설명 분리

--- Android/app/src/main/java/com/codesquadhan/sidedish/ui/main/MainActivity.kt
+++ Android/app/src/main/java/com/codesquadhan/sidedish/ui/main/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.codesquadhan.sidedish.R
+import com.codesquadhan.sidedish.data.Menu
 import com.codesquadhan.sidedish.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,10 +16,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        val menu = listOf<Menu>(
+            Menu(6, "", "", 0, viewType = 1),
+            Menu(1, "잡채", "맛있어요", 15000),
+            Menu(2, "오리 주물럭 반조리", "감질맛 나는 매콤한 양념", 12460),
+            Menu(3, "소갈비찜", "촉촉하게 벤 양념이 일품", 26100),
+            Menu(4, "간장 코다리 찜", "쫀득한 코다리를 국내산 간장소스를 활용", 14900),
+            Menu(5, "꼬막 비빔밥", "매콤하게 입맛 돋우는", 10900),
+        )
+
+        val mainAdapter = MainAdapter()
+        binding.rvMain.adapter = mainAdapter
+        mainAdapter.submitList(menu)
+
         setBtnGitHub()
     }
 
-    fun setBtnGitHub(){
+    private fun setBtnGitHub() {
         binding.btnGithub.setOnClickListener {
             binding.clLogin.visibility = View.GONE
             binding.clMain.visibility = View.VISIBLE
