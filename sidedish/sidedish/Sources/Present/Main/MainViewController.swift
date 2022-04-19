7bc889ed7fd7c0413e25428069ddbf3af57ad446
Jihee hwang <hwangjihee94@gmail.com>
2022-04-19 11:10:52 +0900

    [Test] 바인딩 test

--- sidedish/sidedish/Sources/Present/Main/MainViewController.swift
+++ sidedish/sidedish/Sources/Present/Main/MainViewController.swift
@@ -4,13 +4,36 @@
 //
 //  Created by seongha shin on 2022/04/18.
 //
-
+import Combine
 import UIKit
 
 class MainViewController: UIViewController {
+    let model: MainViewModelBinding = MainViewModel()
+    private var cancellables = Set<AnyCancellable>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        bind()
+        attritbute()
+        layout()
+    
+        model.action.loadData.send() // loadData 호출 (이벤트가 발생했다!)
+        
+    }
+    
+    private func bind() {
+        model.state.loadedData
+            .sink { index in
+                print(index)
+            } .store(in: &cancellables)
+    }
+    
+    private func attritbute() {
         title = "asfdsafas"
         view.backgroundColor = .red
+    }
+    
+    private func layout() {
+        
     }
 }
