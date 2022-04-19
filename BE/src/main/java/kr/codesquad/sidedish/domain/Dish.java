18c67f64199ec34b24c73dc217b6ff84c098faca
Minseok-Choi <cms05041@gmail.com>
2022-04-19 14:05:28 +0900

    Feat: Dish, Image, DeliveryType, DiscountPolicy 구현

---
+++ BE/src/main/java/kr/codesquad/sidedish/domain/Dish.java
@@ -0,0 +1,53 @@
+package kr.codesquad.sidedish.domain;
+
+import org.springframework.data.annotation.Id;
+
+public class Dish {
+
+    @Id
+    private final Long id;
+    private final Long categoryId;
+    private final String name;
+    private final int stock;
+    private final int price;
+    private final DeliveryType deliveryType;
+    private final DiscountPolicy discountPolicy;
+
+    public Dish(Long id, Long categoryId, String name, int stock, int price, DeliveryType deliveryType, DiscountPolicy discountPolicy) {
+        this.id = id;
+        this.categoryId = categoryId;
+        this.name = name;
+        this.stock = stock;
+        this.price = price;
+        this.deliveryType = deliveryType;
+        this.discountPolicy = discountPolicy;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public DeliveryType getDeliveryType() {
+        return deliveryType;
+    }
+
+    public DiscountPolicy getDiscountPolicy() {
+        return discountPolicy;
+    }
+}
