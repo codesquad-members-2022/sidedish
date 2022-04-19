b4be9ed37117dcbf1461b8b5e69505f5f2e38e68
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-04-19 22:46:52 +0900

    [#2] Test: Food, Ordering 간단한 단위테스트 추가
    - 생성자 호출, 인스턴스 메소드 호출 테스트

---
+++ SideDish/SideDishTests/OrderingTest.swift
@@ -0,0 +1,31 @@
+import XCTest
+@testable import SideDish
+
+class OrderingTests: XCTestCase {
+    
+    private var ordering: Ordering!
+    private var food: Food!
+    
+    override func setUp(){
+        ordering = Ordering()
+        food = Food(detailHash: "hash", alt: "alt", foodDescription: "description", category: "category", normalPrice: 100, specialPrice: 100, deliveryInformation: "deliveryInformation", deliveryCharge: 100, amount: 100, badges: ["badge"])
+
+        super.setUp()
+    }
+    
+    func testOrderingInitialization(){
+        XCTAssertNotNil(ordering)
+    }
+    
+    func testAddingFood(){
+        ordering.addFood(food: food)
+        XCTAssertGreaterThan(ordering.foodList.count, 0)
+    }
+    
+    func testSelectingFood(){
+        ordering.addFood(food: food)
+        XCTAssertNil(ordering.selectedMenu)
+        ordering.selectFood(foodHash: "hash")
+        XCTAssertNotNil(ordering.selectedMenu)
+    }
+}
