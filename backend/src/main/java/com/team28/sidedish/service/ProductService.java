d70bc96789d30637be7288c5d751c9ed0c0edc43
jinan159 <wlsdhks0423@naver.com>
2022-04-20 00:45:15 +0900

    feat: 상품 목록 조회 Mock API 추가
    - categoryId 1, 2, 3 에 따라 메인요리, 국물요리, 밑반찬 조회되는 Mock API 추가

---
+++ backend/src/main/java/com/team28/sidedish/service/ProductService.java
@@ -0,0 +1,9 @@
+package com.team28.sidedish.service;
+
+import com.team28.sidedish.controller.dto.ProductListResponse;
+
+public interface ProductService {
+
+
+    ProductListResponse findProductList(Long categoryId);
+}
