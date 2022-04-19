11cd165064e7cde448006369eb5d7e8d13c07dd9
otterp012 <otterp012@gmail.com>
2022-04-19 15:35:44 +0900

    [feat]: header 컴포넌트 완성 및 마우스 호버 이벤트 추가

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import "./reset.css";
+import Card from "./component/Card";
+import Header from "./component/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <p> start </p>
+      <Header />
+      <div className="card-wrapper">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
