fe62c784b86b665b9d86670f2b9c53c99f1efe75
khj <hongjung.kim.dev@gmail.com>
2022-04-20 00:30:48 +0900

    feat: css변수 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Header from "./Header";
 
-export default function App() {
-  return (
-      <h1>project start</h1>
-  );
-}
+const App = () => {
+  return <Header />;
+};
+
+export default App;
---
+++ src/css/variable.js
@@ -0,0 +1,21 @@
+const color = {
+  Black: "#1b1b1b",
+  GreyOne: "#3f3f3f",
+  GreyTwo: "#777777",
+  GreyThree: "#bcbcbc",
+  GreyFour: "#ebebeb",
+  Green: "#6dd082",
+  Orange: "#ff8e14",
+  OffWhite: "#f8f7f7",
+  White: "#fff",
+};
+
+const fontSize = {
+  xl: "1.5rem",
+  l: "1.25rem",
+  m: "1rem",
+  s: "0.875rem",
+  xs: "0.75rem",
+};
+
+export { color, fontSize };
