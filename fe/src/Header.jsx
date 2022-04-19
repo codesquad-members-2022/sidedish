4970605580767af550fcd67a15432911eb8902c7
Cola <bcad1591@gmail.com>
2022-04-19 16:17:36 +0900

    feat: Header 컴포넌트 추가

---
+++ fe/src/Header.jsx
@@ -0,0 +1,52 @@
+import { MenuList } from './MenuList';
+import styled from 'styled-components';
+
+const TopBar = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+`;
+
+const Logo = styled.h1`
+  margin-right: 40px;
+`;
+
+const TopBarUI = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  width: 100%;
+  padding-top: 12px;
+`;
+
+const UserButtons = styled.div`
+  button:not(:last-child) {
+    margin-right: 16px;
+  }
+`;
+
+const searchIcon = '/Search.svg';
+const mypage = '/Mypage.svg';
+const cart = '/Shopping-cart.svg';
+
+export const Header = () => {
+  return (
+    <TopBar>
+      <Logo>Ordering</Logo>
+      <TopBarUI>
+        <MenuList />
+        <UserButtons>
+          <button>
+            <img src={searchIcon} alt={} />
+          </button>
+          <button>
+            <img src={mypage} alt={} />
+          </button>
+          <button>
+            <img src={cart} alt={} />
+          </button>
+        </UserButtons>
+      </TopBarUI>
+    </TopBar>
+  );
+};
