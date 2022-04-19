43518a3829516ce64c500354f9f5fd73acd46dfc
OceanShape <oceanshape700@gmail.com>
2022-04-19 16:58:06 +0900

    [#3] Feat: 상세 반찬 뷰컨트롤러 구현

---
+++ iOS/SideDish-team24/SideDish-team24/ViewController/DetailBanchanViewController.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+class DetailBanchanViewController: UIViewController {
+    
+    private let scrollView = UIScrollView()
+    private let innerView = UIView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        layout()
+    }
+    
+    private func layout() {
+        let safeArea = self.view.safeAreaLayoutGuide
+        self.view.addSubview(scrollView)
+        self.scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            self.scrollView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            self.scrollView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            self.scrollView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            self.scrollView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
+        
+        self.scrollView.addSubview(innerView)
+        self.innerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            self.innerView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+            self.innerView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            self.innerView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            self.innerView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            self.innerView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor),
+            self.innerView.heightAnchor.constraint(greaterThanOrEqualToConstant: self.view.frame.height)
+        ])
+    }
+    
+    
+}
--- iOS/SideDish-team24/SideDish-team24/ViewController/DetailViewController.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class DetailViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-}
