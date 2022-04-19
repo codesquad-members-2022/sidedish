f0c1b531244b16e607410ddd878b7174d3667dfc
bukim0329 <bukim0329@gmail.com>
2022-04-19 23:10:10 +0900

    Feat: Main 영역 lnb 구현

--- frontend/sidedish/src/Label.jsx
+++ frontend/sidedish/src/Label.jsx
@@ -1,23 +1,27 @@
 import styled from "styled-components";
 
-export const Label = ({ string, color, fontSize, backgroundColor, borderColor, padding }) => {
-  const Label = styled.div`
-    display: inline-block;
-    background-color: ${backgroundColor};
-    padding: ${padding};
-    border: 2px solid ${borderColor};
-    box-sizing: border-box;
-    border-radius: 999px;
-  `;
+const LabelBox = styled.div`
+  display: inline-block;
+  background-color: ${(props) => props.backgroundColor};
+  padding: ${(props) => props.padding};
+  border: 2px solid ${(props) => props.borderColor};
+  box-sizing: border-box;
+  border-radius: 999px;
+`;
 
-  const LabelText = styled.div`
-    color: ${color};
-    font-size: ${fontSize};
-  `;
+const LabelText = styled.div`
+  color: ${(props) => props.color};
+  font-size: ${(props) => props.fontSize};
+`;
 
+const Label = ({ string, color, fontSize, backgroundColor, borderColor, padding }) => {
   return (
-    <Label>
-      <LabelText>{string}</LabelText>
-    </Label>
+    <LabelBox backgroundColor={backgroundColor} borderColor={borderColor} padding={padding}>
+      <LabelText color={color} fontSize={fontSize}>
+        {string}
+      </LabelText>
+    </LabelBox>
   );
 };
+
+export default Label;
--- frontend/sidedish/src/Main.jsx
+++ frontend/sidedish/src/Main.jsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import styled from "styled-components";
-import { Label } from "./Label";
+import Label from "./Label";
 
 const MainContainer = styled.main`
   display: flex;
@@ -7,7 +8,7 @@ const MainContainer = styled.main`
 `;
 
 const MainTitleContainer = styled.div`
-  padding: 66px 80px;
+  padding: 66px 80px 33px 80px;
   display: flex;
   align-items: center;
 `;
@@ -18,16 +19,48 @@ const Title = styled.h1`
   font-weight: 700;
   font-size: 40px;
   color: #1b1b1b;
-  padding: 0 0 0 16px;
+  padding-left: 16px;
+`;
+
+const MainLnbContainer = styled.ul`
+  display: flex;
+  border-bottom: 1px solid #ebebeb;
+  padding-left: 48px;
+`;
+const MainLnb = styled.li`
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 30px;
+  margin: 0 32px;
+  padding-bottom: 17px;
+  cursor: pointer;
+
+  border-bottom: ${(props) => (props.title === props.lnbState ? "black solid 1px" : "")};
 `;
 
 const Main = () => {
+  const data = ["풍성한 고기 반찬", "편리한 반찬 세트", "맛있는 제철 요리", "우리 아이 영양 반찬"];
+  const [lnbState, setLnbState] = useState(data[0]);
+
+  const handleLnbState = (event) => {
+    setLnbState((lnbState) => (lnbState = event.target.textContent));
+  };
+
   return (
     <MainContainer>
       <MainTitleContainer>
         <Label string="기획전" color="black" fontSize="16px" backgroundColor="white" borderColor="black" padding="12px 12px"></Label>
         <Title>한 번 주문하면 두 번 반하는 반찬</Title>
       </MainTitleContainer>
+      <MainLnbContainer>
+        {data.map((title, ind) => (
+          <MainLnb onClick={handleLnbState} title={title} lnbState={lnbState} key={ind}>
+            {title}
+          </MainLnb>
+        ))}
+      </MainLnbContainer>
     </MainContainer>
   );
 };
