bb0d4d8b53d20a7620f807846e51ad33205d1c41
sanghee <anshee97@naver.com>
2022-04-19 17:04:35 +0900

    [CHORE] Add HomeViewController (SangHwi-Back/sidedish#1)

---
+++ SideDish20/SideDish20/ViewController/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  SideDish20
+//
+//  Created by 안상희 on 2022/04/19.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
