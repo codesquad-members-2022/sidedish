8bc293cc8ca2ea3448f9421d43a4f558a12ef45f
asqw887 <asqw887@gmail.com>
2022-04-19 17:14:14 +0900

    ADD: 임시 셀 생성

--- sideDish/sideDish/TestCell.swift
+++ sideDish/sideDish/TestCell.swift
@@ -8,5 +8,13 @@
 import UIKit
 
 class TestCell: UICollectionViewCell {
+    override init(frame: CGRect){
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
     
 }
