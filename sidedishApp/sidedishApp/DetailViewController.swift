4e6757e37ee805fa614167e0361e8c88e8f5686d
wnsxor1993 <wnsxor1993@naver.com>
2022-04-19 16:31:50 +0900

    feat: 상세 화면 ScrollView 구현 및 전체적인 View 구역 정리 #4

--- sidedishApp/sidedishApp/DetailViewController.swift
+++ sidedishApp/sidedishApp/DetailViewController.swift
@@ -5,4 +5,43 @@
 //  Created by juntaek.oh on 2022/04/19.
 //
 
-import Foundation
+import UIKit
+
+class DetailViewController: UIViewController{
+    @IBOutlet weak var fullSizeScrollView: UIScrollView!
+    @IBOutlet weak var imageScrollView: UIScrollView!
+    @IBOutlet weak var detailView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+private extension DetailViewController{
+    func addSubViews(){
+        self.fullSizeScrollView.addSubview(imageScrollView)
+        self.fullSizeScrollView.addSubview(detailView)
+        self.view.addSubview(fullSizeScrollView)
+    }
+    
+    func configureLayout(){
+        fullSizeScrollView.translatesAutoresizingMaskIntoConstraints = false
+        fullSizeScrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        fullSizeScrollView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        fullSizeScrollView.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
+        fullSizeScrollView.heightAnchor.constraint(equalTo: self.view.heightAnchor).isActive = true
+        
+        imageScrollView.translatesAutoresizingMaskIntoConstraints = false
+        imageScrollView.leadingAnchor.constraint(equalTo: self.fullSizeScrollView.leadingAnchor).isActive = true
+        imageScrollView.topAnchor.constraint(equalTo: self.fullSizeScrollView.topAnchor).isActive = true
+        imageScrollView.widthAnchor.constraint(equalTo: self.fullSizeScrollView.widthAnchor).isActive = true
+        imageScrollView.heightAnchor.constraint(equalToConstant: 376).isActive = true
+        
+        detailView.translatesAutoresizingMaskIntoConstraints = false
+        detailView.topAnchor.constraint(equalTo: self.imageScrollView.bottomAnchor, constant: 24).isActive = true
+        detailView.leadingAnchor.constraint(equalTo: self.fullSizeScrollView.leadingAnchor, constant: 16).isActive = true
+        detailView.trailingAnchor.constraint(equalTo: self.fullSizeScrollView.trailingAnchor, constant: -16).isActive = true
+        detailView.heightAnchor.constraint(equalToConstant: 553).isActive = true
+    }
+}
