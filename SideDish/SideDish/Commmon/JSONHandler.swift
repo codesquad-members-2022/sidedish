11612dd682b675caa7028679be82dc0e3c1e2d6f
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-04-19 23:00:18 +0900

    [#3] Add: JSONHandler 싱글톤 클래스 추가
    - 추후 인코딩, 디코딩 메소드 구현해야 함

---
+++ SideDish/SideDish/Commmon/JSONHandler.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+final class JSONHandler{
+    
+    private static let encoder = JSONEncoder()
+    private static let decoder = JSONDecoder()
+    
+    func convertJSONToObject(){
+        
+    }
+    
+    func convertObjectToJSON(){
+        
+    }
+    
+}
