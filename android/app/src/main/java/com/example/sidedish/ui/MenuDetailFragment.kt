07a673e241320bd4ff9dd815220eddc29c35fd06
junseokseo <junseoks9306@gmail.com>
2022-04-20 22:54:42 +0900

    [AOS-josh] feat : viewPager 이미지 추가

--- android/app/src/main/java/com/example/sidedish/ui/MenuDetailFragment.kt
+++ android/app/src/main/java/com/example/sidedish/ui/MenuDetailFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import com.bumptech.glide.Glide
+import com.example.sidedish.data.FoodImage
 import com.example.sidedish.databinding.FragmentDetailBinding
 
 class MenuDetailFragment : Fragment() {
@@ -27,15 +28,20 @@ class MenuDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val imageList = mutableListOf<FoodImage>()
 
         viewModel.selectedFoodDetail.observe(viewLifecycleOwner) { detail ->
             binding.foodDetail = detail
             Glide.with(this).load(detail.detailSection[0]).into(binding.ivDetail1)
             Glide.with(this).load(detail.detailSection[1]).into(binding.ivDetail2)
             Glide.with(this).load(detail.detailSection[2]).into(binding.ivDetail3)
+            imageList.add(FoodImage(detail.thumbImage[0]))
+            imageList.add(FoodImage(detail.thumbImage[1]))
+            val adapter = ImageViewPagerAdapter().apply {
+                submitList(imageList)
+            }
+            binding.pagerDetailImage.adapter = adapter
         }
-
-
     }
 
 }
