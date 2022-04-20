9c1c566c138318fb9d4d995884207a8b97287016
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-04-20 17:08:27 +0900

    [#3] Feat: Ordering 생성자 호출 시에 json 데이터 파싱해서 foodList 초기화하는 로직 추가
    - 추후 foodMap 속성을 사용할 것이기 때문에, foodList에 관련된 로직을 추후 수정해야 함
    - 테스트 메소드 및 기타 관련 로직 수정 필요

--- SideDish/SideDish/Model/Ordering.swift
+++ SideDish/SideDish/Model/Ordering.swift
@@ -1,13 +1,27 @@
 import Foundation
 
+enum Category: String, CaseIterable {
+    case main = "main"
+    case side = "side"
+    case soup = "soup"
+}
+
 class Ordering{
-    private (set) var foodList: [String:Food] = [:]
+    //추후 foodList 제거하고, foodMap으로 통일해서 사용할 예정
+    var foodMap: [Category:[Food]] = [:]
+    var foodList: [String:Food] = [:]
     private (set) var selectedMenu: Food?
     private var count: Int = 0
     var deliveryMoney: Int = 2500
     
+    init(){
+        getSampleFoodList()
+    }
+    
     var sum: Int{
-        guard let price = selectedMenu?.specialPrice else { return -1}
+        //가격정보가 String으로 응답이 와서, 우선 이를 Int로 형변환 처리하기 전이라 임시로 Int값을 할당해서 처리
+        //guard let price = selectedMenu?.specialPrice else { return -1}
+        let price = 5000
         if count * price > 40000 {
             return (count * price) + deliveryMoney
         } else {
@@ -29,4 +43,25 @@ class Ordering{
         guard let unwrappedFood = foodList[foodHash] else { return }
         selectedMenu = unwrappedFood
     }
+    
+    private func getSampleJSONData(fileName: String)-> Data?{
+        guard let path = Bundle.main.url(forResource: fileName, withExtension: "json") else { return nil }
+        guard let data: Data = try? Data(contentsOf: path) else { return nil }
+        return data
+    }
+    
+    private func getSampleFoodList(){
+        for category in Category.allCases {
+            guard let data = getSampleJSONData(fileName: category.rawValue) else { continue }
+            guard let response = JSONHandler.convertJSONToObject(from: data, to: Response.self) else { continue }
+            
+            let foods = response.body
+            foodMap[category] = foods
+            //추후 foodList 속성 제거되면 아래 로직도 제거
+            for food in foods {
+                foodList[food.detailHash] = food
+            }
+        }
+    }
+    
 }
