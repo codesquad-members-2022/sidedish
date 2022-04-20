14ff32515595724106f8acdc0a80d4ebf0bdb62c
Lee SeEun <sally4405@naver.com>
2022-04-20 21:09:06 +0900

    [#1] feat: header custom view 사이즈 조정

--- BanchanApp/BanchanApp/Controllers/BanchanListViewController.swift
+++ BanchanApp/BanchanApp/Controllers/BanchanListViewController.swift
@@ -8,36 +8,48 @@
 import UIKit
 
 class BanchanListViewController: UICollectionViewController {
-
+    
+    private let headerItem = ["모두가 좋아하는\n든든한 메인 요리", "정성이 담긴\n뜨끈뜨끈 국물 요리", "식탁을 풍성하게 하는\n정갈한 밑반찬"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setCollectionLayout()
     }
-
+    
     private func configureUI() {
-
+        
     }
-
+    
+    private func setCollectionLayout() {
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            let width = view.frame.width
+            layout.headerReferenceSize = CGSize(width: width, height: 144)
+        }
+    }
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
-
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return headerItem.count
+    }
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
         return cell
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         guard let viewController = UIStoryboard(name: "BanchanDetailViewController", bundle: nil)
             .instantiateInitialViewController() else { return }
         self.navigationController?.pushViewController(viewController, animated: true)
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        if kind == UICollectionView.elementKindSectionHeader {
-            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath)
-            return header
-        } else {
-            return UICollectionReusableView()
-        }
+        guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath) as? Header else { return UICollectionReusableView() }
+        header.title.text = headerItem[indexPath.section]
+        return header
     }
+    
 }
--- BanchanApp/BanchanApp/Views/Header.swift
+++ BanchanApp/BanchanApp/Views/Header.swift
@@ -8,13 +8,31 @@
 import UIKit
 
 class Header: UICollectionReusableView {
+    
+    let title: UILabel = UILabel()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = .gray
+        setup()
     }
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        self.backgroundColor = .gray
+        setup()
     }
+
+    private func setup() {
+        self.addSubview(title)
+        title.numberOfLines = 0
+        title.font = .systemFont(ofSize: 32)
+        title.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            title.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            title.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            title.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 24),
+            title.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -24)
+        ])
+    }
+    
 }
