fd9566a6dd28979a49980366768b13c26866e3e0
asqw887 <asqw887@gmail.com>
2022-04-20 16:37:43 +0900

    ADD: mock data 생성

--- sideDish/sideDish/Food.swift
+++ sideDish/sideDish/Food.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+struct Food: Codable{
+    private(set) var detailHash: String
+    private(set) var title: String
+    private(set) var description: String
+    private(set) var image: String
+    private(set) var alt: String
+    private(set) var badge: [Badge]
+    private(set) var deliveryType: [String]
+    private(set) var originPrice: Int
+    private(set) var salePrice: Int?
+    
+    enum CodingKyes: String, CodingKey{
+        case detailHash = "detail_hash"
+        case title
+        case description
+        case image
+        case alt
+        case badge
+        case deliveryType = "delivery_type"
+        case originPrice = "n_price"
+        case salePrice = "s_price"
+    }
+}
+
+enum Badge: String, Codable{
+    case 이벤트특가
+    case 점심특가
+}
