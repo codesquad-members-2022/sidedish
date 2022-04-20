be7b80127b82237f8e6b925614fbde18499fb60e
BumgeunSong <isp1195@gmail.com>
2022-04-20 11:05:20 +0900

    [#3] Feat: fetch Products in NetworkManager

---
+++ SideDishApp/SideDishAppTests/NetworkManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManagerTests.swift
+//  SideDishAppTests
+//
+//  Created by Bumgeun Song on 2022/04/20.
+//
+
+import XCTest
+
+class NetworkManagerTests: XCTestCase {
+
+    func testExample() throws {
+        let two = 2
+        XCTAssertEqual(1+1, two)
+    }
+
+}
