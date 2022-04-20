6f7571108494b7457319fd844f02b9380371d07b
sanghee <anshee97@naver.com>
2022-04-20 09:22:21 +0900

    [DESIGN] 상품 목록 화면 구성 (SangHwi-Back/sidedish#1)

--- SideDish20/SideDish20/Application/SceneDelegate.swift
+++ SideDish20/SideDish20/Application/SceneDelegate.swift
@@ -4,5 +4,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        let storyboard = UIStoryboard(name: "Home", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "HomeSB") as! HomeViewController
+        self.window?.rootViewController = vc
+    }
 }
 
--- SideDish20/SideDish20/ViewController/HomeViewController.swift
+++ SideDish20/SideDish20/ViewController/HomeViewController.swift
@@ -8,10 +8,72 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    var products = [HomeModel]()
+    let homeCollectionViewCellId = "homeCollectionViewCell"
 
     @IBOutlet weak var collectionView: UICollectionView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        registerXib()
+
+        let nibName = UINib(nibName: "HomeCollectionViewCell", bundle: nil)
+        collectionView.register(nibName, forCellWithReuseIdentifier: homeCollectionViewCellId)
+
+        for _ in 1...25 {
+            let model = HomeModel()
+            model?.image = "img01"
+            model?.name = "뜨끈한 국밥"
+            model?.description = "아주 따뜻해유"
+            model?.discountedPrice = "12,370원"
+            model?.originalPrice = "18,380원"
+            model?.specialMessage = "이벤트특가"
+            products.append(model!)
+        }
+        collectionView.reloadData()
+    }
+
+    private func registerXib() {
+        let nibName = UINib(nibName: "HomeCollectionViewCell", bundle: nil)
+        collectionView.register(nibName, forCellWithReuseIdentifier: homeCollectionViewCellId)
+    }
+}
+
+extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "homeCollectionViewCell",
+                                                            for: indexPath) as? HomeCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.imageView.image = UIImage(named: "img01")
+        cell.nameLabel.text = products[indexPath.item].name
+        cell.descriptionLabel.text = products[indexPath.item].description
+        cell.discountedPriceLabel.text = products[indexPath.item].discountedPrice
+        cell.originalPriceLabel.text = products[indexPath.item].originalPrice
+        cell.specialMessagelabel.text = products[indexPath.item].specialMessage
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        let inset: CGFloat = 10
+        return UIEdgeInsets(top: inset, left: inset, bottom: inset, right: inset)
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.width, height: 130)
     }
 }
