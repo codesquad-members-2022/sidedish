3707da8332aafe31d646f357cc78bf2441e6961c
hwj <jminie@naver.com>
2022-04-20 15:27:49 +0900

    feat: git 병합 및 충돌 해결

---
+++ android/app/src/main/java/com/example/sidedish/BindingAdapter.kt
@@ -0,0 +1,30 @@
+
+import android.graphics.Paint
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.sidedish.GlideApp
+
+@BindingAdapter("imageUrl")
+fun loadImage(view: ImageView, imageUrl: String) {
+    GlideApp.with(view)
+        .load(imageUrl)
+        .into(view)
+}
+
+@BindingAdapter("setPrice")
+fun setPrice(view: TextView, body: String?) {
+    view.text = body
+    view.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+    if (body == null) {
+        view.visibility = View.GONE
+    }
+}
+
+@BindingAdapter("setEventOn")
+fun setEventOn(view: TextView, event: String?) {
+    if (event == null) {
+        view.visibility = View.GONE
+    }
+}
--- android/app/src/main/java/com/example/sidedish/MainActivity.kt
+++ android/app/src/main/java/com/example/sidedish/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.sidedish
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.sidedish.databinding.ActivityMainBinding
 
@@ -12,11 +12,47 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.lifecycleOwner = this
 
-        binding.button.setOnClickListener {
-            val intent = Intent(this, ProductDetailActivity::class.java)
-            startActivity(intent)
+        val mainAdapter = MainHomeAdapter()
+        val soupAdapter = MainHomeAdapter()
+        val sideAdapter = MainHomeAdapter()
+
+        binding.recyclerviewMainlist.adapter = mainAdapter
+        binding.recyclerviewSouplist.adapter = soupAdapter
+        binding.recyclerviewSidelist.adapter = sideAdapter
+
+        mainAdapter.submitList(mainPut())
+        soupAdapter.submitList(soupPut())
+        sideAdapter.submitList(sidePut())
+
+        val count1 = mainAdapter.itemCount.toString()
+        binding.headerSub1.text = "${count1}개 상품이 등록되어 있습니다."
+
+        val count2 = soupAdapter.itemCount.toString()
+        binding.headerSub2.text = "${count2}개 상품이 등록되어 있습니다."
+
+        val count3 = sideAdapter.itemCount.toString()
+        binding.headerSub3.text = "${count3}개 상품이 등록되어 있습니다."
+
+        binding.headerTitle1.setOnClickListener {
+            if (binding.headerSub1.visibility == View.GONE) {
+                binding.headerSub1.visibility = View.VISIBLE
+            } else
+                binding.headerSub1.visibility = View.GONE
+        }
+
+        binding.headerTitle2.setOnClickListener {
+            if (binding.headerSub2.visibility == View.GONE) {
+                binding.headerSub2.visibility = View.VISIBLE
+            } else
+                binding.headerSub2.visibility = View.GONE
+        }
+
+        binding.headerTitle3.setOnClickListener {
+            if (binding.headerSub3.visibility == View.GONE) {
+                binding.headerSub3.visibility = View.VISIBLE
+            } else
+                binding.headerSub3.visibility = View.GONE
         }
     }
 }
