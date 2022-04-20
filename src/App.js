113621c72ec86e18667d512bd94f3aa2bd090c9a
seungchanHan <seungchanhan00@gmail.com>
2022-04-20 15:50:01 +0900

    feat: [#2] 목데이터 받아오기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './Header';
+import MainCard from './MainCard';
+import { fetchData } from './utility/util';
 
 const App = () => {
-  return <Header />;
+  const [mainCardData, setMainCardData] = useState([]);
+  const [mainDishData, setMainDishData] = useState([]);
+  const [sideDishData, setSideDishData] = useState([]);
+  const [soupData, setSoupData] = useState([]);
+
+  useEffect(() => {
+    const mainCardUrl = 'data/mainCard.json';
+    const mainDishUrl = 'data/mainDish.json';
+    const sideDishUrl = 'data/sideDish.json';
+    const soupUrl = 'data/soup.json';
+
+    const getData = async (url, setData) => {
+      const response = await fetchData(url);
+      setData(response.data);
+    };
+
+    getData(mainCardUrl, setMainCardData);
+    getData(mainDishUrl, setMainDishData);
+    getData(sideDishUrl, setSideDishData);
+    getData(soupUrl, setSoupData);
+  }, [setMainCardData, setMainDishData, setSideDishData, setSoupData]);
+
+  return (
+    <>
+      <Header />
+      <MainCard data={(mainCardData, mainDishData)} />
+    </>
+  );
 };
 
 export default App;
