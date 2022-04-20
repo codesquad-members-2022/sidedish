62f8c4d465d243eaf7147dcf86c6c11e08228182
nak253 <nwon4413@naver.com>
2022-04-20 15:38:22 +0900

    feat: ItemResponse, ItemsResponse, ItemDetailResponse Dto 클래스 작성

---
+++ BE/src/main/java/com/team34/sidedish/dto/ItemDetailResponse.java
@@ -0,0 +1,18 @@
+package com.team34.sidedish.dto;
+
+import java.util.List;
+
+public class ItemDetailResponse {
+
+    private int originPrice;
+    private int discountPrice;
+    private int deliveryFee;
+    private double mileageRatio;
+    private boolean earlyDeliverable;
+    private Integer freeShippingAmount;
+    private String title;
+    private String content;
+    private List<String> images;
+    private List<String> badges;
+    private List<ItemResponse> recommendedItems;
+ }
---
+++ BE/src/main/java/com/team34/sidedish/dto/ItemsResponse.java
@@ -0,0 +1,9 @@
+package com.team34.sidedish.dto;
+
+import java.util.List;
+
+public class ItemsResponse {
+
+    private List<ItemsResponse> eventItems;
+    private List<ItemsResponse> items;
+}
