30f1de116cd569d962cab108b650d364426e671b
yongseongjeon <yongseong.jeon95@gmail.com>
2022-04-20 22:30:47 +0900

    💄 color, font size, weight 변수 선언

---
+++ src/variable/font.js
@@ -0,0 +1,15 @@
+const SIZE = {
+  SMALL: "0.857rem", // 12px
+  BASE: "1rem", // 14px
+  MEDIUM: "1.142rem", // 16px
+  LARGE: "1.428rem", // 20px
+  X_LARGE: "1.714rem", // 24px
+};
+
+const WEIGHT = {
+  REGULAR: 400,
+  MEDIUM: 500,
+  BOLD: 700,
+};
+
+export { SIZE, WEIGHT };
