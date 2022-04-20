104c54412a0f6b08062a063bb6323c04c0317a6d
BumgeunSong <isp1195@gmail.com>
2022-04-20 11:41:49 +0900

    [#3] Test: Add test case for fetchProducts()

--- SideDishApp/SideDishAppTests/NetworkManagerTests.swift
+++ SideDishApp/SideDishAppTests/NetworkManagerTests.swift
@@ -6,12 +6,31 @@
 //
 
 import XCTest
+import OSLog
+@testable import SideDishApp
 
 class NetworkManagerTests: XCTestCase {
 
     func testExample() throws {
         let two = 2
         XCTAssertEqual(1+1, two)
+    }
+    
+    func testFetchProducts() throws {
+        let promise = XCTestExpectation(description: "Fetch products success")
+        
+        let networkManager = NetworkManager()
+        networkManager.fetchProducts { result in
+            switch result {
+            case .success(let products):
+                debugPrint(products)
+                promise.fulfill()
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+        
+        wait(for: [promise], timeout: 1)
     }
 
 }
