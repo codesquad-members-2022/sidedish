a1dea9a425ce1a93eec8b7e25f795200fe93bb60
khj <hongjung.kim.dev@gmail.com>
2022-04-21 00:39:11 +0900

    feat: [#1] hover시에 border선 내리기

--- src/Header.js
+++ src/Header.js
@@ -6,85 +6,97 @@ const Header = () => {
   return (
     <StyledHeader>
       <FlexBetween>
-        <HeaderTitle>Ordering</HeaderTitle>
-        <HeaderGnb>
-          <HeaderGnbTap>
-            <span>든든한 메인요리</span>
-            <HeaderGnbCategory>
-              <HeaderGnbContent>육류 요리</HeaderGnbContent>
-              <HeaderGnbContent>해산물 요리</HeaderGnbContent>
-            </HeaderGnbCategory>
-          </HeaderGnbTap>
+        <Title>Ordering</Title>
+        <Gnb>
+          <GnbTap>
+            <GnbTitle>든든한 메인요리</GnbTitle>
+            <GnbCategory>
+              <GnbContent>육류 요리</GnbContent>
+              <GnbContent>해산물 요리</GnbContent>
+            </GnbCategory>
+          </GnbTap>
 
-          <HeaderGnbTap>
-            <span>뜨끈한 국물요리</span>
-            <HeaderGnbCategory>
-              <HeaderGnbContent>국/탕/찌개</HeaderGnbContent>
-            </HeaderGnbCategory>
-          </HeaderGnbTap>
+          <GnbTap>
+            <GnbTitle>뜨끈한 국물요리</GnbTitle>
+            <GnbCategory>
+              <GnbContent>국/탕/찌개</GnbContent>
+            </GnbCategory>
+          </GnbTap>
 
-          <HeaderGnbTap>
-            <span>정갈한 밑반찬</span>
-            <HeaderGnbCategory>
-              <HeaderGnbContent>나물/무침</HeaderGnbContent>
-              <HeaderGnbContent>조림/볶음</HeaderGnbContent>
-              <HeaderGnbContent>절임/장아찌</HeaderGnbContent>
-            </HeaderGnbCategory>
-          </HeaderGnbTap>
-        </HeaderGnb>
+          <GnbTap>
+            <GnbTitle>정갈한 밑반찬</GnbTitle>
+            <GnbCategory>
+              <GnbContent>나물/무침</GnbContent>
+              <GnbContent>조림/볶음</GnbContent>
+              <GnbContent>절임/장아찌</GnbContent>
+            </GnbCategory>
+          </GnbTap>
+        </Gnb>
       </FlexBetween>
-      <HeaderMenu>
-        <HeaderMenuIcon src='assets/search.png' alt='search'></HeaderMenuIcon>
-        <HeaderMenuIcon src='assets/user.png' alt='user'></HeaderMenuIcon>
-        <HeaderMenuIcon src='assets/basket.png' alt='basket'></HeaderMenuIcon>
-      </HeaderMenu>
+
+      <Menu>
+        <MenuIcon src='assets/search.png' alt='search'></MenuIcon>
+        <MenuIcon src='assets/user.png' alt='user'></MenuIcon>
+        <MenuIcon src='assets/basket.png' alt='basket'></MenuIcon>
+      </Menu>
     </StyledHeader>
   );
 };
 
-const HeaderGnb = styled.section`
-  ${flexBetween}
-`;
-
-const HeaderGnbTap = styled.div`
-  padding: 24px;
-  &:hover {
-    ul {
-      display: block;
-      position: absolute;
-      margin-top: 10px;
-    }
-  }
-`;
-
-const HeaderGnbCategory = styled.ul`
-  display: none;
-`;
-
-const HeaderGnbContent = styled.li`
-  padding-top: 10px;
-`;
-
 const StyledHeader = styled.header`
   ${flexBetween}
+  border-bottom: 1px solid black;
 `;
 
 const FlexBetween = styled.div`
   ${flexBetween}
 `;
 
-const HeaderTitle = styled.h2`
+const Title = styled.h2`
   margin: 18px 40px 30px 80px;
   font-size: 40px;
   font-weight: 900;
 `;
 
-const HeaderMenu = styled.div`
-  ${flexCenter}
-  margin-right: 80px;
+const Gnb = styled.section`
+  ${flexBetween}
 `;
 
-const HeaderMenuIcon = styled.img`
+const GnbTap = styled.div`
+  padding: 35px 24px 24px 0;
+  &:hover {
+    height: 90px;
+    ul {
+      display: block;
+
+      position: absolute;
+      margin-top: 10px;
+    }
+  }
+`;
+
+const GnbTitle = styled.span`
+  cursor: pointer;
+`;
+
+const GnbCategory = styled.ul`
+  display: none;
+`;
+
+const GnbContent = styled.li`
+  padding-top: 10px;
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Menu = styled.div`
+  ${flexCenter}
+  margin: 16px 80px 0 0;
+`;
+
+const MenuIcon = styled.img`
   padding: 16px;
 `;
 
