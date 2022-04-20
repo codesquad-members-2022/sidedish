5dd13150a77d16f63b34714822716d6ad5609a85
soralee <sora2821@gmail.com>
2022-04-20 20:55:14 +0900

    [#6] Chore: sass 모듈 추가, react 인식 불가 에러 해결

--- FE/src/index.js
+++ FE/src/index.js
@@ -4,8 +4,12 @@ import ReactDOM from "react-dom/client";
 import App from "@/App";
 =======
 import App from "./App";
+<<<<<<< HEAD
 >>>>>>> ccf5e4b ([#6] Chore: CRA 없이 React 빌드 환경 구축)
 
+=======
+window.React = React;
+>>>>>>> 3caa9c6 ([#6] Chore: sass 모듈 추가, react 인식 불가 에러 해결)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
