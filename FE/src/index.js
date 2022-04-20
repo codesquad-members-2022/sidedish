66528bc3a4652908cba04817472fd16838d49ad3
Hemudi <ksum1205@naver.com>
2022-04-20 18:00:55 +0900

    Refactor: GlobalStyle 적용 범위 수정 - #9
    해당 컴포넌트를 상단으로 올려 App 컴포넌트에서 적용되게 수정
    
    Co-authored-by: Millie <jaypedia@users.noreply.github.com>

--- FE/src/index.js
+++ FE/src/index.js
@@ -9,8 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
       <GlobalStyle />
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
