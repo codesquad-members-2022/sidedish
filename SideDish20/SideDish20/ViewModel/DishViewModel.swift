498b52b3a15de6f7cf2af4dbc58556d85664a37e
백상휘 <sanghwiback@gmail.com>
2022-04-20 14:58:31 +0900

    [FEAT] DishViewModel에 이미지를 호출하는 기능을 추가 (SangHwi-Back/sidedish#4)

--- SideDish20/SideDish20/ViewModel/DishViewModel.swift
+++ SideDish20/SideDish20/ViewModel/DishViewModel.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class DishViewModel {
     private let fetchOnComplete: ([UIImage]?) -> Void
-    private let fetchCachingImages = FetchCachingImages()
     
+    private let fetchCachingImages = FetchCachingImages()
     private let cacheImageDataMiddleWare = CacheImageDataMiddleWare()
     
     init(fetchOnComplete: @escaping ([UIImage]?) -> Void) {
@@ -36,14 +36,14 @@ class DishViewModel {
         }
     }
     
-    func fetchCachedImage(as name: String) {
+    func fetchCachedImage(as name: String, fetchImageHandler: @escaping (UIImage?) -> Void) {
         fetchCachingImages.fetchCacheImage(as: name) { result in
-            guard let result = result, let image = UIImage(data: result.data) else {
-                self.fetchOnComplete(nil)
+            guard let result = result else {
+                fetchImageHandler(nil)
                 return
             }
             
-            self.fetchOnComplete([image])
+            fetchImageHandler(UIImage(data: result.data))
         }
     }
     
@@ -51,5 +51,34 @@ class DishViewModel {
         guard let data = image.jpegData(compressionQuality: 1.0) else { return }
         cacheImageDataMiddleWare.cacheImage(as: name, contentsOf: data)
         fetchOnComplete([image])
+    }
+    
+    func getImage(from urlString: String, getImageHandler: @escaping (UIImage?) -> Void) {
+        guard let url = URL(string: urlString) else {
+            getImageHandler(nil)
+            return
+        }
+        
+        fetchCachedImage(as: url.lastPathComponent) { image in
+            if let image = image {
+                getImageHandler(image)
+                return
+            }
+            
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                guard error == nil, let data = data else {
+                    getImageHandler(nil)
+                    return
+                }
+                
+                let image = UIImage(data: data)
+                getImageHandler(image)
+                
+                if let image = image {
+                    self.cacheImage(as: url.lastPathComponent, contentsOf: image)
+                }
+                
+            }.resume()
+        }
     }
 }
