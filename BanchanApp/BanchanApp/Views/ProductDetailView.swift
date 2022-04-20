384245b5603d9de650705d0fd956e6880d51dc70
Lee SeEun <sally4405@naver.com>
2022-04-20 21:11:22 +0900

    [#1] feat: productDetailView 구현

---
+++ BanchanApp/BanchanApp/Views/ProductDetailView.swift
@@ -0,0 +1,126 @@
+//
+//  ProductDetailView.swift
+//  BanchanApp
+//
+//  Created by Se Eun Lee on 2022/04/19.
+//
+
+import UIKit
+
+class ProductDetailView: UIView {
+    
+    private let title: UILabel = UILabel()
+    private let menuDescription: UILabel = UILabel()
+    private let nomalPrice: UILabel? = UILabel()
+    private let salePrice: UILabel = UILabel()
+    private let badges: UIStackView = UIStackView()
+    private let badgeList: [String] = ["런칭특가", "이벤트특가"]
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    private func setup() {
+        title.text = "오리 주물럭_반조리"
+        menuDescription.text = "감칠맛 나는 매콤한 양념"
+        nomalPrice?.text = "12,640원"
+        salePrice.text = "15,800원"
+        badgeList.forEach {
+            let badge = BadgeLabel(color: .systemBlue)
+            badge.text = $0
+            badges.addArrangedSubview(badge)
+        }
+        
+        badges.addArrangedSubview(BadgeLabel(color: .systemBackground))
+        badges.axis = .horizontal
+        badges.spacing = 5
+        badges.distribution = .fillProportionally
+        
+        title.font = .systemFont(ofSize: 14, weight: .semibold)
+        menuDescription.font = .systemFont(ofSize: 14)
+        menuDescription.textColor = .systemGray2
+        salePrice.font = .systemFont(ofSize: 14, weight: .semibold)
+        nomalPrice?.font = .systemFont(ofSize: 14)
+        nomalPrice?.textColor = .systemGray2
+        nomalPrice?.applyStrikethoroughStyle()
+        
+        let priceStackView: UIStackView = UIStackView(arrangedSubviews: [salePrice])
+        if let nomalPrice = nomalPrice {
+            priceStackView.addArrangedSubview(nomalPrice)
+        }
+        priceStackView.axis = .horizontal
+        
+        let stackView: UIStackView = UIStackView(arrangedSubviews: [title, menuDescription, priceStackView, badges])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        
+        self.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            stackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            stackView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 13),
+            stackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -13)
+        ])
+    }
+    
+    private func translateViews(_ views: [UIView?]) {
+        views.forEach {
+            if let view = $0 {
+                view.translatesAutoresizingMaskIntoConstraints = false
+            }
+        }
+    }
+    
+}
+
+extension UILabel {
+    
+    func applyStrikethoroughStyle() {
+        if let text = self.text {
+            let attributeString = NSMutableAttributedString(string: text)
+            attributeString.addAttribute(.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: attributeString.length))
+            self.attributedText = attributeString
+        }
+    }
+    
+}
+
+class BadgeLabel: UILabel {
+    
+    private var padding = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+
+    convenience init(color: UIColor) {
+        self.init()
+        setupStyle(with: color)
+    }
+    
+    private func setupStyle(with color: UIColor) {
+        self.font = .systemFont(ofSize: 12, weight: .semibold)
+        self.textColor = .white
+        self.backgroundColor = color
+        self.textAlignment = .center
+        self.layer.masksToBounds = true
+        self.layer.cornerCurve = .continuous
+        self.layer.cornerRadius = 15
+    }
+    
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: padding))
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        var contentSize = super.intrinsicContentSize
+        contentSize.height += padding.top + padding.bottom
+        contentSize.width += padding.left + padding.right
+        return contentSize
+    }
+    
+}
