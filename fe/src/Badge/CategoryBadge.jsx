b52b2e57979ddf17d2ab5a64cb7da7a65886ffc2
Cola <bcad1591@gmail.com>
2022-04-21 00:03:08 +0900

    style: CategoryBadge 컴포넌트 린트 오류 수정

--- fe/src/Badge/CategoryBadge.jsx
+++ fe/src/Badge/CategoryBadge.jsx
@@ -4,9 +4,9 @@ const Root = styled.button`
   padding: 8px 16px;
   border: 2px solid black;
   border-radius: 999px;
-  background-color: #F8F7F7; // TODO: 변수로 바꾸기
+  background-color: #f8f7f7; // TODO: 변수로 바꾸기
 `;
 
 export const CategoryBadge = ({ name }) => {
-  return <Root className={"fonts-md"}>{name}</Root>;
+  return <Root className={'fonts-md'}>{name}</Root>;
 };
