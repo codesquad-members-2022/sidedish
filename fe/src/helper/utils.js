686235967dafda90dc2fee9bbdc920a295fb3754
bangdler <zbthz90@gmail.com>
2022-04-20 16:54:18 +0900

    [fe#15] chore: applyFlex 매개변수 전달 방식 객체로 수정

--- fe/src/helper/utils.js
+++ fe/src/helper/utils.js
@@ -1,10 +1,14 @@
 import { css } from 'styled-components';
 
-function flex(justify = 'start', align = 'stretch') {
+function flex({ justify, align, direction }) {
+  justify = justify || 'start';
+  align = align || 'stretch';
+  direction = direction || 'row';
   return `
     display: flex;
     justify-content: ${justify};
     align-items: ${align};
+    flex-direction: ${direction};
   `;
 }
 
@@ -12,7 +16,7 @@ export function applyFlex(props) {
   return (
     props.flex &&
     css`
-      ${flex(props.justify, props.align)}
+      ${flex(props)}
     `
   );
 }
