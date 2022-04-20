b505c8e76cc0a35fb336933e8459033e86300de9
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-04-20 15:02:33 +0900

    [#3] Feat: JSONHandler의 인코딩, 디코딩 메소드 구현
    - 싱글톤 객체로 사용할 것이기 때문에 모든 메소드를 static으로 선언
    - 제네릭을 사용해서, 모든 Codable 채택한 타입을 받을 수 있도록 함

--- SideDish/SideDish/Commmon/JSONHandler.swift
+++ SideDish/SideDish/Commmon/JSONHandler.swift
@@ -5,12 +5,11 @@ final class JSONHandler{
     private static let encoder = JSONEncoder()
     private static let decoder = JSONDecoder()
     
-    func convertJSONToObject(){
-        
+    static func convertJSONToObject<T: Decodable>(from data: Data, to targetType: T.Type)-> T?{
+        return try? decoder.decode(T.self, from: data)
     }
     
-    func convertObjectToJSON(){
-        
+    static func convertObjectToJSON<T: Encodable>(from object: T)-> Any?{
+        return try? encoder.encode(object)
     }
-    
 }
