3b4cc2c6398f57665968f73e2507501aab43d0d4
Linus <zerolecture0922@gmail.com>
2022-04-20 15:40:43 +0900

    [AOS] feat: 상품 상세 화면(detail) 레이아웃 구성 완료 - 조리법 및 상세 설명을 이미지로 보여주는 화면을 리사이클러뷰로 구성 - 테스트 데이터로 화면에 이미지 정상적으로 출력 확인

--- Android/app/src/main/java/com/codesquadhan/sidedish/ui/detail/DetailActivity.kt
+++ Android/app/src/main/java/com/codesquadhan/sidedish/ui/detail/DetailActivity.kt
@@ -1,16 +1,19 @@
 package com.codesquadhan.sidedish.ui.detail
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.viewpager2.widget.ViewPager2
 import com.codesquadhan.sidedish.R
 import com.codesquadhan.sidedish.databinding.ActivityDetailBinding
 
-class DetailActivity: AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailBinding
     private lateinit var viewPagerAdapter: ViewPagerAdapter
+    private lateinit var detailAdapter: DetailAdapter
+    private val detailViewModel: DetailViewModel by viewModels()
 
     private val testUrlList = ArrayList<String>()
 
@@ -22,6 +25,12 @@ class DetailActivity: AppCompatActivity() {
         binding.vpDetail.adapter = viewPagerAdapter
         binding.vpDetail.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
+        detailAdapter = DetailAdapter()
+        binding.rvFoodDetail.adapter = detailAdapter
+        detailViewModel.detailImageListLd.observe(this) {
+            detailAdapter.submitList(it.toList())
+        }
+
         setViewPagerListener()
 
         testUrlList.add("https://ww.namu.la/s/32b66f77ef969034adcbce3840d362f66470c1ade8b3b67e9859954467a61a77ffe24051fd4db50a6a5251acd242a9140df6fc120653c85407c5508b00e763d8b2b09754bbe86f5ec315d6c2bfa597a8dc287028f3608155e80c67801d60595d")
@@ -29,11 +38,13 @@ class DetailActivity: AppCompatActivity() {
         viewPagerAdapter.updateImageList(testUrlList)
 
         binding.tvTotalPage.text = viewPagerAdapter.itemCount.toString()
+
+
     }
 
-    fun setViewPagerListener(){
+    fun setViewPagerListener() {
         // 페이지 리스너 달아보기
-        binding.vpDetail.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback(){
+        binding.vpDetail.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
                 binding.tvCurrentPage.text = (position + 1).toString()
---
+++ Android/app/src/main/java/com/codesquadhan/sidedish/ui/detail/DetailAdapter.kt
@@ -0,0 +1,49 @@
+package com.codesquadhan.sidedish.ui.detail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.codesquadhan.sidedish.data.DetailImage
+import com.codesquadhan.sidedish.databinding.ItemDetailBinding
+
+class DetailAdapter : ListAdapter<DetailImage, DetailAdapter.DetailViewHolder>(diffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailViewHolder {
+        val binding = ItemDetailBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return DetailViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: DetailViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class DetailViewHolder(private val binding: ItemDetailBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(detailImage: DetailImage) {
+            binding.detailImage = detailImage
+
+            Glide.with(binding.root)
+                .load(detailImage.imagePath)
+                .centerCrop()
+                .into(binding.ivFoodDetail)
+        }
+
+    }
+
+    companion object {
+
+        val diffUtil = object : DiffUtil.ItemCallback<DetailImage>() {
+            override fun areItemsTheSame(oldItem: DetailImage, newItem: DetailImage): Boolean {
+                return oldItem.menuId == newItem.menuId
+            }
+
+            override fun areContentsTheSame(oldItem: DetailImage, newItem: DetailImage): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
