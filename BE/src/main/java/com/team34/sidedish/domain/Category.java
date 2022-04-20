b72f7b3bfd011146177e337f7554ffd9547323a5
nak253 <nwon4413@naver.com>
2022-04-20 15:18:02 +0900

    feat: Caregory 클래스에 parent와 children 필드 추가
    - FE요청에 따른 Category parent와 List<Category> children를 추가하였다.

--- BE/src/main/java/com/team34/sidedish/domain/Category.java
+++ BE/src/main/java/com/team34/sidedish/domain/Category.java
@@ -1,8 +1,12 @@
 package com.team34.sidedish.domain;
 
+import java.util.List;
+
 public class Category {
 
     private Long id;
     private String name;
     private boolean isEvent;
+    private Category parent;
+    private List<Category> children;
 }
