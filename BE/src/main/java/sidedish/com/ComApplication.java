e8eca1311335793c4e7481d3d6383d38b794847a
Louie <dhdustnr0134@naver.com>
2022-04-20 14:24:31 +0900

    Feat: 프로젝트 초기 세팅
    ref: #8

---
+++ BE/src/main/java/sidedish/com/ComApplication.java
@@ -0,0 +1,13 @@
+package sidedish.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComApplication.class, args);
+	}
+
+}
