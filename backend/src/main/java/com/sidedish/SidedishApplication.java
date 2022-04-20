2adca401557407288827364e71d07e9de3801282
rkolx <olx.rko.o@gmail.com>
2022-04-20 23:15:55 +0900

    build : 프로젝트 디렉터리 구조 변경
    무의미한 example 디렉터리 삭제하고 sidedish 디렉터리를 상위로 디렉터리 이동

--- backend/src/main/java/com/sidedish/SidedishApplication.java
+++ backend/src/main/java/com/sidedish/SidedishApplication.java
@@ -1,4 +1,4 @@
-package com.example.sidedish;
+package com.sidedish;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/sidedish/SidedishApplicationTests.java
+++ backend/src/test/java/com/sidedish/SidedishApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.sidedish;
+package com.sidedish;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
