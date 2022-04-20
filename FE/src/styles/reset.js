fc57090826b560d591305cf8c7795663352d3b50
Jwu <sju02048@naver.com>
2022-04-20 21:07:11 +0900

    [team-03][FE][쥬&도리] 1주차 첫 PR: 프로젝트 환경설정 및 설계 (#25)
    * [공통] Issues, PR templates 와 프로젝트 소개 README.md 추가 (#2)
    
    * Docs: 프로젝트 및 팀원 소개(readme.md)
    
    * Chore: Issues, PR templates 추가
    
    ref: #1
    
    Co-authored-by: “Louie-03” <dhdustnr0134@naver.com>
    
    * Docs: 팀원 수정
    
    - 팀원 한 마디 추가
    
    * Chore: 초기개발환경
    
    Chore: CRA 초기구성
    
    Chore: eslint 구성
    
    Chore: prettier 구성
    
    * Style: App.js -> jsx 수정
    
    Co-authored-by: kukim <57086195+ku-kim@users.noreply.github.com>
    Co-authored-by: “Louie-03” <dhdustnr0134@naver.com>

---
+++ FE/src/styles/reset.js
@@ -0,0 +1,9 @@
+import reset from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+// global 추가할 것
+`;
+
+export default GlobalStyle;
