9992e7c3f6593b08c14f4922efdbcbf9a503d4f6
Cola <bcad1591@gmail.com>
2022-04-20 22:34:25 +0900

    refactor: Logo를 svg로 대체

--- fe/src/Header/UserButtons.jsx
+++ fe/src/Header/UserButtons.jsx
@@ -11,11 +11,17 @@ const mypage = 'Mypage.svg';
 const cart = 'Shopping-cart.svg';
 
 export const UserButtons = () => {
-  return(
+  return (
     <Root>
-      <button><img src={searchIcon} alt="검색"/></button>
-      <button><img src={mypage} alt="마이페이지"/></button>
-      <button><img src={cart} alt="장바구니"/></button>
+      <button>
+        <img src={searchIcon} alt="검색" />
+      </button>
+      <button>
+        <img src={mypage} alt="마이페이지" />
+      </button>
+      <button>
+        <img src={cart} alt="장바구니" />
+      </button>
     </Root>
   );
-}
+};
