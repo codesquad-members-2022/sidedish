e6d654b138db07b2ae29df9a84c3b468d2556ede
ju-kkim <wndus8198@naver.com>
2022-04-21 17:46:44 +0900

    fix: Header 렌더 수정 및 아이콘 추가

--- src/Components/header/GNB.jsx
+++ src/Components/header/GNB.jsx
@@ -1,35 +1,107 @@
 import React, { useState } from 'react';
-import ListItem from './ListItem';
 import styled from 'styled-components';
+import { F_basic, Color } from '../../Assets/CommonStyle';
+import List from '../List';
 
-const GnbList = styled.ul`
-  display: flex;
-  margin-left: 40px;
-`;
+const GNB_style = styled.nav`
+  margin: 0 40px;
 
-function Gnb() {
-  const categoryList = {
-    '든든한 메인요리': ['육류 요리', '해산물 요리'],
-    '뜨끈한 국물요리': ['국,탕,찌개'],
-    '정갈한 밑반찬': ['나물/무침', '조림/볶음', '절임/장아찌'],
-  };
-  const [category, setCategory] = useState(categoryList);
-  const template = [];
+  > ul {
+    ${F_basic}
 
-  for (let key in category) {
-    template.push(
-      <li key={key}>
-        <span>{key}</span>
-        {ListItem(category[key])}
-      </li>
-    );
+    > li {
+      position: relative;
+      padding: 0 12px;
+
+      &:hover > a {
+        font-weight: 500;
+        color: ${Color.orange};
+      }
+
+      ul {
+        visibility: hidden;
+        opacity: 0;
+        position: absolute;
+        top: 100%;
+        left: 0;
+        width: 100%;
+        padding: 16px 12px;
+
+        > li {
+          margin-bottom: 8px;
+
+          &:hover {
+            color: ${Color.orange};
+            text-decoration: underline;
+          }
+        }
+      }
+    }
   }
 
-  return (
-    <nav>
-      <GnbList>{template}</GnbList>
-    </nav>
-  );
-}
+  &:hover > ul ul {
+    transition: 0.3s;
+    opacity: 1;
+    visibility: visible;
+  }
 
-export default Gnb;
+  a {
+    display: block;
+  }
+`;
+
+const GNB = () => {
+  const GNB_LIST = [
+    {
+      id: 'main',
+      title: '든든한 메인요리',
+      menu: [
+        {
+          id: 'meat',
+          title: '육류 요리',
+        },
+        {
+          id: 'seafood',
+          title: '해산물 요리',
+        },
+      ],
+    },
+    {
+      id: 'soup',
+      title: '뜨끈한 국물요리',
+      menu: [
+        {
+          id: 'soup1',
+          title: '국,탕,찌개',
+        },
+      ],
+    },
+    {
+      id: 'side',
+      title: '정갈한 밑반찬',
+      menu: [
+        {
+          id: 'side1',
+          title: '나물/무침',
+        },
+        {
+          id: 'side2',
+          title: '조림/볶음',
+        },
+        {
+          id: 'side3',
+          title: '절임/장아찌',
+        },
+      ],
+    },
+  ];
+  const [GNB_MENU, setGNB_MENU] = useState(GNB_LIST);
+
+  return (
+    <GNB_style>
+      <List menu={GNB_MENU} />
+    </GNB_style>
+  );
+};
+
+export default GNB;
