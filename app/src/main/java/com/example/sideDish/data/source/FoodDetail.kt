50188d812207b0e236c578862f9b0c851d66d912
shim506 <shim506@naver.com>
2022-04-22 01:38:04 +0900

    feat: foodDetail 뷰모델 적용
    - 데이터 바인딩미적용 부분 수정
    - 임시데이터를 위해 레파지토리 함수 생성

---
+++ app/src/main/java/com/example/sideDish/data/source/FoodDetail.kt
@@ -0,0 +1,13 @@
+package com.example.sideDish.data.source
+
+class FoodDetail(
+    val deliveryFee: String,
+    val deliveryInfo: String,
+    val detailImageUrls: List<String>,
+    val point: String,
+    val originalPrice: Int,
+    val discountedPrice: Int,
+    val productDescription: String,
+    val thumbImageUrls: List<String>,
+    val topImageUrl: String
+)
