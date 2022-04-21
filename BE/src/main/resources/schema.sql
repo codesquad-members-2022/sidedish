8424d18e509e78ab85b7d2a9fa7e55cbbf6fb9c8
ikjo93 <auddlr100@naver.com>
2022-04-21 11:06:04 +0900

    etc : 데이터 베이스 데이터 초기화 작업 및 필드 속성 수정
    Co-authored-by: NB993 <qnalsfj@gmail.com>

--- BE/src/main/resources/schema.sql
+++ BE/src/main/resources/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE event
 (
     id   INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(32),
-    progress_flag TINYINT(1) NOT NULL DEFAULT 0 COMMENT '이벤트 진행 유무 : 진행 안함 0, 진행 중 1',
+    progress_flag TINYINT(1) DEFAULT 0 COMMENT '이벤트 진행 유무 : 진행 안함 0, 진행 중 1',
     PRIMARY KEY (id)
 );
 
@@ -27,8 +27,8 @@ CREATE TABLE sidedish
     event_category_id          INT,
     name                       VARCHAR(32),
     description                VARCHAR(64),
-    dawn_delivery_flag         TINYINT(1) NOT NULL DEFAULT 0 COMMENT '새벽배송 가능 유무 : 불가 0, 가능 : 1',
-    whole_nation_delivery_flag TINYINT(1) NOT NULL DEFAULT 0 COMMENT '전국 택배 가능 유무 : 불가 0, 가능 : 1',
+    dawn_delivery_flag         TINYINT(1) DEFAULT 0 COMMENT '새벽배송 가능 유무 : 불가 0, 가능 : 1',
+    whole_nation_delivery_flag TINYINT(1) DEFAULT 0 COMMENT '전국 택배 가능 유무 : 불가 0, 가능 : 1',
     created_datetime           TIMESTAMP,
     modified_datetime          TIMESTAMP,
     PRIMARY KEY (id),
@@ -50,13 +50,13 @@ CREATE TABLE sidedish_price
 
 CREATE TABLE image_file
 (
-    id                INT         NOT NULL AUTO_INCREMENT,
-    sidedish_id       INT         NOT NULL,
+    id                INT NOT NULL AUTO_INCREMENT,
+    sidedish_id       INT NOT NULL,
     file_name         VARCHAR(32),
     save_file_name    VARCHAR(32),
     content_type      VARCHAR(32) COMMENT 'image/png, image/jpg 등',
     image_type        VARCHAR(32) NOT NULL COMMENT 'ma : 메인 사진, etc : 기타 사진',
-    delete_flag       TINYINT(1) NOT NULL DEFAULT 0 COMMENT '삭제 유무 : 존재 0, 삭제 : 1',
+    delete_flag       TINYINT(1) DEFAULT 0 COMMENT '삭제 유무 : 존재 0, 삭제 : 1',
     created_datetime  TIMESTAMP,
     modified_datetime TIMESTAMP,
     PRIMARY KEY (id),
