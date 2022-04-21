149177401a1c9702447188dadb657681aaa3b930
Damagucci-Juice <jsr9874@gmail.com>
2022-04-21 15:30:57 +0900

    [#7] feat: SectionHeader.label 의 폰트를 SFProDisplay.32 로 조정

--- SideDish/SideDish/View/SectionHeader.swift
+++ SideDish/SideDish/View/SectionHeader.swift
@@ -17,7 +17,7 @@ class SectionHeader: UICollectionReusableView {
                     모두가 좋아하는
                     든든한 메인 요리
                     """
-        label.font = label.font.withSize(32)
+        label.font = UIFont.init(name: Font.sfRegular, size: 32)
         backgroundColor = .yellow
     }
 
