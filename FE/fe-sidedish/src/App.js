06105f715a3c10db262d427385029ad5ad4e49e4
Sangbeom Heo <mansaout@gmail.com>
2022-04-21 17:46:04 +0900

    Refactor: 기존 적용된 전역 베이스 스타일을 styled-components로 전환
    - index.js에서 ThemeProvider로 변수를 등록하고 GlobalStyle로 리셋 스타일을 적용

--- FE/fe-sidedish/src/App.js
+++ FE/fe-sidedish/src/App.js
@@ -1,4 +1,3 @@
-import "./styles/base/app.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./components/pages/Main";
 import NotFound from "./components/pages/NotFound";
---
+++ FE/fe-sidedish/src/styles/theme.jsx
@@ -0,0 +1,32 @@
+const color = {
+  black: "#1b1b1b",
+  grey1: "#3f3f3f",
+  grey2: "#777777",
+  grey3: "#bcbcbc",
+  offWhite: "#f8f7f7",
+  white: "#fff",
+  green: "#6dd028",
+  orange: "#ff8e14",
+};
+
+const fontSize = {
+  display: "2.5rem",
+  xLarge: "1.5rem",
+  large: "1.25rem",
+  medium: "1rem",
+  small: "0.875rem",
+  xSmall: "0.75rem",
+};
+
+const fontWeight = {
+  heavy: "900",
+  bold: "700",
+  mediumBold: "500",
+  regular: "400",
+};
+
+export const theme = {
+  color,
+  fontSize,
+  fontWeight,
+};
