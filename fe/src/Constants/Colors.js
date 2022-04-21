9e3493e0cdb8474872eb9b09f7909d6cdb347ac9
godhyeongman <aaaa4000@naver.com>
2022-04-21 11:32:12 +0900

    refactor: 컬러코드 컨벤션 토대로 수정

---
+++ fe/src/Constants/Colors.js
@@ -0,0 +1,11 @@
+export default {
+  DARK_GREY: '#3f3f3f',
+  GREY: '#777',
+  LIGHT_GREY: '#bcbcbc',
+  PALE_GREY: '#ebebeb',
+  OFF_WHITE: '#f8f7f7',
+  GREEN: '#6dd028',
+  ORANGE: '#ff8e14',
+  BLACK: '#1b1b1b',
+  WHITE: '#fff',
+};
