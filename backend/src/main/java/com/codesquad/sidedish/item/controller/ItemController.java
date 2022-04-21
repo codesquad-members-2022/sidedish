3e1dadcf08a1f0a76329dec274148ba56954292a
seyeong <seyoung7555@naver.com>
2022-04-21 17:02:33 +0900

    feat: 존재하지 않는 아이디로 조회한 경우 예외 처리 추가 (#17)
    - RuntimeException을 상속한 Custom Exception을 정의하였습니다.
    - Controller 계층에서 예외를 받아 404 상태코드를 전달합니다.

--- backend/src/main/java/com/codesquad/sidedish/item/controller/ItemController.java
+++ backend/src/main/java/com/codesquad/sidedish/item/controller/ItemController.java
@@ -2,13 +2,18 @@ package com.codesquad.sidedish.item.controller;
 
 import com.codesquad.sidedish.item.dto.CategoryItemsDto;
 import com.codesquad.sidedish.item.dto.DetailItemDto;
+import com.codesquad.sidedish.item.exception.ItemIdNotFoundException;
 import com.codesquad.sidedish.item.service.ItemService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ItemController {
+    private static final Logger log = LoggerFactory.getLogger(ItemController.class);
     private final ItemService itemService;
 
     public ItemController(ItemService itemService) {
@@ -16,12 +21,20 @@ public class ItemController {
     }
 
     @GetMapping("/items")
-    public CategoryItemsDto list() {
-        return itemService.findAll();
+    public ResponseEntity<CategoryItemsDto> list() {
+        CategoryItemsDto categoryItemsDto = itemService.findAll();
+        return ResponseEntity.ok(categoryItemsDto);
     }
 
     @GetMapping("/items/{id}")
-    public DetailItemDto detail(@PathVariable Integer id) {
-        return itemService.findById(id);
+    public ResponseEntity<DetailItemDto> detail(@PathVariable Integer id) {
+        DetailItemDto detailItemDto;
+        try {
+            detailItemDto = itemService.findById(id);
+        } catch (ItemIdNotFoundException e) {
+            log.debug("ItemController: {}", e.getMessage());
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(detailItemDto);
     }
 }
---
+++ backend/src/main/java/com/codesquad/sidedish/item/exception/ItemIdNotFoundException.java
@@ -0,0 +1,7 @@
+package com.codesquad.sidedish.item.exception;
+
+public class ItemIdNotFoundException extends RuntimeException {
+    public ItemIdNotFoundException(String message) {
+        super(message);
+    }
+}
