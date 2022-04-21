3cda1228907bc87db52cfc0afcd4f5193cbae7cf
happyGyu <happyboyto.dev@gmail.com>
2022-04-21 15:37:29 +0900

    Style: category component design #22

--- frontend/src/components/Category.js
+++ frontend/src/components/Category.js
@@ -1,7 +1,75 @@
-export const Category = ({category_id, data}) => {
+import { useState } from "react";
+import styled from "styled-components";
+import { ProductCard } from "./ProductCard";
 
-    useEffect(() => {
-        fetch1
-    })
-    return
-}
+const CategoryTitle = styled.span`
+  ${(props) =>
+    props.size === "large" &&
+    css`
+      display: none;
+    `}
+
+  ${(props) =>
+    props.size === "medium" &&
+    css`
+      line-height: 38px;
+      font-size: 24px;
+    `}
+
+    ${(props) =>
+    props.size === "small" &&
+    css`
+      line-height: 30px;
+      font-size: 20px;
+    `}
+`;
+
+const ProductCardWrapper = styled.div`
+  ${(props) =>
+    props.size === "large" &&
+    css`
+      width: 1281px;
+      height: 565px;
+    `}
+
+  ${(props) =>
+    props.size === "medium" &&
+    css`
+      width: 1280px;
+      height: 479px;
+    `}
+
+    ${(props) =>
+    props.size === "small" &&
+    css`
+      width: 864px;
+      height: 226px;
+    `}
+`;
+
+const SlideButtons = styled.div``;
+
+export const Category = ({ categoryID, categoryName, size }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchpProducts = async () => {
+      await (await fetch(`/api/products/${categoryID}`)).json();
+      setProducts(products.data);
+    };
+    fetchpProducts();
+  }, []);
+
+  return (
+    <>
+      <CategoryTitle size={size}>{categoryName}</CategoryTitle>
+      <ProductCardWrapper size={size}>
+        $
+        {products.map((productData) => (
+          <ProductCard size={size} {...productData}></ProductCard>
+        ))}
+      </ProductCardWrapper>
+      <SlideButtons size={size}></SlideButtons>
+    </>
+  );
+};
