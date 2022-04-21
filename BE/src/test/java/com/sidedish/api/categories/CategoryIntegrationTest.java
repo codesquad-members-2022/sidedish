51323e238735f61484facf7768cdcd40bf5f586a
jiwoo <zbqmgldjfh@gmail.com>
2022-04-21 17:37:47 +0900

    fix: CategoryIntegrationTest 로 이름변경

--- BE/src/test/java/com/sidedish/api/categories/CategoryIntegrationTest.java
+++ BE/src/test/java/com/sidedish/api/categories/CategoryIntegrationTest.java
@@ -36,7 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
-class CategoryControllerTest {
+class CategoryIntegrationTest {
 
     @Autowired
     MockMvc mockMvc;
