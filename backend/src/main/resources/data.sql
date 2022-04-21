ad65ab7c210c42627e14b61b5de78bbbf134a2fe
rkolx <olx.rko.o@gmail.com>
2022-04-21 17:03:00 +0900

    feat : DB테이블 작성
    초기 테이블 설계 및 작성

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+/*INSERT INTO product (title, description, price, stock, sale_type, main_category, event_category)
+VALUES
+    ('오리주물럭인디', '감칠맛 나는 매콤한 양념', 15600, 100, 'none', 'main', 'meat'),
+    ('냠냠뇸뇸', '감칠맛 나는 매콤한 양념', 15600, 100, 'none', 'main', 'meat');*/
