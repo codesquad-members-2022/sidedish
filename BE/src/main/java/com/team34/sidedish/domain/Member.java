7944f86b2d52a124f30ec32791c74ff1ed0bc49f
who-hoo <who.ho3ov@gmail.com>
2022-04-21 22:54:28 +0900

    feat: MemberRepository 작성
    - Spring Data JDBC 테스트를 위한 MemberRepository,Member, MemberRepositoryTest 작성했다.

--- BE/src/main/java/com/team34/sidedish/domain/Member.java
+++ BE/src/main/java/com/team34/sidedish/domain/Member.java
@@ -1,10 +1,13 @@
 package com.team34.sidedish.domain;
 
 import java.time.LocalDateTime;
+import org.springframework.data.annotation.Id;
 
 public class Member {
 
+    @Id
     private Long id;
+
     private String name;
     private String password;
     private LocalDateTime createdAt;
