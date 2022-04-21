4494cac7eaf36923f2ba85ec725c64115a32546e
youryu0212 <youryu0212@naver.com>
2022-04-21 16:54:35 +0900

    [fe#15] refactor: mock 데이터 분리

--- fe/src/helper/constants.js
+++ fe/src/helper/constants.js
@@ -24,3 +24,6 @@ export const icons = {
     </svg>
   ),
 };
+
+export const gnbMenus = ['든든한 메인요리', '뜨끈한 국물요리', '정갈한 밑반찬'];
+export const gnbSubMenus = [['육류 요리', '해산물 요리'], ['국/탕/찌개'], ['나물/무침', '조림/볶음', '절임/장아찌']];
