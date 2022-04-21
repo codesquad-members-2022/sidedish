e54e8432a057be9629e7afb2073301c7030750d0
Senglee Choi <tmdfl1027@gmail.com>
2022-04-21 17:18:24 +0900

    [#21] Feat: 테이블 구조 스키마 작성
    member, discount_policy, delivery, item, item_image, orders 테이블 생성 쿼리 작성.

--- BE/src/main/resources/schema.sql
+++ BE/src/main/resources/schema.sql
@@ -1,10 +1,72 @@
+drop table if exists orders cascade;
+drop table if exists delivery cascade;
+drop table if exists item_image cascade;
+drop table if exists item cascade;
 drop table if exists member cascade;
-create table member (
-    member_id bigint auto_increment,
+drop table if exists discount_policy cascade;
+
+create table member
+(
+    member_id   bigint auto_increment,
     member_name varchar(10) not null,
-    district varchar(10),
-    city varchar(10),
-    mileage integer,
+    district    varchar(10),
+    city        varchar(10),
+    mileage     integer,
     primary key (member_id)
 );
+
+create table discount_policy
+(
+    discount_policy_id bigint auto_increment,
+    name               varchar(10),
+    discount_rate      double,
+    primary key (discount_policy_id)
+);
+
+create table item
+(
+    item_id            bigint auto_increment,
+    name               varchar(10) not null,
+    description        varchar(255),
+    price              int,
+    discount_policy_id bigint,
+    item_section         varchar(10),
+    stock              int,
+    primary key (item_id),
+    CONSTRAINT item_discount_constraint FOREIGN KEY (discount_policy_id) REFERENCES discount_policy (discount_policy_id) on delete cascade on update cascade
+);
+
+create table delivery
+(
+    delivery_id bigint auto_increment,
+    fee int,
+    district    varchar(10),
+    city        varchar(10),
+    delivery_type varchar(10),
+    primary key (delivery_id)
+);
+
+create table orders
+(
+    order_id           bigint auto_increment,
+    item_id            bigint,
+    member_id          bigint,
+    delivery_id        bigint,
+    discount_policy_id bigint,
+    order_item_price   int,
+    item_count         int,
+    primary key (order_id),
+    CONSTRAINT orders_item_constraint FOREIGN KEY (item_id) REFERENCES item (item_id) on delete cascade on update cascade,
+    CONSTRAINT orders_member_constraint FOREIGN KEY (member_id) REFERENCES member (member_id) on delete cascade on update cascade,
+    CONSTRAINT orders_delivery_constraint FOREIGN KEY (delivery_id) REFERENCES delivery (delivery_id) on delete cascade on update cascade,
+    CONSTRAINT orders_discount_constraint FOREIGN KEY (discount_policy_id) REFERENCES discount_policy (discount_policy_id) on delete cascade on update cascade
+);
+
+create table item_image (
+    item_image_id bigint auto_increment,
+    item_id bigint,
+    item_image_sequence int,
+    primary key (item_image_id),
+    CONSTRAINT image_item_constraint FOREIGN KEY (item_id) REFERENCES item (item_id) on delete cascade on update cascade
+)
 
