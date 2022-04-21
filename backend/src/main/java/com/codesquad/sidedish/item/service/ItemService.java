875bd22d4231f608b8523b126181677f069dd041
seyeong <seyoung7555@naver.com>
2022-04-21 15:18:02 +0900

    refactor: Item 정보를 aggregate root인 category에서 관리하도록 개선 (#6)
    - Item 테이블이 아닌 Category 테이블을 통해서 관리하도록 개선하였습니다.

--- backend/src/main/java/com/codesquad/sidedish/item/service/ItemService.java
+++ backend/src/main/java/com/codesquad/sidedish/item/service/ItemService.java
@@ -1,36 +1,48 @@
 package com.codesquad.sidedish.item.service;
 
-import com.codesquad.sidedish.item.domain.DiscountPolicyRepository;
+import com.codesquad.sidedish.item.domain.Category;
+import com.codesquad.sidedish.item.domain.CategoryRepository;
 import com.codesquad.sidedish.item.domain.Item;
 import com.codesquad.sidedish.item.domain.ItemRepository;
+import com.codesquad.sidedish.item.dto.CategoryItemDto;
 import com.codesquad.sidedish.item.dto.CategoryItemsDto;
 import com.codesquad.sidedish.item.dto.DetailItemDto;
+import com.codesquad.sidedish.item.dto.ItemDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
 public class ItemService {
     private static final Logger log = LoggerFactory.getLogger(ItemService.class);
     private final ItemRepository itemRepository;
-    private final DiscountPolicyRepository discountPolicyRepository;
+    private final CategoryRepository categoryRepository;
 
-    public ItemService(ItemRepository itemRepository, DiscountPolicyRepository discountPolicyRepository) {
+    public ItemService(ItemRepository itemRepository, CategoryRepository categoryRepository) {
         this.itemRepository = itemRepository;
-        this.discountPolicyRepository = discountPolicyRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     public CategoryItemsDto findAll() {
-        Map<Integer, List<Item>> itemMap = itemRepository.findAll().stream()
-                .collect(Collectors.groupingBy(Item::getCategoryId));
-        return null;
+        List<CategoryItemDto> categoryItemDtoList = new ArrayList<>();
+        List<Category> categoryList = categoryRepository.findAll();
+
+        for (Category category : categoryList) {
+            String categoryName = category.getName();
+            Set<Item> items = category.getItems();
+
+            List<ItemDto> itemDtoList = items.stream().map(ItemDto::from).collect(Collectors.toList());
+            categoryItemDtoList.add(new CategoryItemDto(categoryName, itemDtoList));
+        }
+        return new CategoryItemsDto(categoryItemDtoList);
     }
 
-    public DetailItemDto findById(Integer itemId) {
+    public DetailItemDto findById(Integer id) {
         return null;
     }
 }
