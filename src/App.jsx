82520082f13c7f52d5b8418e81f2ca2f4aba6f81
yongseongjeon <yongseong.jeon95@gmail.com>
2022-04-21 14:16:12 +0900

    ✨ t를 눌러서 Template 컴포넌트 토글로 랜더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/no-noninteractive-tabindex */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable no-console */
+import React, { useState } from "react";
+import Sidedish from "./Sidedish";
+import Template from "./template/Template";
 
 export default function App() {
-  return <div className="App" />;
+  const [isTemplateMode, toggleTemplateMode] = useState(true);
+  function handleKeyDown(e) {
+    if (e.key === "t") {
+      toggleTemplateMode(!isTemplateMode);
+    }
+  }
+  return (
+    <div tabIndex="0" className="App" onKeyDown={(e) => handleKeyDown(e)}>
+      {isTemplateMode ? <Template /> : <Sidedish />}
+    </div>
+  );
 }
