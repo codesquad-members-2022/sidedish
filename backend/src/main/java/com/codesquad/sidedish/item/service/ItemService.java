42de462b727130a4601f0815d17ddb6955f4606a
seyeong <seyoung7555@naver.com>
2022-04-21 11:19:45 +0900

    feat: Item 관련 요청을 처리할 서비스 계층 기능 구현 중 (#6)

--- backend/src/main/java/com/codesquad/sidedish/item/service/ItemService.java
+++ backend/src/main/java/com/codesquad/sidedish/item/service/ItemService.java
@@ -1,13 +1,32 @@
 package com.codesquad.sidedish.item.service;
 
+import com.codesquad.sidedish.item.domain.DiscountPolicyRepository;
+import com.codesquad.sidedish.item.domain.Item;
+import com.codesquad.sidedish.item.domain.ItemRepository;
 import com.codesquad.sidedish.item.dto.CategoryItemsDto;
 import com.codesquad.sidedish.item.dto.DetailItemDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class ItemService {
+    private static final Logger log = LoggerFactory.getLogger(ItemService.class);
+    private final ItemRepository itemRepository;
+    private final DiscountPolicyRepository discountPolicyRepository;
+
+    public ItemService(ItemRepository itemRepository, DiscountPolicyRepository discountPolicyRepository) {
+        this.itemRepository = itemRepository;
+        this.discountPolicyRepository = discountPolicyRepository;
+    }
 
     public CategoryItemsDto findAll() {
+        Map<Integer, List<Item>> itemMap = itemRepository.findAll().stream()
+                .collect(Collectors.groupingBy(Item::getCategoryId));
         return null;
     }
 
