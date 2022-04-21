d9ed9428896d62f29c29f103ae9214f62b6e3f9c
godhyeongman <aaaa4000@naver.com>
2022-04-21 15:09:28 +0900

    fix: menu.jsx 병합충돌 해결

--- fe/src/Header/Menu.jsx
+++ fe/src/Header/Menu.jsx
@@ -1,27 +1,12 @@
 import styled from 'styled-components';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Colors from '../Constants/Colors';
-=======
-import COLOR from '../Constants/Constants';
-import FONT from '../test/test';
->>>>>>> 4ebb429 (feat: 카드 컴포넌트 구현)
-=======
-import COLOR from '../Constants/Colors';
->>>>>>> aef6423 (fix: 컴포넌트 img태그 수정)
 
 const MenuWrapper = styled.li`
   margin-right: 24px;
 `;
 
 const SubMenuList = styled.ul`
-<<<<<<< HEAD
   margin: 16px 0;
-=======
-  display: flex
-
-  margin-top: 16px;
->>>>>>> 4ebb429 (feat: 카드 컴포넌트 구현)
 
   flex-direction: column;
 
@@ -31,11 +16,7 @@ const SubMenuList = styled.ul`
 
   li:hover {
     text-decoration: underline;
-<<<<<<< HEAD
     color: ${Colors.GREY};
-=======
-    color: ${COLOR.GREY};
->>>>>>> 4ebb429 (feat: 카드 컴포넌트 구현)
   }
 `;
 
