c4097fec8f05b846c56c475c39d34747cea45bfb
asqw887 <asqw887@gmail.com>
2022-04-21 10:59:11 +0900

    REFACTOR: UILabel 강제언래핑 클로저로 변경

--- sideDish/sideDish/CollectionHeaderView.swift
+++ sideDish/sideDish/CollectionHeaderView.swift
@@ -9,7 +9,14 @@ import UIKit
 
 class CollectionHeaderView: UICollectionReusableView {
     
-    private var title: UILabel!
+    private var title: UILabel = {
+        let tempTitle = UILabel()
+        tempTitle.text = "모두가 좋아하는 든든한 메인요리"
+        tempTitle.textColor = UIColor(red: 0.31, green: 0.31, blue: 0.31, alpha: 1)
+        tempTitle.font = UIFont.systemFont(ofSize: 32)
+        tempTitle.numberOfLines = 2
+        return tempTitle
+    }()
     
     override init(frame: CGRect){
         super.init(frame: frame)
@@ -22,13 +29,14 @@ class CollectionHeaderView: UICollectionReusableView {
     }
     
     func configureLayout(){
-        title = UILabel(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height))
-        title.text = "모두가 좋아하는 든든한 메인요리"
-        title.textColor = UIColor(red: 0.31, green: 0.31, blue: 0.31, alpha: 1)
-        title.font = UIFont.systemFont(ofSize: 32)
-        title.numberOfLines = 2
-        self.addSubview(title)        
+        self.addSubview(title)
+        title.translatesAutoresizingMaskIntoConstraints = false
+        title.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        title.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        title.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        title.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
     }
+    
     
     func setTitle(text: String){
         title.text = text
