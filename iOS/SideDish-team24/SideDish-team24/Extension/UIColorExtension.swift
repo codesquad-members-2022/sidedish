641d4dc924115d53b6cfb0197acc6457fb1f69e9
sungju-kim <wnwn0719@gmail.com>
2022-04-21 11:22:23 +0900

    [#20] Refactor: UIColor - init 수정

--- iOS/SideDish-team24/SideDish-team24/Extension/UIColorExtension.swift
+++ iOS/SideDish-team24/SideDish-team24/Extension/UIColorExtension.swift
@@ -9,12 +9,12 @@ extension UIColor {
     static let dishBlue = UIColor(rgb: 0x0066D6)
     static let dishSkyBlue = UIColor(rgb: 0x80BCFF)
     
-    convenience init(red: Int, green: Int, blue: Int) {
+    convenience init(red: Int, green: Int, blue: Int, alpha: CGFloat = 1.0) {
         assert(red >= 0 && red <= 255, "Invalid red component")
         assert(green >= 0 && green <= 255, "Invalid green component")
         assert(blue >= 0 && blue <= 255, "Invalid blue component")
         
-        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: alpha)
     }
     
     convenience init(rgb: Int) {
