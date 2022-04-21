4a90689b9971110b6d073e84874b71452596b3d6
happyGyu <happyboyto.dev@gmail.com>
2022-04-22 04:56:27 +0900

    Design: discount tag design #22

--- frontend/src/components/DiscountTag.js
+++ frontend/src/components/DiscountTag.js
@@ -2,28 +2,21 @@ import styled from "styled-components";
 import { tagConvetion } from "../convention";
 
 export const TagItem = styled.li`
+  margin-right: 8px;
   padding: 6px 16px;
-  ${({ theme, id }) =>
-    theme.custom_flex("row", "flex-start") +
-    theme.custom_static(0, null, null, 0) +
-    theme.flex_none(tagConvetion[id].order, 0, 8)}
-  height: 30px;
   border-radius: 999px;
-  background: ${({ id, theme }) => theme.colors[tagConvetion[id].color]} span {
+  ${({ theme, id }) => theme.custom_flex("row", "flex-start")}
+  background-color: ${({ id, theme }) => theme.colors[tagConvetion[id].color]};
+  span {
     text-align: center;
     ${({ theme }) =>
       theme.custom_font("Noto Sans KR", 12, 500, 18, -0.008) +
-      `color:${theme.colors.White}` +
-      theme.flex_none(0, 0, 10)}
+      `color:${theme.colors.White}`}
   }
 `;
 export const TagWrapper = styled.ul`
-  padding: 0px;
-  ${({ theme }) =>
-    theme.custom_flex("row", "flex-start") +
-    theme.custom_static(535) +
-    theme.flex_none(2, 16, 0)}
-  height: 30px;
+  margin-top: 16px;
+  ${({ theme }) => theme.custom_flex("row", "flex-start")}
 `;
 
 export const DiscountTag = ({ discount }) => {
