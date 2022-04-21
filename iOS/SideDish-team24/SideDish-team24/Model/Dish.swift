fb7502dbb9f622755e1868b1c9624e771c884478
sungju-kim <wnwn0719@gmail.com>
2022-04-21 11:25:34 +0900

    [#20] Refactor: Dish init 초기값 설정

--- iOS/SideDish-team24/SideDish-team24/Model/Dish.swift
+++ iOS/SideDish-team24/SideDish-team24/Model/Dish.swift
@@ -12,19 +12,7 @@ class Dish {
     private let discountPrice : Int?
     private let discountPolicy : String?
     
-    init(id: String, name: String, price: Int, stock: Int, type: String, image: URL, description: String) {
-        self.id = id
-        self.name = name
-        self.price = price
-        self.stock = stock
-        self.type = type
-        self.image = image
-        self.description = description
-        self.discountPrice = nil
-        self.discountPolicy = nil
-    }
-    
-    init(id: String, name: String, price: Int, stock: Int, type: String, image: URL, description: String, discountPrice: Int, discountPolicy: String) {
+    init(id: String, name: String, price: Int, stock: Int, type: String, image: URL, description: String, discountPrice : Int?, discountPolicy : String?) {
         self.id = id
         self.name = name
         self.price = price
@@ -35,4 +23,5 @@ class Dish {
         self.discountPrice = discountPrice
         self.discountPolicy = discountPolicy
     }
+    
 }
