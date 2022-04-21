2cf795bed83e3597921f25a76592c7601856576c
함석호 <kalejuice02@gmail.com>
2022-04-21 21:55:46 +0900

    feat : subCategory 테이블과 도메인 클래스 작성

--- be/src/main/java/sidedish/domain/category/MainCategory.java
+++ be/src/main/java/sidedish/domain/category/MainCategory.java
@@ -3,12 +3,14 @@ package sidedish.domain.category;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class MainCategory {
 
+	@Id
 	private Long id;
 	private String title;
 }
