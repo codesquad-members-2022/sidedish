54584f5a4a5628cf9919e4e4c2e444c2ec662a25
LeeJoobang <hii5074@gmail.com>
2022-04-21 15:31:38 +0900

    [#4] Feat: CollectionView 속성 추가
    - Navigation Controller 연결

--- SideDish/SideDish/Controllers/MainViewController.swift
+++ SideDish/SideDish/Controllers/MainViewController.swift
@@ -3,9 +3,18 @@ import UIKit
 class MainViewController: UIViewController {
     
     private let ordering = Ordering()
+    private lazy var foodCollectionView: UICollectionView = {
+       let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical // 수직으로 표현된다.
+
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        return collectionView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.title = "Ordering"
+        
     }
 }
 
