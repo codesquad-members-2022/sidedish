c8d49f2d64c718ead6c060156fb8283e3e7f5ad6
nori <nohriter@gmail.com>
2022-04-21 14:42:27 +0900

    Chore: 프로젝트 초기 설정 - nohriter-#6
    - data-jdbc 데이터베이스 연결 후 주석 제거 예정
    - mysql-connector (mysql 을 위한 드라이버)

---
+++ backend/src/main/java/team31/codesuqad/sidedish/SidedishApplication.java
@@ -0,0 +1,13 @@
+package team31.codesuqad.sidedish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidedishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidedishApplication.class, args);
+	}
+
+}
