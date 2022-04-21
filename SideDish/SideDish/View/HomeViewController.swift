25c6efaba8bd5a2d04b42351143e11b34e772252
Damagucci-Juice <jsr9874@gmail.com>
2022-04-21 10:27:37 +0900

    [#7] feat: dev 브랜치에서 verticalCell을 따와 수직으로 셀 작업

--- SideDish/SideDish/View/HomeViewController.swift
+++ SideDish/SideDish/View/HomeViewController.swift
@@ -14,6 +14,7 @@ extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelega
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 3
     }
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DishCell", for: indexPath)
         return cell
@@ -21,7 +22,7 @@ extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelega
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 3
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         switch kind {
         case UICollectionView.elementKindSectionHeader:
@@ -31,5 +32,23 @@ extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelega
         default:
             assert(false, "invalid element Type")
         }
+    }
+}
+
+// MARK: - 행 당 셀의 개수, 셀의 크기
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numberOfCellPerRow = 1
+        guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else {
+            return CGSize(width: 100, height: 100)
+        }
+        let totalSpace = flowLayout.sectionInset.left
+                        + flowLayout.sectionInset.right
+                        + (flowLayout.minimumInteritemSpacing * CGFloat(numberOfCellPerRow - 1))
+        let size = Int((collectionView.bounds.width - totalSpace)
+                       / CGFloat(numberOfCellPerRow))
+        return CGSize(width: size, height: size)
     }
 }
