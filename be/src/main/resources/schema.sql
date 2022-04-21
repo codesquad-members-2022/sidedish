2ef309d8115b057f28af675c9f49a3ff57906bf2
함석호 <kalejuice02@gmail.com>
2022-04-21 14:41:24 +0900

    refactor : application.properties에 mysql 관련 설정 추가 및 schema 문법 오류 수정

--- be/src/main/resources/schema.sql
+++ be/src/main/resources/schema.sql
@@ -1,26 +1,26 @@
 create table main_category(
     id bigint not null AUTO_INCREMENT PRIMARY KEY,
     title varchar(255) not null
-)
+);
 create table sub_category(
     id bigint not null AUTO_INCREMENT PRIMARY KEY,
     main_category_id bigint not null,
     title varchar(255) not null,
     FOREIGN KEY(main_category_id) references main_category(id)
-)
+);
 create table dish(
     id bigint not null AUTO_INCREMENT PRIMARY KEY,
     title varchar(50) NOT NULL,
-    description varchar NOT NULL,
+    description varchar(60) NOT NULL,
     price int NOT NULL,
     quantity int NOT NULL,
-    discount_policy varchar,
-    morning_delivery boolean NOT NULL,
-)
+    discount_policy varchar(60),
+    morning_delivery boolean NOT NULL
+);
 create table dish_category(
     id bigint not null AUTO_INCREMENT PRIMARY KEY,
     dish_id bigint NOT NULL,
     category_id bigint NOT NULL,
     FOREIGN KEY(dish_id) references dish(id),
     FOREIGN KEY(category_id) references sub_category(id)
-)
+);
