75c2bcf1d8b6a3ddf18a77297c5733ec22327fb6
YUNHO <kimyouknow@naver.com>
2022-04-21 17:36:58 +0900

    [FE] MealContainer 레이아웃 작성 (#16)
    * Chore: vscode debugger .gitingore에추가
    
    * Chore: axios 라이브러리 추가
    
    * Feat: MealContainer
    
    - MealHeader
    - Carousel -> MealCard여러개
    
    * Refactor: useMemo 삭제
    
    * Chore: TODO 주석 작성
    
    - 컴포넌트 분리
    - stlye 코드 분리
    - status 코드

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,17 +1,44 @@
+import axios from "axios";
+import React, { useCallback, useEffect, useState } from "react";
+import MealContainer from "./MealContainer";
+
+// TODO: contant로 빼기
+const MOCK_SERVER_URL = "https://c8611651-19e0-4ab7-9b27-51a7c3fe2fa6.mock.pstmn.io/"; // TODO: .env파일로 빼기
+
 function App() {
+	const [meals, setMeals] = useState({
+		mealHeader: "",
+		mealCards: [],
+	});
+	const fetchCategoryMeal = useCallback(async () => {
+		try {
+			const { status, data } = await axios.get(`${MOCK_SERVER_URL}/api/products?meal=main`);
+			// TODO: status 코드 300번 구간도 포함해야하나 공부해보기
+			if (status >= 400) {
+				throw Error(`${status}: 데이터를 받아오는 도중 에러 발생`);
+			}
+
+			setMeals({
+				mealHeader: "식탁을 풍성하게 하는 정갈한 밑반찬",
+				mealCards: data,
+			});
+		} catch (error) {
+			console.error(error);
+		}
+	}, []);
+
+	useEffect(() => {
+		fetchCategoryMeal();
+	}, []);
+
 	return (
 		<>
 			<div className="App">
-				<header></header>
 				<main>
 					<div className="Best">
 						<header className="Best__header"></header>
 					</div>
-					<div className="CategoryContainer">
-						<div className="Category"></div>
-						<div className="Category"></div>
-						<div className="Category"></div>
-					</div>
+					<MealContainer meals={meals} />
 				</main>
 			</div>
 			<div className="Modal"></div>
