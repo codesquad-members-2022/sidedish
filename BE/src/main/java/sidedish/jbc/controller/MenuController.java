dd202106299fbe6dc2e7babf658b5530a244f199
honeySleepr <gruzzimo@naver.com>
2022-04-22 12:15:24 +0900

    [BE] feat: Swagger 연동하여 API 문서화

--- BE/src/main/java/sidedish/jbc/controller/MenuController.java
+++ BE/src/main/java/sidedish/jbc/controller/MenuController.java
@@ -1,5 +1,8 @@
 package sidedish.jbc.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +17,7 @@ import sidedish.jbc.dto.OrderRequest;
 import sidedish.jbc.service.MenuService;
 import sidedish.jbc.service.OrderService;
 
+@Api(tags = "Sidedish project API")
 @Controller
 @RequestMapping("/menus")
 public class MenuController {
@@ -26,26 +30,33 @@ public class MenuController {
 		this.orderService = orderService;
 	}
 
+	@ApiOperation(value = "전체 메뉴 업데이트", notes = "모든 메뉴를 불러옵니다")
 	@GetMapping
 	@ResponseBody
 	public List<MenuResponse> welcome() {
 		return menuService.findAll();
 	}
 
+	@ApiOperation(value = "특정 메뉴 타입 요청", notes = "지정된 타입의 메뉴들을 불러옵니다")
 	@GetMapping("/{type}")
 	@ResponseBody
-	public List<MenuResponse> findMenu(@PathVariable("type") String type) {
+	public List<MenuResponse> findMenu(
+		@ApiParam(name = "type", value = "요청할 메뉴 타입(예: soup)", required = true) @PathVariable("type") String type) {
 		return menuService.findMenu(type);
 	}
 
+	@ApiOperation(value = "상세 화면 업데이트", notes = "지정된 id의 메뉴 상세정보를 불러옵니다.")
 	@GetMapping("/detail/{id}")
 	@ResponseBody
-	public DetailMenuResponse findDetailMenu(@PathVariable("id") int menuId) {
+	public DetailMenuResponse findDetailMenu(
+		@ApiParam(name = "id", value = "상세 정보를 요청할 menu_id", required = true) @PathVariable("id") int menuId) {
 		return menuService.findDetailMenu(menuId);
 	}
 
+	@ApiOperation(value = "메뉴 주문", notes = "지정된 id의 메뉴를 주문합니다.")
 	@PostMapping("/detail/{id}")
-	public String makeOrder(@RequestBody OrderRequest orderRequest) {
+	public String makeOrder(
+		@RequestBody @ApiParam(name = "id", value = "상세 정보를 요청할 menu_id", required = true) OrderRequest orderRequest) {
 		orderService.save(orderRequest);
 		return "redirect:/menus/detail/{id}";
 	}
