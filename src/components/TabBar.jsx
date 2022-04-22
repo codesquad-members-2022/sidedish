de68742e793166258e9373cfcf303a741d2bbced
yongseongjeon <yongseong.jeon95@gmail.com>
2022-04-22 11:43:44 +0900

    ♻️ TabBar의 map 내부에 구현된 로직 함수로 분리
    - getTabButton 함수 생성
    - boolean 값으로 if 분기처리하던 것에서 isSelected에 바로 삽입

--- src/components/TabBar.jsx
+++ src/components/TabBar.jsx
@@ -4,23 +4,19 @@ import TabButton from './TabButton';
 
 export default function TabBar(props) {
   const { tabNames } = props;
-  return (
-    <Wrap>
-      {tabNames.map((tabName, i) => {
-        if (i === 0) {
-          return <TabButton value={tabName} isSelected />;
-        }
-        return <TabButton value={tabName} />;
-      })}
-    </Wrap>
-  );
+  return <Wrap>{tabNames.map(getTabButton)}</Wrap>;
 }
 
 TabBar.defaultProps = {
-  tabNames: [],
+  tabNames: []
 };
+
+function getTabButton(tabName, i) {
+  const isFirstTabButton = i === 0;
+  return <TabButton value={tabName} isSelected={isFirstTabButton} />;
+}
 
 const Wrap = styled.div({
   display: 'flex',
-  gap: '32px',
+  gap: '32px'
 });
