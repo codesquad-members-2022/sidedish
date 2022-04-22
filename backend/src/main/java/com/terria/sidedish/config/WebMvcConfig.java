ab9224f9fc1291c9de122bccf7ab32f5d3af0364
naneun <rmtlrksdp@naver.com>
2022-04-22 13:03:02 +0900

    [#21] fix: 서버 측 문제가 발생하여 원복
    - 미해결

--- backend/src/main/java/com/terria/sidedish/config/WebMvcConfig.java
+++ backend/src/main/java/com/terria/sidedish/config/WebMvcConfig.java
@@ -1,10 +1,7 @@
 package com.terria.sidedish.config;
 
-import com.terria.sidedish.interceptor.CorsInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.HandlerTypePredicate;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,15 +11,5 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix("/api", HandlerTypePredicate.forBasePackage("com.terria.sidedish.api"));
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:8080/");
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new CorsInterceptor()).addPathPatterns("/**");
     }
 }
