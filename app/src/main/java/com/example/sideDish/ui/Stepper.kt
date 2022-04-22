e85f9ef9ca8fdb281c2e0418619d6d61c2460cdc
shim506 <shim506@naver.com>
2022-04-22 12:33:40 +0900

    feat: stepperEvent 처리
    - customView 에 대한 데이터바인딩을 못했고 twoWay bindging 을 위해 viewModel 을 DetailFragment 의 변수로 넘겨주었다.

--- app/src/main/java/com/example/sideDish/ui/Stepper.kt
+++ app/src/main/java/com/example/sideDish/ui/Stepper.kt
@@ -7,17 +7,31 @@ import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.example.sideDish.R
+import com.example.sideDish.ui.productdetail.FoodDetailViewModel
 
 class Stepper(context: Context, attrs: AttributeSet) :
     LinearLayout(context, attrs) {
     lateinit var plus: ImageButton
     lateinit var value: TextView
     lateinit var minus: ImageButton
+    var viewModel: FoodDetailViewModel? = null
+    var count: Int = 0
 
     init {
         LayoutInflater.from(context).inflate(R.layout.stepper, this, true)
         plus = findViewById(R.id.button_stepper_plus)
         value = findViewById(R.id.text_view_stepper_value)
         minus = findViewById(R.id.button_stepper_minus)
+
+        value.text = count.toString()
+
+        plus.setOnClickListener {
+            viewModel?.orderCount?.value = viewModel?.orderCount?.value?.plus(1)
+        }
+        minus.setOnClickListener {
+            viewModel?.orderCount?.value = viewModel?.orderCount?.value?.minus(1)
+        }
     }
+
+
 }
--- app/src/main/java/com/example/sideDish/ui/productdetail/FoodDetailViewModel.kt
+++ app/src/main/java/com/example/sideDish/ui/productdetail/FoodDetailViewModel.kt
@@ -6,14 +6,15 @@ import androidx.lifecycle.ViewModel
 import com.example.sideDish.data.source.FoodDetail
 import com.example.sideDish.data.source.FoodRepository
 
-class FoodDetailViewModel( private val repository: FoodRepository , hash: String) : ViewModel() {
+class FoodDetailViewModel(private val repository: FoodRepository, hash: String) : ViewModel() {
     private val _detail = MutableLiveData<FoodDetail>()
     val detail: LiveData<FoodDetail> = _detail
+
+    val orderCount = MutableLiveData<Int>(0)
 
     init {
         getDetail(hash)
     }
-
     fun getDetail(hash: String) {
         _detail.value = repository.getFoodDetail(hash)
     }
