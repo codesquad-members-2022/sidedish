433a7483512d16437a033705ac5d9bd2a28151cd
leejohy-0223 <leejohy@naver.com>
2022-04-22 11:06:49 +0900

    feat: User dummy data 추가 (#21)

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,6 @@ INSERT INTO ITEM (CATEGORY_ID, NAME, DESCRIPTION, PRICE, STOCK, MAIN_IMAGE_LINK)
 (3, 'item6', 'des2', 1000, 1000, "image"),
 (3, 'item7', 'des3', 1000, 1000, "image");
 
+INSERT INTO USER (USERNAME, PASSWORD) VALUES
+('santory', '1111'),
+('lucid', '2222');
