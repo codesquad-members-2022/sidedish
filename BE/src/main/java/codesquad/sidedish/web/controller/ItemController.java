170f9dc878cb4f0941bbce279d9f1b550b4e46e0
ttasjwi <ttasjwi920@gmail.com>
2022-04-22 11:23:59 +0900

    [#35] Refactor: 상품의 섹션을 쿼리 파라미터로 필터링해서 가져오도록 함
    - "/items?section=xxxx"으로 상품목록을 가져옴
    - 변경시의 장점 : items 전체에서 해당 조건에 맞는 자원들을 필터링해서 가져오는 의미가 강화됨
    - 결국 상품들을 가져오는건데 다른 url쪽에서 가져오는 것보다는 더 의미가 분명해짐

--- BE/src/main/java/codesquad/sidedish/web/controller/ItemController.java
+++ BE/src/main/java/codesquad/sidedish/web/controller/ItemController.java
@@ -1,10 +1,11 @@
 package codesquad.sidedish.web.controller;
 
 import codesquad.sidedish.web.dto.ItemDetailResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import codesquad.sidedish.web.dto.ItemListElement;
+import codesquad.sidedish.web.dto.ItemListResponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/items")
@@ -13,5 +14,12 @@ public class ItemController {
     @GetMapping("/{itemId}")
     public ItemDetailResponse itemDetail(@PathVariable Long itemId) {
         return new ItemDetailResponse();
+    }
+
+    @GetMapping
+    public ItemListResponse loadItems(@RequestParam String section) {
+        ItemListElement sampleElement = new ItemListElement();
+        List<ItemListElement> items = List.of(sampleElement);
+        return new ItemListResponse(items);
     }
 }
