b86ecc07d4ee0d3fef7cf89671e6e4ca818fcd82
soralee <sora2821@gmail.com>
2022-04-22 13:49:42 +0900

    [#13] Chore: babel transform-runtime-plugin 설치, 설정

--- FE/babel.config.js
+++ FE/babel.config.js
@@ -10,5 +10,9 @@ module.exports = {
   ],
 =======
   presets: ["@babel/preset-env", "@babel/preset-react"],
+<<<<<<< HEAD
 >>>>>>> ccf5e4b ([#6] Chore: CRA 없이 React 빌드 환경 구축)
+=======
+  plugins: ["@babel/plugin-transform-runtime"],
+>>>>>>> 6a903a6 ([#13] Chore: babel transform-runtime-plugin 설치, 설정)
 };
