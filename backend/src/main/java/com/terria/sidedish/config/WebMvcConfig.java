b4676a3410b5f6c36a5d9fa6845deafac30f110b
naneun <rmtlrksdp@naver.com>
2022-04-22 13:31:07 +0900

    [#21] Interceptor 을 사용하여 CORS 에러 문제 해결

--- backend/src/main/java/com/terria/sidedish/config/WebMvcConfig.java
+++ backend/src/main/java/com/terria/sidedish/config/WebMvcConfig.java
@@ -1,7 +1,9 @@
 package com.terria.sidedish.config;
 
+import com.terria.sidedish.interceptor.CorsInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +13,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix("/api", HandlerTypePredicate.forBasePackage("com.terria.sidedish.api"));
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new CorsInterceptor()).addPathPatterns("/**");
     }
 }
