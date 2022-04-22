a9b58cff09e2fa3dfcba07a489c54099189723c5
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-04-23 00:59:55 +0900

    [#4] Chore: MainViewController에서 분기된 개별 함수 모두 호출

--- SideDish/SideDish/Controllers/MainViewController.swift
+++ SideDish/SideDish/Controllers/MainViewController.swift
@@ -5,7 +5,7 @@ class MainViewController: UIViewController {
     private let ordering = Ordering()
     
     private lazy var foodCollectionView: UICollectionView = {
-       let layout = UICollectionViewFlowLayout()
+        let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical // 수직으로 표현된다.
         layout.itemSize = CGSize(width: view.frame.width * 0.85, height: 150)
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
@@ -13,12 +13,15 @@ class MainViewController: UIViewController {
         collectionView.register(FoodCollectionViewCell.self, forCellWithReuseIdentifier: "FoodCollectionViewCell")
         
         collectionView.translatesAutoresizingMaskIntoConstraints = false
-            return collectionView
+        return collectionView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Ordering"
+        initializeOrdering()
+        setFoodCollectionView()
+        setLayout()
     }
     
     private func initializeOrdering(){
