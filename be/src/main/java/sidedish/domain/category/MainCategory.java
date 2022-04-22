dc80390b1d217a167509ad266411f711de67bfc1
함석호 <kalejuice02@gmail.com>
2022-04-22 11:25:57 +0900

    feat : Category 관련 클래스 추가 및 카테고리 목록 조회 기능 구현
    - Controller, Service, Repository 작성

--- be/src/main/java/sidedish/domain/category/MainCategory.java
+++ be/src/main/java/sidedish/domain/category/MainCategory.java
@@ -4,13 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.MappedCollection;
+
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class MainCategory {
 
-	@Id
-	private Long id;
-	private String title;
+    @Id
+    private Long id;
+    private String title;
+    @MappedCollection(idColumn = "main_category_id", keyColumn = "id")
+    private List<SubCategory> subCategories;
 }
