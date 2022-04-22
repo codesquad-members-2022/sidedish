1ca315fcfe3570bc083f7520ea6ea398e4a22230
ramkid91 <ramkid91@gmail.com>
2022-04-22 11:09:06 +0900

    feat: Modify detail badge logic

--- app/src/main/java/com/example/todo/sidedish/ui/menu/MenuFragment.kt
+++ app/src/main/java/com/example/todo/sidedish/ui/menu/MenuFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
