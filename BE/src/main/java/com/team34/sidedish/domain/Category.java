f7b8a5cb22566b450378b7efc49621681e4e61d8
nak253 <nwon4413@naver.com>
2022-04-22 14:22:41 +0900

    feat: Category클래스 Entity 관계 Mapping작업
    - 필드에 @MappedCollection 어노테이션 추가
    - 매개변수를 갖는 생성자 선언
    - getter 추가

--- BE/src/main/java/com/team34/sidedish/domain/Category.java
+++ BE/src/main/java/com/team34/sidedish/domain/Category.java
@@ -1,12 +1,57 @@
 package com.team34.sidedish.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.MappedCollection;
 
 public class Category {
 
+    @Id
     private Long id;
+
     private String name;
     private boolean isEvent;
-    private Category parent;
-    private List<Category> children;
+    private Long parentCategoryId;
+
+    @MappedCollection(idColumn = "parent_category_id", keyColumn = "id")
+    private List<Category> children = new ArrayList<>();
+
+    public Category(Long id, String name, boolean isEvent, Long parentCategoryId,
+        List<Category> children) {
+        this.id = id;
+        this.name = name;
+        this.isEvent = isEvent;
+        this.parentCategoryId = parentCategoryId;
+        this.children = children;
+    }
+
+    public void addCategory(Long id, String name, boolean isEvent, Long parentCategoryId,
+        List<Category> children) {
+        children.add(new Category(id, name, isEvent, parentCategoryId, children));
+    }
+
+    public void clearCategory() {
+        children.clear();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isEvent() {
+        return isEvent;
+    }
+
+    public Long getParentCategoryId() {
+        return parentCategoryId;
+    }
+
+    public List<Category> getChildren() {
+        return children;
+    }
 }
