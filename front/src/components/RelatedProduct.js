97e4599ff7d1de4e6e43eec0d47db575ba3ffe8d
SeungHyun <fm10033@gmail.com>
2022-04-23 21:12:42 +0900

    타이틀 구현, json 파싱 오류 발생

--- front/src/components/RelatedProduct.js
+++ front/src/components/RelatedProduct.js
@@ -10,16 +10,25 @@ const RelatedProductWrap = styled.div`
   flex-direction: row;
   justify-content: center;
 `;
-const TitleAndButtons = styled.div`
-  width: 598px;
+const TitleAndNavButtons = styled.div`
+  /* width: 100%; */
   display: flex;
   justify-content: space-between;
   border: 1px solid black;
 `;
+const Title = styled.div`
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 30px;
+`;
+const RelatedProductSlide = styled.div``;
 function RelatedProduct(relatedDishes) {
   return (
     <RelatedProductWrap>
-      <TitleAndButtons></TitleAndButtons>
+      <TitleAndNavButtons>
+        <Title>함께하면 더욱 맛있는 상품</Title>
+      </TitleAndNavButtons>
+      <RelatedProductSlide></RelatedProductSlide>
     </RelatedProductWrap>
   );
 }
