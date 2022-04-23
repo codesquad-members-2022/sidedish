751b4d243b6a9921ce3c8cb3e8dad7dcb6ebfa78
soralee2821 <70058081+soralee2821@users.noreply.github.com>
2022-04-23 14:20:18 +0900

    [Team-07] [FE] [ver & S] 두번째 PR (#90)
    * [#6] Chore: CRA 없이 React 빌드 환경 구축
    
    * [#6] Chore: sass 모듈 추가, react 인식 불가 에러 해결
    
    * [#6] Fix: typo 수정
    
    * [#8] Chore: Header 이미지 파일 추가
    
    * [#8] Feat: Header 구현
    
    * [#8] Design: Header 스타일 작성
    
    * [#8] Rename: 컴포넌트 확장자 변경
    
    * [#8] Rename: assets 디렉토리 생성 및 image 디렉토리 이동
    
    * [#6] Chore: webpack.config.js에 절대경로 alias 설정
    
    * [#8] Refactor: import 경로 수정
    
    * [#8] Refactor: Nav함수 구조분해할당 적용
    
    * [#8] Refactor: PR리뷰 반영
    
    Nav 컴포넌트 함수에 conditional rendering 적용
    makeNavCatetory함수 로직을 MainSubNav 컴포넌트 함수에서 처리
    
    * [#8] Refactor: MainNav, MainSubNav 컴포넌트 함수 이름 변경
    
    * [#8] Design: subCategory 마우스 호버 스타일 추가
    
    * [#13] Feat: Promotion 1차 구현
    
    * [#13] Chore: babel transform-runtime-plugin 설치, 설정
    
    * [#13] Feat: 기획전 컴포넌트 구현
    
    목서버에서 데이터 fetch 함수 구현
    PromotionBar에서 클릭이벤트 등록
    클릭이벤트의 타겟 ID에 따른 PromotionSection 렌더링 구현
    
    * Chore: 깃 충돌 해결 과정에서 꼬인 코드 수정
    
    * Remove: 깃 수정 과정에서 생긴 오류 수정
    
    * [#13] Feat: useEffect를 사용하여 TAB 랜더링 구현 & TAB 클릭 스타일 변경 구현
    
    Co-authored-by: DESKTOP-88VMMCI\user <luzverde0314@gmail.com>
    Co-authored-by: ver <95198109+lv0314@users.noreply.github.com>

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,8 +1,16 @@
-import { Header } from "./components/Header/Header";
+import { Header } from "@src/components/Header/Header";
 import "./App.scss";
+import { Promotion } from "@src/components/Promotion/Promotion";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <main>
+        <Promotion />
+      </main>
+    </div>
+  );
 }
 
 export default App;
