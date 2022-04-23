381fecfe5027a3d28a6eea0fc145d3109d03c777
leejohy-0223 <leejohy@naver.com>
2022-04-23 16:04:04 +0900

    refactor: ResponseEntity 반환을 일반 객체로 변경 (#32)

--- backend/src/main/java/com/codesquad/sidedish/item/domain/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codesquad.sidedish.item.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ItemRepository extends CrudRepository<Item, Integer> {
-
-}
