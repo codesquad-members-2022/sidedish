8eccbef6083da86017fb606dab9a1ade1b79ce80
sally-ksh <sally.devz@gmail.com>
2022-04-23 14:23:29 +0900

    refactor: Menu, Exhibition 생성자 접근제어자 protected, final 추가

--- BE/BE-sidedish/src/main/java/team14/sidedish/exhibition/Exhibition.java
+++ BE/BE-sidedish/src/main/java/team14/sidedish/exhibition/Exhibition.java
@@ -6,22 +6,26 @@ import org.springframework.data.relational.core.mapping.Table;
 import java.time.Duration;
 import java.time.LocalDate;
 
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
 @Table("SIDEDISH_EXHIBITIONS")
 public class Exhibition {
 	@Column("EXHIBITION_ID")
-	private Long exhibitionId;
+	private final Long exhibitionId;
 	@Column("EXHIBITION_TITLE")
-	private String title;
+	private final String title;
 	@Column("EXHIBITION_START")
-	private LocalDate startAt;
+	private final LocalDate startAt;
 	@Column("EXHIBITION_DURATION")
-	private Duration duration;
+	private final Duration duration;
 
 	protected static Exhibition of(String title, LocalDate startAt, Duration duration) {
 		return new Exhibition(null, title, startAt, duration);
+	}
+
+	protected Exhibition(Long exhibitionId, String title, LocalDate startAt, Duration duration) {
+		this.exhibitionId = exhibitionId;
+		this.title = title;
+		this.startAt = startAt;
+		this.duration = duration;
 	}
 
 	protected Long getExhibitionId() {
--- BE/BE-sidedish/src/main/java/team14/sidedish/menu/Menu.java
+++ BE/BE-sidedish/src/main/java/team14/sidedish/menu/Menu.java
@@ -6,31 +6,37 @@ import org.springframework.data.relational.core.mapping.Table;
 
 import java.math.BigDecimal;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @ToString
-@NoArgsConstructor
-@AllArgsConstructor
 @Table("SIDEDISH_MENUS")
 public class Menu {
 	@Id
 	@Column("MENU_ID")
 	private Long menuId;
 	@Column("MENU_NAME")
-	private String name;
+	private final String name;
 	@Column("DESCRIPTION")
-	private String description;
+	private final String description;
 	@Column("MENU_PRICE")
-	private BigDecimal price;
+	private final BigDecimal price;
 	@Column("MENU_CATEGORY")
-	private String category;
+	private final String category;
 	@Column("INVENTORY_QUANTITY")
-	private int inventoryQuantity;
+	private final int inventoryQuantity;
 
 	protected static Menu of(String name, String description, BigDecimal price, String category, int inventoryQuantity) {
 		return new Menu(null, name, description, price, category, inventoryQuantity);
+	}
+
+	protected Menu(Long menuId, String name, String description, BigDecimal price, String category,
+		int inventoryQuantity) {
+		this.menuId = menuId;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.category = category;
+		this.inventoryQuantity = inventoryQuantity;
 	}
 }
 
