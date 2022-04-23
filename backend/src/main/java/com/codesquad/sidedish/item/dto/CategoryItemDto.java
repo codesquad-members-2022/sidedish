8e6b57ab8efcfcd8bdd59c9b1e1b0c4319a5735a
leejohy-0223 <leejohy@naver.com>
2022-04-23 16:01:46 +0900

    refactor: DTO 내에 list naming 수정 및 안쓰는 메서드 제거 (#32)

--- backend/src/main/java/com/codesquad/sidedish/item/dto/CategoryItemDto.java
+++ backend/src/main/java/com/codesquad/sidedish/item/dto/CategoryItemDto.java
@@ -4,22 +4,18 @@ import java.util.List;
 
 public class CategoryItemDto {
     private String categoryName;
-    private List<ItemDto> itemsInCategory;
+    private List<ItemDto> items;
 
     public CategoryItemDto(String categoryName, List<ItemDto> itemsInCategory) {
         this.categoryName = categoryName;
-        this.itemsInCategory = itemsInCategory;
-    }
-
-    public void addItemDto(ItemDto itemDto) {
-        itemsInCategory.add(itemDto);
+        this.items = itemsInCategory;
     }
 
     public String getCategoryName() {
         return categoryName;
     }
 
-    public List<ItemDto> getItemsInCategory() {
-        return itemsInCategory;
+    public List<ItemDto> getItems() {
+        return items;
     }
 }
