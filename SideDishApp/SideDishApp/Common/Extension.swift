a3d806398bc6e2f1d81c9ff8919395ba4efe893f
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-04-24 22:31:48 +0900

    [#16] Feat:StrikeThrough String extension 추가

--- SideDishApp/SideDishApp/Common/Extension.swift
+++ SideDishApp/SideDishApp/Common/Extension.swift
@@ -5,4 +5,11 @@
 //  Created by Kai Kim on 2022/04/24.
 //
 
-import Foundation
+import UIKit
+extension String {
+    func strikeThrough() -> NSAttributedString {
+            let attributeString: NSMutableAttributedString = NSMutableAttributedString(string: self)
+            attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: attributeString.length))
+            return attributeString
+        }
+}
