2f17d9ff3c272f2cbad08e3d2ff9c6f86ce0e4f8
tokaji <fpzk5656@gmail.com>
2022-04-24 15:02:51 +0900

    [refactor] 변수명을 스크리밍 스네이크 케이스 방식으로 변경

--- be/src/main/resources/sql/schema.sql
+++ be/src/main/resources/sql/schema.sql
@@ -2,13 +2,13 @@ DROP TABLE IF EXISTS PRODUCT;
 
 CREATE TABLE PRODUCT
 (
-    id              INTEGER         PRIMARY KEY   AUTO_INCREMENT,
-    name            VARCHAR(30)     NOT NULL,
-    content         VARCHAR(300)    NOT NULL,
-    price           INT             NOT NULL,
-    quantity        INTEGER         NOT NULL,
-    dishType        VARCHAR(30)     NOT NULL,
-    sideDishType    VARCHAR(30),
-    applyEvent      VARCHAR(30),
-    imgUrl          VARCHAR(3000)
+    id                INTEGER           PRIMARY KEY   AUTO_INCREMENT,
+    name              VARCHAR(30)       NOT NULL,
+    content           VARCHAR(300)      NOT NULL,
+    price             INT               NOT NULL,
+    quantity          INTEGER           NOT NULL,
+    dish_type         VARCHAR(30)       NOT NULL,
+    side_dish_type    VARCHAR(30),
+    apply_event       VARCHAR(30),
+    img_url           VARCHAR(3000)
 );
