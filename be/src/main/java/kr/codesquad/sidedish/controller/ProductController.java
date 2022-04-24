ebdb7e25e1d6aee92fdc690e827281a7cbb76c0d
tokaji <fpzk5656@gmail.com>
2022-04-24 13:31:32 +0900

    [refactor] for문으로 돌리는 영역을 stream으로 간결화

--- be/src/main/java/kr/codesquad/sidedish/controller/ProductController.java
+++ be/src/main/java/kr/codesquad/sidedish/controller/ProductController.java
@@ -1,7 +1,7 @@
 package kr.codesquad.sidedish.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import kr.codesquad.sidedish.dto.DetailProductInfo;
 import kr.codesquad.sidedish.dto.ProductDTO;
 import kr.codesquad.sidedish.dto.RequestProduct;
@@ -11,12 +11,10 @@ import kr.codesquad.sidedish.response.CommonResponse;
 import kr.codesquad.sidedish.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,14 +32,13 @@ public class ProductController {
 	public ResponseEntity<CommonResponse<List<SimpleProductInfo>>> loadListByType(
 		@PathVariable String dishType,
 		@PathVariable String sideDishType) {
-		List<ProductDTO> dtoList = productService.loadListByType(dishType, sideDishType);
+		List<ProductDTO> productDTOS = productService.loadListByType(dishType, sideDishType);
 
-		List<SimpleProductInfo> simpleDtoList = new ArrayList<>();
-		for (int i = 0; i < dtoList.size(); i++) {
-			SimpleProductInfo simpleDto = SimpleProductInfo.from(dtoList.get(i));
-			simpleDtoList.add(simpleDto);
-		}
-		return new CommonResponse(CommonCode.SUCCESS, simpleDtoList).toResponseEntity();
+		List<SimpleProductInfo> simpleDTOS = productDTOS.stream()
+			.map(p -> SimpleProductInfo.from(p))
+			.collect(Collectors.toList());
+
+		return new CommonResponse(CommonCode.SUCCESS, simpleDTOS).toResponseEntity();
 	}
 
 
