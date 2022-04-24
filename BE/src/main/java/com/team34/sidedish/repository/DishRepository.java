2a49dd20040b27de6419783b5653d4c19ab6e236
nak253 <nwon4413@naver.com>
2022-04-24 11:39:13 +0900

    feat: DishRepository 클래스 작성
    - DishRepository 클래스 작성
    - DishRepositoryTest 작성

---
+++ BE/src/main/java/com/team34/sidedish/repository/DishRepository.java
@@ -0,0 +1,7 @@
+package com.team34.sidedish.repository;
+
+import com.team34.sidedish.domain.Dish;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DishRepository extends CrudRepository<Dish, Long> {
+}
