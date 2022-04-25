bb30e503c37b674d772e49721ca670635a4847d8
ak2j38 <k2j38@naver.com>
2022-04-25 18:02:55 +0900

    feature: repository 파일 추가
    1. repository 파일 추가
    
    end

---
+++ backend/src/main/java/com/team28/sidedish/repository/DiscountRepository.java
@@ -0,0 +1,7 @@
+package com.team28.sidedish.repository;
+
+import com.team28.sidedish.repository.entity.DiscountEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DiscountRepository extends CrudRepository<DiscountEntity, Long> {
+}
