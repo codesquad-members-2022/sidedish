1476eac9c9568291d73c47d01be6e07ec698e5d5
sally-ksh <sally.devz@gmail.com>
2022-04-25 11:23:56 +0900

    feat: SpecialMenuInfo 생성
    - findByExhibitionIdAndSpecialTitle() 조회 결과를 담는 객체

---
+++ BE/BE-sidedish/src/main/java/team14/sidedish/specialmenu/SpecialMenuInfo.java
@@ -0,0 +1,33 @@
+package team14.sidedish.specialmenu;
+
+import org.springframework.data.relational.core.mapping.Column;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+import team14.sidedish.menu.MenuModel;
+
+@ToString
+@Getter
+@AllArgsConstructor
+public class SpecialMenuInfo implements MenuModel {
+	private final String specialTitle;
+	private final Long menuId;
+	private final String menuName;
+	private final String description;
+	private final BigDecimal menuPrice;
+	@Column("MENU_IMAGE_URL")
+	private final String imageUrl;
+
+	@Override
+	public Long getMenuId() {
+		return menuId;
+	}
+
+	@Override
+	public BigDecimal getPrice() {
+		return this.menuPrice;
+	}
+}
