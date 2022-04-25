ac14986f26a6a5f0a046d6d7fe4b092d71777532
jinan159 <wlsdhks0423@naver.com>
2022-04-25 10:50:57 +0900

    refactor: OrderController 에만 있던 예외처리 로직을 전역 예외 처리 클래스로 이동
    - OrderController : 예외처리 로직 제거
    - GlobalExceptionHandler : 프로젝트 전역 예외처리 로직 추가

---
+++ backend/src/main/java/com/team28/sidedish/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.team28.sidedish.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
