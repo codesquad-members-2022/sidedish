9381eba5986ede29bc6949c68263957269241497
bibi <bibi6666667@kakao.com>
2022-04-25 17:03:25 +0900

    feat : MainCard를 Codable하게 변경 #20

--- sidedishApp/sidedishApp/model/MainCard.swift
+++ sidedishApp/sidedishApp/model/MainCard.swift
@@ -7,12 +7,26 @@
 
 import Foundation
 
-struct MainCard {
+struct MainCard: Codable {
     private(set) var detailHash: String
     private(set) var imageURL: String
+    private(set) var alt: String
+    private(set) var deliveryType: [String]
     private(set) var title: String
     private(set) var description: String
     private(set) var normalPrice: String
     private(set) var salePrice: String?
-    private(set) var badgeList: [Badge]?
+    private(set) var badgeList: [String]?
+    
+    enum CodingKeys: String, CodingKey {
+        case detailHash = "detail_hash"
+        case imageURL
+        case alt
+        case deliveryType = "delivery_type"
+        case title
+        case description
+        case normalPrice = "n_price"
+        case salePrice = "s_price"
+        case badgeList = "badge"
+    }
 }
