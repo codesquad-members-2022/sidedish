ae054aeffcff210eb87a03f470f2e1a6cfa2ee59
soralee <sora2821@gmail.com>
2022-04-25 10:36:24 +0900

    Feat: constant 파일을 만들어서 상수 변수를 관리함

---
+++ FE/src/constant/constant.js
@@ -0,0 +1,24 @@
+export const navCategory = [
+  {
+    id: "main1",
+    mainCategory: "든든한 메인요리",
+    subCategory: ["육류 요리", "해산물 요리"],
+  },
+  {
+    id: "main2",
+    mainCategory: "뜨끈한 국물요리",
+    subCategory: ["국/탕/찌개"],
+  },
+  {
+    id: "main3",
+    mainCategory: "정갈한 밑반찬",
+    subCategory: ["나물/무침", "조림/볶음", "절임/장아찌"],
+  },
+];
+
+export const categoryText = [
+  "풍성한 고기 반찬",
+  "편리한 반찬 세트",
+  "맛있는 제철 요리",
+  "우리 아이 영양 반찬",
+];
