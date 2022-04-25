21503a2923cc76b3f7039f59cb86f2745d16966a
bibi <bibi6666667@kakao.com>
2022-04-25 17:34:26 +0900

    feat : SideDishManager 추가 및 main화면의 maindish 요청 (진행 중) #20

---
+++ sidedishApp/sidedishApp/model/SideDishManager.swift
@@ -0,0 +1,39 @@
+//
+//  SideDishManager.swift
+//  sidedishApp
+//
+//  Created by Bibi on 2022/04/25.
+//
+
+import Foundation
+
+final class SideDishManager {
+    
+    static let identifier = "SideDishManager"
+    
+    private(set) var mainDishes = [MainCard]()
+    private(set) var soupDishes = [MainCard]()
+    private(set) var sideDishes = [MainCard]()
+    
+    static let shared = SideDishManager()
+    
+    private init() {} // shared 싱글톤을 위해 init을 private로 설정
+    
+    func getMainDishes() {
+        HTTPManager.requestGet(url: "https://api.codesquad.kr/onban/main") { data in
+            guard let mainDishes: [MainCard] = JSONConverter.decodeJsonArray(data: data) else { return }
+            
+            self.mainDishes.append(contentsOf: mainDishes)
+            
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: SideDishManager.identifier), object: self)
+        }
+    }
+    
+    func getSoupDishes() {
+        
+    }
+    
+    func getSideDishes() {
+        
+    }
+}
