3e2a078b87231c3055bb20e26d95cbfeb0823555
asqw887 <asqw887@gmail.com>
2022-04-25 17:57:58 +0900

    FEAT: Food 관리 객체 구현

--- sideDish/sideDish/Model/FoodManager.swift
+++ sideDish/sideDish/Model/FoodManager.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+class FoodManager{
+    
+    let category = ["main", "soup", "side"]
+    
+    var mainFood: [Food]?
+    var soupFood: [Food]?
+    var sideFood: [Food]?
+    
+    // API로 요청 -> 실패시 mockup data 로 데이터 가져오도록
+    
+    init(){
+        foodLoadData()
+    }
+    
+    func foodLoadData(){
+        mainFood = JsonConvertor.mockLoad(file: "main")
+        soupFood = JsonConvertor.mockLoad(file: "soup")
+        sideFood = JsonConvertor.mockLoad(file: "side")
+    }
+
+}
