7402f94fb1053aa40b9db7a828b44054e79d6b03
asqw887 <asqw887@gmail.com>
2022-04-25 17:58:51 +0900

    FEAT: UIImage url load extension

--- sideDish/sideDish/Extension/UIImageView.swift
+++ sideDish/sideDish/Extension/UIImageView.swift
@@ -5,4 +5,16 @@
 //  Created by 최예주 on 2022/04/25.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async {
+            [weak self] in if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async { self?.image = image }
+                }
+            }
+        }
+    }
+}
