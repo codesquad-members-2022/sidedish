4c158f7e4ad1cdb6bcd0fe3fb7905bd44e2fe674
seyeong <seyoung7555@naver.com>
2022-04-25 11:10:09 +0900

    feat: 각 api별 설명 및 동작 예시 작성 (#33)

--- backend/src/main/java/com/codesquad/sidedish/item/controller/ItemController.java
+++ backend/src/main/java/com/codesquad/sidedish/item/controller/ItemController.java
@@ -4,6 +4,12 @@ import com.codesquad.sidedish.item.dto.CategoryItemsDto;
 import com.codesquad.sidedish.item.dto.DetailItemDto;
 import com.codesquad.sidedish.item.exception.ItemIdNotFoundException;
 import com.codesquad.sidedish.item.service.ItemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "items", description = "반찬 조회 API")
 @RequestMapping("/items")
 @RestController
 public class ItemController {
@@ -23,11 +30,20 @@ public class ItemController {
         this.itemService = itemService;
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "반찬 목록 조회 성공", content = @Content(schema = @Schema(implementation = CategoryItemsDto.class)))
+    })
+    @Operation(summary = "반찬 목록 조회", description = "판매하는 반찬의 목록을 조회합니다.")
     @GetMapping
     public CategoryItemsDto itemList() {
         return itemService.findAll();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "반찬 조회 성공", content = @Content(schema = @Schema(implementation = CategoryItemsDto.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스 접근")
+    })
+    @Operation(summary = "반찬 상세정보 조회", description = "판매하는 반찬의 상세 정보를 확인합니다.")
     @GetMapping("/{id}")
     public ResponseEntity<DetailItemDto> detail(@PathVariable int id) {
         DetailItemDto detailItemDto;
