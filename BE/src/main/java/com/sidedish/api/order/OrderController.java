36949e7525c1d515fbd1f35b426470fd2d31fd93
jiwoo <zbqmgldjfh@gmail.com>
2022-04-25 22:13:03 +0900

    feat(OrderRequest): OrderRequest에 Validation 추가하기

--- BE/src/main/java/com/sidedish/api/order/OrderController.java
+++ BE/src/main/java/com/sidedish/api/order/OrderController.java
@@ -2,12 +2,16 @@ package com.sidedish.api.order;
 
 import com.sidedish.api.ResultDto;
 import com.sidedish.api.order.dto.OrderRequest;
+import com.sidedish.exception.QuantityException;
 import com.sidedish.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,7 +20,10 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
-    public ResultDto makeOrder(@RequestBody OrderRequest orderRequest) {
+    public ResultDto makeOrder(@Valid @RequestBody OrderRequest orderRequest, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            throw new QuantityException();
+        }
         return orderService.order(orderRequest.getId(), orderRequest.getQuantity());
     }
 }
--- BE/src/main/java/com/sidedish/api/order/dto/OrderRequest.java
+++ BE/src/main/java/com/sidedish/api/order/dto/OrderRequest.java
@@ -4,10 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderRequest {
+
+    @NotNull
     private Long id;
+
+    @Min(1)
     private int quantity;
 }
