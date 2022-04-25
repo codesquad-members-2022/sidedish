20be85531c10787967bf8cc4f339e2b066bd6180
Damagucci-Juice <jsr9874@gmail.com>
2022-04-25 11:09:44 +0900

    [#17] feat: Model 도입전 디렉터리 생성

---
+++ SideDish/SideDish/Model/Entity/DishCellInfo.swift
@@ -0,0 +1,8 @@
+//
+//  DishCellInfo.swift
+//  SideDish
+//
+//  Created by YEONGJIN JANG on 2022/04/25.
+//
+
+import Foundation
---
+++ SideDish/SideDish/ViewModel/DishCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DishCellViewModel.swift
+//  SideDish
+//
+//  Created by YEONGJIN JANG on 2022/04/25.
+//
+
+import Foundation
