b078096286382e1ba68f67932c448da166cf1664
youryu0212 <youryu0212@naver.com>
2022-04-25 22:25:48 +0900

    [fe#29] feat: fetch 함수 생성

--- fe/src/helper/utils.js
+++ fe/src/helper/utils.js
@@ -17,3 +17,13 @@ export function applyFlex({ flex, justify, align, direction }) {
     `
   );
 }
+
+export async function fetchData(url) {
+  try {
+    const data = await fetch(url);
+    return data.json();
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
