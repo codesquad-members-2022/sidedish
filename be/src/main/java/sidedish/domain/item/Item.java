0d20c958f24920e81aead9f4c2b5033a7df8539e
함석호 <kalejuice02@gmail.com>
2022-04-25 10:55:23 +0900

    refactor : ResponseItemDto에 있던 비즈니스 로직을 Item 도메인 객체로 이동

--- be/src/main/java/sidedish/domain/item/Item.java
+++ be/src/main/java/sidedish/domain/item/Item.java
@@ -1,19 +1,14 @@
 package sidedish.domain.item;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.relational.core.mapping.MappedCollection;
-import org.springframework.data.relational.core.mapping.Table;
 import sidedish.domain.images.Images;
 
 import java.util.List;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
-
 public class Item {
 
 	@Id
@@ -23,8 +18,26 @@ public class Item {
 	private String description;
 	private int price;
 	private int quantity;
+	@Transient
+	private double accumulate;
 	private String discountPolicy;
+	@Transient
+	private double discountRate;
 	private boolean morningDelivery;
 	@MappedCollection(idColumn = "item_id", keyColumn = "id")
 	private List<Images> images;
+
+	public Item(Long id, Long mainCategoryId, String title, String description, int price, int quantity, String discountPolicy, boolean morningDelivery, List<Images> images) {
+		this.id = id;
+		this.mainCategoryId = mainCategoryId;
+		this.title = title;
+		this.description = description;
+		this.price = price;
+		this.quantity = quantity;
+		this.accumulate = price * 0.01;
+		this.discountPolicy = discountPolicy;
+		this.discountRate = 0.1;
+		this.morningDelivery = morningDelivery;
+		this.images = images;
+	}
 }
