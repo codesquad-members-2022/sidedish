a63959d061c40cc6b61e6af08300e1318db4d772
youryu0212 <youryu0212@naver.com>
2022-04-25 22:25:09 +0900

    [fe#29] feat: 배송 옵션 토글 기능 추가

--- fe/src/components/ProductCard.js
+++ fe/src/components/ProductCard.js
@@ -51,3 +51,34 @@ function EventWrapper({ events, size }) {
     </StyledWrapper>
   );
 }
+
+function DeliveryCircle({ shippingInfo, hover }) {
+  if (!shippingInfo) {
+    return;
+  }
+  if (!hover) {
+    return;
+  }
+  const deliveryType = parseShippingInfo(shippingInfo);
+
+  return (
+    <StyledCircle flex direction="column" justify="center" align="center">
+      {deliveryType.map((delivery, idx) => {
+        if (!delivery) {
+          return false;
+        }
+        return <DeliveryInfo key={`${delivery}-${idx}`}>{delivery}</DeliveryInfo>;
+      })}
+    </StyledCircle>
+  );
+}
+
+function parseShippingInfo(shippingInfo) {
+  shippingInfo = shippingInfo.replace('새벽배송', '새벽 배송');
+  shippingInfo = shippingInfo.replace('전국택배', '전국 택배');
+  const checkDeliveryType = str => {
+    return shippingInfo.includes(str) && str;
+  };
+  const [earlyDelivery, allCountryDelivery] = [checkDeliveryType('새벽 배송'), checkDeliveryType('전국 택배')];
+  return [earlyDelivery, allCountryDelivery];
+}
--- fe/src/components/ProductCard.styled.js
+++ fe/src/components/ProductCard.styled.js
@@ -116,6 +116,30 @@ export const Event = styled.div`
 `;
 
 export const CardWrapper = styled.div`
+  position: relative;
   margin-right: 24px;
   cursor: pointer;
+`;
+
+export const StyledCircle = styled.div`
+  ${({ flex, justify, align, direction }) => applyFlex({ flex, justify, align, direction })};
+  position: absolute;
+  width: 120px;
+  height: 120px;
+  top: 5%;
+  right: 10%;
+
+  background: rgba(248, 247, 247, 0.8);
+
+  border: 1px solid #1b1b1b;
+  box-sizing: border-box;
+  border-radius: 50%;
+  p:last-child {
+    border: none;
+  }
+`;
+
+export const DeliveryInfo = styled.p`
+  padding: 10px;
+  border-bottom: 1px solid black;
 `;
