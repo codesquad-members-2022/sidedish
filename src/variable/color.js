18bbdd756a9f769110aad9d8e008f323fa4d6bc3
jindonyy <rsefa94@gmail.com>
2022-04-25 20:23:44 +0900

    ♻️ color scale 100단위로 적용

--- src/variable/color.js
+++ src/variable/color.js
@@ -1,9 +1,11 @@
 const COLOR = {
   BLACK: '#1b1b1b',
-  GREY1: '#3f3f3f',
-  GREY2: '#777',
-  GREY3: '#bcbcbc',
-  GREY4: '#ebebeb',
+  GREY: {
+    100: '#3f3f3f',
+    200: '#777',
+    300: '#bcbcbc',
+    400: '#ebebeb'
+  },
   OFF_WHITE: '#f8f7f7',
   WHITE: '#fff',
   GREEN: '#6DD028',
