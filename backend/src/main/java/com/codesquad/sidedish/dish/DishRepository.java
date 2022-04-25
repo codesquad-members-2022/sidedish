ab5a281c3ab1e354e1e5fc653eef7edda182e742
Kang Changgu (Miller) <rkckr1@naver.com>
2022-04-25 23:33:42 +0900

    refactor: [#34] Dish aggregate 의 리포지토리 수정 및 N:1 매핑 제거

--- backend/src/main/java/com/codesquad/sidedish/dish/DishRepository.java
+++ backend/src/main/java/com/codesquad/sidedish/dish/DishRepository.java
@@ -8,24 +8,16 @@ import org.springframework.data.repository.query.Param;
 
 public interface DishRepository extends CrudRepository<Dish, Integer> {
 
-    @Query(value = "select d.dish_id, d.title, d.description, d.price, d.stock,"
-        + " di.dish_image_id, di.image_path, di.sequence, dc.category_id"
-        + " from dish d"
-        + " left outer join dish_image di on d.dish_id = di.dish_id and di.sequence = 1"
+    @Query(value = "select d.* from dish d"
         + " join dish_category dc on d.dish_id = dc.dish_id"
         + " join category sub on dc.category_id = sub.category_id"
         + " join category sec on sub.section_id = sec.category_id and sec.category_name = :section_name"
-        + " order by d.dish_id",
-        rowMapperClass = DishRowMapper.class)
+        + " order by d.dish_id")
     List<Dish> findBySectionName(@Param("section_name") String sectionName);
 
-    @Query(value = "select d.dish_id, d.title, d.description, d.price, d.stock,"
-        + " di.dish_image_id, di.image_path, di.sequence, dc.category_id"
-        + " from dish d"
-        + " left outer join dish_image di on d.dish_id = di.dish_id and di.sequence = 1"
+    @Query(value = "select d.* from dish d"
         + " join dish_category dc on d.dish_id = dc.dish_id"
         + " join category sub on dc.category_id = sub.category_id and sub.category_name = :category_name"
-        + " order by d.dish_id",
-        rowMapperClass = DishRowMapper.class)
+        + " order by d.dish_id")
     List<Dish> findByCategoryName(@Param("category_name") String categoryName);
 }
