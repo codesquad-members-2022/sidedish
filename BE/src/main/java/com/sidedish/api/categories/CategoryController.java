67ebd60670e9948d406b40e3c919b5b74a921828
geombong <shoy1415@gmail.com>
2022-04-25 11:27:20 +0900

    fix(CategoryComtroller): 메서드 네이밍 변경 #22
    Contents
    - 좀 더 명확한 메서드 네이밍 적용

--- BE/src/main/java/com/sidedish/api/categories/CategoryController.java
+++ BE/src/main/java/com/sidedish/api/categories/CategoryController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.beans.PropertyEditorSupport;
 import java.util.List;
-import java.util.Locale;
 import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -37,15 +36,15 @@ public class CategoryController {
     }
 
     @GetMapping("/{type}")
-    public CollectionModel<ItemResource> getCategoriesByType(@PathVariable CategoryType type, @RequestParam Long pageId) {
+    public CollectionModel<ItemResource> getItemsByCategory(@PathVariable CategoryType type, @RequestParam Long pageId) {
 
         List<Item> items = itemService.findUnitPageById(type.getTypeNumber(), pageId);
         List<ItemResource> itemResources = items.stream().map(ItemResource::new).collect(Collectors.toList());
 
         CollectionModel<ItemResource> responseMainType = CollectionModel.of(itemResources);
-        responseMainType.add(linkTo(methodOn(CategoryController.class).getCategoriesByType(type, pageId)).withSelfRel());
-        responseMainType.add(linkTo(methodOn(CategoryController.class).getCategoriesByType(type,pageId-1)).withRel("prev-page"));
-        responseMainType.add(linkTo(methodOn(CategoryController.class).getCategoriesByType(type, pageId+1)).withRel("next-page"));
+        responseMainType.add(linkTo(methodOn(CategoryController.class).getItemsByCategory(type, pageId)).withSelfRel());
+        responseMainType.add(linkTo(methodOn(CategoryController.class).getItemsByCategory(type,pageId-1)).withRel("prev-page"));
+        responseMainType.add(linkTo(methodOn(CategoryController.class).getItemsByCategory(type, pageId+1)).withRel("next-page"));
 
         return responseMainType;
     }
