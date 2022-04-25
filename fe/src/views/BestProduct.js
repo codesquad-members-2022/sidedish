0a65b73dc9a8e65d4375c12830acc317464a53d1
youryu0212 <youryu0212@naver.com>
2022-04-25 22:28:58 +0900

    [fe#29] feat: 기획전 렌더링 서버 api 연동

--- fe/src/views/BestProduct.js
+++ fe/src/views/BestProduct.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import { bestProductTabDatas } from '../helper/mock';
 import { TabMenu } from './bestProduct/tabMenu.js';
 import { TabList } from './bestProduct/tabList';
 import {
@@ -7,38 +6,49 @@ import {
   CategoryBadge,
   StyledBestProduct,
   TabBar,
-  Title,
   TitleWrapper,
 } from './bestProduct/BestProduct.styled';
+import { fetchData } from '../helper/utils';
+import { API } from '../helper/constants';
+import { ExhibitionTitle } from './bestProduct/exhibitionTitle';
 
 export function BestProduct() {
+  const [exhibitionTitle, setExhibitionTitle] = useState(null);
   const [bestProductTab, setBestProductTab] = useState(null);
   const [tabList, setTabList] = useState(null);
   const [curTab, setCurTab] = useState(null);
+  const [sideDishCardDatas, setSideDishCardDatas] = useState([]);
   useEffect(() => {
-    const tabMenus = [
-      { title: '풍성한 고기반찬', id: 0 },
-      { title: '편리한 반찬 세트', id: 1 },
-      { title: '맛있는 제철 요리', id: 2 },
-      { title: '우리 아이 영양 반찬', id: 3 },
-    ];
-    setBestProductTab(tabMenus);
-    setCurTab(0);
+    async function getExhibitionData() {
+      const exhibitionData = await fetchData(API.exhibitions);
+      const { exhibitionTitle, categoryResponses } = exhibitionData;
+      setExhibitionTitle(exhibitionTitle);
+      setBestProductTab(categoryResponses);
+      setSideDishCardDatas(
+        categoryResponses.reduce((sideDishCardDatas, category) => {
+          sideDishCardDatas[category.categoryId] = category.sideDishCardResponses;
+          return sideDishCardDatas;
+        }, {})
+      );
+      const firstCategoryId = categoryResponses[0].categoryId;
+      setCurTab(firstCategoryId);
+    }
+    getExhibitionData();
   }, []);
 
   useEffect(() => {
     if (curTab === null) {
       return;
     }
-    setTabList(bestProductTabDatas[curTab]);
-  }, [curTab]);
+    setTabList(sideDishCardDatas[curTab]);
+  }, [sideDishCardDatas, curTab]);
 
   return (
     <StyledBestProduct>
       <BestProductHeader>
         <TitleWrapper flex align="center">
           <CategoryBadge>기획전</CategoryBadge>
-          <Title>한 번 주문하면 두 번 반하는 반찬</Title>
+          <ExhibitionTitle title={exhibitionTitle}></ExhibitionTitle>
         </TitleWrapper>
         <TabBar flex>
           <TabMenu
