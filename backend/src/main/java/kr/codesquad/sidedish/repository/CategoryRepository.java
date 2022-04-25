0481578a50a251186e02c02ef9742d75194a35fe
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-04-25 18:19:27 +0900

    Refactor: 레포지토리에서 리스트 타입 반환

--- backend/src/main/java/kr/codesquad/sidedish/repository/CategoryRepository.java
+++ backend/src/main/java/kr/codesquad/sidedish/repository/CategoryRepository.java
@@ -1,9 +1,13 @@
 package kr.codesquad.sidedish.repository;
 
+import java.util.List;
 import kr.codesquad.sidedish.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+	@Override
+	List<Category> findAll();
 }
