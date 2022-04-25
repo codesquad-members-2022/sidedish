5ffa846cc2eea747c63259f233d10e70b58d5d19
leekm0310 <leekm310@gmail.com>
2022-04-25 10:56:09 +0900

    [#27] Modify: schema.sql - or_price, dc_price 축약어가 아닌 풀네임으로 변경하기

--- BE/src/main/resources/data.sql
+++ BE/src/main/resources/data.sql
@@ -12,67 +12,67 @@ INSERT INTO best_type (best_name) VALUES ('meat');
 INSERT INTO best_type (best_name) VALUES ('simple');
 INSERT INTO best_type (best_name) VALUES ('seasonal');
 INSERT INTO best_type (best_name) VALUES ('nutrition');
-INSERT INTO item (category_id, item_name, description, or_price, dc_price, stock, label_id, main_image,
+INSERT INTO item (category_id, item_name, description, origianl_price, discount_price, stock, label_id, main_image,
                   delivery_id, best_id)
 VALUES (3,'오리주물럭','맛있는 오리고기와 오징어의 조합',15800, 12640,10,1,
         'https://user-images.githubusercontent.com/92678171/164159697-1c8ef07a-141e-464b-b4e5-bb73e2119535.png',
         3,1);
-INSERT INTO item (category_id, item_name, description, or_price, dc_price, stock, label_id, main_image,
+INSERT INTO item (category_id, item_name, description, origianl_price, discount_price, stock, label_id, main_image,
                   delivery_id)
 VALUES (2,'한돈 돼지 김치 찌개','김치찌개는 역시 한돈이지',9900,8910,10,2,
         '',
         1);
-INSERT INTO item (category_id, item_name, description, or_price, dc_price, stock, label_id, main_image,
+INSERT INTO item (category_id, item_name, description, origianl_price, discount_price, stock, label_id, main_image,
                   delivery_id, best_id)
 VALUES (1,'호두 멸치볶음','잔멸치와 호두가 만나 짭쪼롬하지만 고소하지',5800,5220,10,2,
         '',
         2,2);
-INSERT INTO item (category_id, item_name, description, or_price, stock, label_id, main_image,
+INSERT INTO item (category_id, item_name, description, origianl_price, stock, label_id, main_image,
                   delivery_id, best_id)
 VALUES (1,'달래장','간장게장 뺨치는 밥도둑', 4000,10,3,
         '',
         2,3);
-INSERT INTO item (category_id, item_name, description, or_price, stock, label_id, main_image,
+INSERT INTO item (category_id, item_name, description, origianl_price, stock, label_id, main_image,
                   delivery_id, best_id)
 VALUES (1,'미나리 무침','향긋하고 고소한 봄나물', 4800,10, 3,
         '',
         2,3);
-INSERT INTO item (category_id, item_name, description, or_price, dc_price, stock, label_id, main_image,
+INSERT INTO item (category_id, item_name, description, origianl_price, discount_price, stock, label_id, main_image,
                   delivery_id, best_id)
 VALUES (1,'새콤달콤 유채나물 무침','새콤달콤보다 더 새콤달콤한 유채나물 무침',4800, 4320, 10, 2,
         '',
         2,3);
-INSERT INTO item (category_id, item_name, description, or_price, stock, label_id, main_image,
+INSERT INTO item (category_id, item_name, description, origianl_price, stock, label_id, main_image,
                   delivery_id, best_id)
 VALUES (1,'새콤 오이무생채','아이들이 먹기 좋은 깔끔한 무생채', 3500, 10,3,
         '',
         1,4);
-INSERT INTO item (category_id, item_name, description, or_price, dc_price, stock, label_id, main_image,
+INSERT INTO item (category_id, item_name, description, origianl_price, discount_price, stock, label_id, main_image,
                   delivery_id, best_id)
 VALUES (2,'아이들 된장국', '아이들도 먹지 좋은 맵지 않은 된장국', 8800, 7920, 10, 2,
         '',
         3, 4);
-INSERT INTO item (category_id, item_name, description, or_price, dc_price, stock, label_id, main_image,
+INSERT INTO item (category_id, item_name, description, origianl_price, discount_price, stock, label_id, main_image,
                   delivery_id, best_id)
 VALUES (1, '계란말이','두툼하고 부드러운 계란말이',5800,5220, 10,2,
         '',
         3, 4);
-INSERT INTO item (category_id, item_name, description, or_price, dc_price, stock, label_id, main_image,
+INSERT INTO item (category_id, item_name, description, origianl_price, discount_price, stock, label_id, main_image,
                   delivery_id)
 VALUES (2, '시래기 된장국','구수하면서 시원한 맛', 7800,7020,10,2,
         '',
         2);
-INSERT INTO item (category_id, item_name, description, or_price, dc_price, stock, label_id, main_image,
+INSERT INTO item (category_id, item_name, description, origianl_price, discount_price, stock, label_id, main_image,
                   delivery_id)
 VALUES (2,'콩나물국','국내산 콩나물로 더 건강하게', 6800,6120, 10,2,
         '',
         1);
-INSERT INTO item (category_id, item_name, description, or_price, dc_price, stock, label_id, main_image,
+INSERT INTO item (category_id, item_name, description, origianl_price, discount_price, stock, label_id, main_image,
                   delivery_id, best_id)
 VALUES (3, '소갈비찜', '맛있는 소갈비와 양념의 조합', 28900, 26010, 10, 2,
         '',
         3,1);
-INSERT INTO item (category_id, item_name, description, or_price, stock, label_id, main_image,
+INSERT INTO item (category_id, item_name, description, origianl_price, stock, label_id, main_image,
                   delivery_id, best_id)
 VALUES (3, '쭈꾸미 한돈 제육볶음','쭈꾸미와 한돈의 조합', 16900, 10, 3,
         '',
