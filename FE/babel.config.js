b3671d9530f3644607d3248dd891731a4d9b7232
soralee <sora2821@gmail.com>
2022-04-25 10:24:35 +0900

    [#6] Chore: babel config에 `runtime: automatic' 추가

--- FE/babel.config.js
+++ FE/babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
+  presets: [
+    "@babel/preset-env",
+    ["@babel/preset-react", { runtime: "automatic" }],
+  ],
   plugins: ["@babel/plugin-transform-runtime"],
 };
