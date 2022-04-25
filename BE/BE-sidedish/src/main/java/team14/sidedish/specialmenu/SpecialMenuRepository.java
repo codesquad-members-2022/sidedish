382593ce88308b137e8341b7c23a453361e71e65
sally-ksh <sally.devz@gmail.com>
2022-04-25 10:50:20 +0900

    feat: SpecialMenuRepository 생성
    - 추천메뉴 저장소

---
+++ BE/BE-sidedish/src/main/java/team14/sidedish/specialmenu/SpecialMenuRepository.java
@@ -0,0 +1,14 @@
+package team14.sidedish.specialmenu;
+
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface SpecialMenuRepository extends CrudRepository<SpecialMenu, Long> {
+	@Query("select a.SPECIAL_TITLE, a.MENU_ID, b.MENU_NAME, b.DESCRIPTION, b.MENU_PRICE, c.MENU_IMAGE_URL from SIDEDISH_SPECIAL_MENUS a\n"
+		+ "inner join SIDEDISH_MENUS b on a.MENU_ID = b.MENU_ID\n"
+		+ "inner join SIDEDISH_IMAGE c on a.MENU_ID = c.IMAGE_MENU_ID\n"
+		+ "where a.EXHIBITIONS_ID = :exhibitionId and a.SPECIAL_TITLE = :specialTitle and c.IMAGE_DEFAULT = 1;")
+	List<SpecialMenuInfo> findByExhibitionIdAndSpecialTitle(Long exhibitionId, String specialTitle);
+}
