a262d21ca1053e11579f37c73c95bee2ac61bd69
seyeong <seyoung7555@naver.com>
2022-04-25 11:10:37 +0900

    feat: 각 dto별 설명 및 example 값 작성 (#33)

--- backend/src/main/java/com/codesquad/sidedish/item/dto/DetailItemDto.java
+++ backend/src/main/java/com/codesquad/sidedish/item/dto/DetailItemDto.java
@@ -3,14 +3,22 @@ package com.codesquad.sidedish.item.dto;
 import java.util.Set;
 
 import com.codesquad.sidedish.item.domain.ItemImage;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public class DetailItemDto {
+    @Schema(description = "Item 번호", example = "1")
     private Integer id;
+    @Schema(description = "할인 정책 이름", example = "런칭특가")
     private String discountPolicy;
+    @Schema(description = "할인율", example = "20")
     private Integer discountRate;
+    @Schema(description = "상품 설명", example = "감칠맛 나는 매콤한 양념")
     private String description;
+    @Schema(description = "상품 이름", example = "오리 주물럭_반조리")
     private String name;
+    @Schema(description = "상품 가격(원가)", example = "15800")
     private Integer price;
+    @Schema(description = "메인 이미지 링크", example = "https://leejohy-springboot-build.s3.ap-northeast-2.amazonaws.com/2cdf7235b65a114abff05b9133f90085.jpeg")
     private String mainImageLink;
     private Set<ItemImage> detailImageLink;
 
