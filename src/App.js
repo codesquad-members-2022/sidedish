5adcc05a235dfc1da0507b651c529ee9f262bf4d
decay95 <decay95@naver.com>
2022-04-25 18:20:04 +0900

    feat: 로딩 이후 data요청 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import React, { useEffect, useState } from 'react';
 import Header from './Components/header/main';
+import Main from './Components/main/main';
+import { setfoodData } from './Store/foodData';
+
 function App() {
+  const [hasData, setHasData] = useState(false);
+  useEffect(() => {
+    async function setData() {
+      await setfoodData();
+      setHasData(true);
+    }
+    setData();
+  }, [hasData]);
   return (
     <div className="App">
       <Header />
+      <Main hasData={hasData} />
     </div>
   );
 }
