e3330bd2197becc2acd574c103c9d306e71c32da
sally-ksh <sally.devz@gmail.com>
2022-04-25 20:19:31 +0900

    feat: MenuRepository - findByCategory()
    - 카테고리별로 메뉴 조회

--- BE/BE-sidedish/src/main/java/team14/sidedish/menu/MenuRepository.java
+++ BE/BE-sidedish/src/main/java/team14/sidedish/menu/MenuRepository.java
@@ -2,5 +2,8 @@ package team14.sidedish.menu;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MenuRepository extends CrudRepository<Menu, Long> {
+	List<Menu> findByCategory(Menu.Category category);
 }
