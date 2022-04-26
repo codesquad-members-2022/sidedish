374257915bd39b4ac2db30113307e561a5a7f4b0
sally-ksh <sally.devz@gmail.com>
2022-04-26 20:31:50 +0900

    feat: EventRepository findEventByEventIdIn()
    - aggragate root 이용한 조회로 변경하여
      기본의 OngoingEventInfo 삭제

--- BE/BE-sidedish/src/main/java/team14/sidedish/event/EventRepository.java
+++ BE/BE-sidedish/src/main/java/team14/sidedish/event/EventRepository.java
@@ -1,13 +1,18 @@
 package team14.sidedish.event;
 
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface EventRepository extends CrudRepository<Event, Long> {
-	@Query("select a.EVENT_ID, a.EVENT_TITLE, b.SALE_TITLE, b.SALE_AMOUNT from SIDEDISH_EVENTS a\n"
-		+ "inner join SIDEDISH_SALE_POLICY b on a.EVENT_ID = b.EVENT_ID where a.EVENT_ID in (:eventId)")
-	List<OngoingEventInfo> findMenuOngoingByEventIdIn(List<Long> eventId);
+	List<Event> findEventByEventIdIn(List<Long> ids);
 }
+
+
+/*  // compare
+	@Query("select a.EVENT_ID, a.EVENT_TITLE, b.SALE_TITLE, b.SALE_AMOUNT from SIDEDISH_EVENTS a "
+		+ "inner join SIDEDISH_SALE_POLICY b on a.EVENT_ID = b.EVENT_ID where a.EVENT_ID in (:eventIds);")
+	List<OngoingEventInfo> findMenuOngoingByEventIdIn(@Param("eventIds") List<Long> eventIds);
+*/
+
 
