4943ac7801a6c8c698dc056809979528e5a0fb39
sally-ksh <sally.devz@gmail.com>
2022-04-26 20:32:32 +0900

    feat: EventService 조회 반환타입 Event로 변경
    - EventRepository findEventByEventIdIn()로 변경 결과
    - aggragate root 이용한 조회로 변경하여
      기본의 OngoingEventInfo 삭제

--- BE/BE-sidedish/src/main/java/team14/sidedish/event/EventService.java
+++ BE/BE-sidedish/src/main/java/team14/sidedish/event/EventService.java
@@ -12,7 +12,6 @@ import team14.sidedish.salepolicy.SalePolicy;
 @Service
 public class EventService {
 	private final EventRepository eventRepository;
-
 	/**
 	 * 현재 진행중인 이벤트의 menuId 에 해당하는 eventId 목록을 전달 받아, 해당 이벤트 정보를 조회 합니다.
 	 * - 이벤트 배지 정보
@@ -21,8 +20,8 @@ public class EventService {
 	 * @return
 	 */
 	public EventAndSalesDto read(List<Long> eventIds) {
-		List<OngoingEventInfo> ongoingEventInfos = eventRepository.findMenuOngoingByEventIdIn(eventIds);
-		return getEventAndSalesDto(ongoingEventInfos);
+		List<Event> events = eventRepository.findEventByEventIdIn(eventIds);
+		return getEventAndSalesDto(events);
 	}
 
 	/**
@@ -30,22 +29,25 @@ public class EventService {
 	 * @param ongoingEventInfos
 	 * @return
 	 */
-	private EventAndSalesDto getEventAndSalesDto(List<OngoingEventInfo> ongoingEventInfos) {
+	private EventAndSalesDto getEventAndSalesDto(List<Event> ongoingEventInfos) {
 		List<Event> events = getEvents(ongoingEventInfos);
 		List<SalePolicy> salePolicies = getSalePolicies(ongoingEventInfos);
 		return new EventAndSalesDto(events, salePolicies);
 	}
 
-	private List<SalePolicy> getSalePolicies(List<OngoingEventInfo> ongoingEventInfos) {
-		return ongoingEventInfos.stream()
-			.map(ongoingEventInfo -> SalePolicy.of(ongoingEventInfo.getSaleTitle(), ongoingEventInfo.getSaleAmount(),
-				ongoingEventInfo.getEventId()))
+	private List<SalePolicy> getSalePolicies(List<Event> events) {
+		return events.stream()
+			.map(event ->
+				event.getSalePolicies().stream()
+				.map(salePolicy -> SalePolicy.of(salePolicy.policy(), salePolicy.discountAmount(), event.getEventId()))
+				.collect(Collectors.toList())
+			).flatMap(salePolicies -> salePolicies.stream())
 			.collect(Collectors.toList());
 	}
 
-	private List<Event> getEvents(List<OngoingEventInfo> ongoingEventInfos) {
-		return ongoingEventInfos.stream()
-			.map(ongoingEventInfo -> new Event(ongoingEventInfo.getEventId(), ongoingEventInfo.getEventTitle()))
+	private List<Event> getEvents(List<Event> events) {
+		return events.stream()
+			.map(event -> new Event(event.getEventId(), event.title()))
 			.collect(Collectors.toList());
 	}
 }
