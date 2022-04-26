1c3710abf3755da44d53622ed560e8974685ede6
ak2j38 <k2j38@naver.com>
2022-04-26 10:35:49 +0900

    feature: 카테고리 리포지토리 작업
    1. 카테고리 리포지토리 기능 구현
    
    end

--- backend/src/main/java/com/team28/sidedish/repository/CategoryRepository.java
+++ backend/src/main/java/com/team28/sidedish/repository/CategoryRepository.java
@@ -3,6 +3,9 @@ package com.team28.sidedish.repository;
 import com.team28.sidedish.repository.entity.CategoryEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends CrudRepository<CategoryEntity, Long> {
 
+    List<CategoryEntity> findAll();
 }
