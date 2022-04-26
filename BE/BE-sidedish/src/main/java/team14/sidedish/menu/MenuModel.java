5137a3abd731b79f77d3c62f34ccf8518e8f2b29
sally-ksh <sally.devz@gmail.com>
2022-04-27 08:24:25 +0900

    feat: MenuModel - getPrice(), setDiscountedPrice(), setEvent()
    - 메서드와 dto 에서 공통 이용위한 타입

--- BE/BE-sidedish/src/main/java/team14/sidedish/menu/MenuModel.java
+++ BE/BE-sidedish/src/main/java/team14/sidedish/menu/MenuModel.java
@@ -1,8 +1,17 @@
 package team14.sidedish.menu;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 /**
  * 추천메뉴, 메뉴 객체들을 MenuDto, EventPlannerDto 에서 공통으로 이용하기 위한 타입
  */
 public interface MenuModel {
 	Long getMenuId();
+
+	BigDecimal getPrice();
+
+	void setDiscountedPrice(BigDecimal discountedPrice);
+
+	void setEvent(List<String> events);
 }
