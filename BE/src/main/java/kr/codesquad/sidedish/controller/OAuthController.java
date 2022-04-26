3f4c478300cfebae5bc737a85462563c500dc52c
leezzangmin <ckdals12345678@gmail.com>
2022-04-26 12:14:26 +0900

    Feat: OAuth 기반 인증을 통한 user Email 획득

---
+++ BE/src/main/java/kr/codesquad/sidedish/controller/OAuthController.java
@@ -0,0 +1,34 @@
+package kr.codesquad.sidedish.controller;
+
+import java.io.IOException;
+
+import kr.codesquad.sidedish.dto.GitHubToken;
+import kr.codesquad.sidedish.service.OAuthService;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class OAuthController {
+
+    private final OAuthService oAuthService;
+
+    public OAuthController(OAuthService oAuthService) {
+        this.oAuthService = oAuthService;
+    }
+
+    @GetMapping("/oauth")
+    public ResponseEntity<String> requestAccessToken(@RequestParam String code, HttpServletResponse httpServletResponse) throws IOException {
+        GitHubToken token = oAuthService.getAccessToken(code);
+        String userEmail = oAuthService.getUserEmail(token);
+        httpServletResponse.setHeader("userEmail", userEmail);
+        httpServletResponse.sendRedirect("localhost:8080");
+        return new ResponseEntity<>(HttpStatus.TEMPORARY_REDIRECT);
+
+    }
+
+}
---
+++ BE/src/main/java/kr/codesquad/sidedish/service/OAuthService.java
@@ -0,0 +1,63 @@
+package kr.codesquad.sidedish.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.codesquad.sidedish.dto.GitHubToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Service
+public class OAuthService {
+
+    private final Logger log = LoggerFactory.getLogger(OAuthService.class);
+
+    private final String ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
+    private final String GET_RESOURCE_URL = "https://api.github.com/user/emails";
+
+    @Value("${github_client_id}")
+    private String clientId;
+    @Value("${github_secret}")
+    private String secret;
+
+    public GitHubToken getAccessToken(String code) {
+        Map<String, String> requestBody = generateBody(code);
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.postForObject(ACCESS_TOKEN_URL, requestBody,
+                GitHubToken.class);
+    }
+
+    public String getUserEmail(GitHubToken token) throws JsonProcessingException {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        ObjectMapper objectMapper = new ObjectMapper();
+        HttpEntity<String> request = new HttpEntity<>(headers);
+
+        headers.set("Authorization", token.getTokenHeaderString());
+        ResponseEntity<String> userResource = restTemplate.exchange(GET_RESOURCE_URL, HttpMethod.GET, request, String.class);
+        JsonNode jsonNode = objectMapper.readTree(userResource.getBody());
+        for (JsonNode node : jsonNode) {
+            if (node.get("primary").asBoolean()) {
+                return node.get("email").textValue();
+            }
+        }
+        return "";
+    }
+
+
+    private Map<String,String> generateBody(String code) {
+        return Map.of("client_id", clientId,
+                    "client_secret", secret,
+                    "code", code);
+    }
+
+}
