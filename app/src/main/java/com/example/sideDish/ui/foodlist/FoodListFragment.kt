2e360431b8bb40be9d7b92601158140c28acb0b3
shim506 <shim506@naver.com>
2022-04-27 01:09:08 +0900

    feat: FoodList 가져오기 ceh 적용

--- app/src/main/java/com/example/sideDish/ui/foodlist/FoodListFragment.kt
+++ app/src/main/java/com/example/sideDish/ui/foodlist/FoodListFragment.kt
@@ -1,9 +1,11 @@
 package com.example.sideDish.ui.foodlist
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.viewModels
@@ -15,13 +17,18 @@ import com.example.sideDish.common.EventObserver
 import com.example.sideDish.data.model.FoodCategory
 import com.example.sideDish.ui.productdetail.ProductDetailFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.supervisorScope
+import java.lang.Exception
 
 @AndroidEntryPoint
 class FoodListFragment : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
     private val viewModel by viewModels<FoodListViewModel>()
+    private lateinit var adapter: FoodListAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,23 +42,42 @@ class FoodListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val adapter = FoodListAdapter(viewModel)
+
+        adapter = FoodListAdapter(viewModel)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = adapter
+
 
         viewModel.items.observe(viewLifecycleOwner) {
             adapter.submitList(it)
         }
 
-        viewLifecycleOwner.lifecycleScope.launch() {
-            launch { adapter.updateCategoryItems(FoodCategory.MAIN) }.join()
-            launch { adapter.updateCategoryItems(FoodCategory.SOUP) }.join()
-            launch { adapter.updateCategoryItems(FoodCategory.SIDE) }.join()
-        }
-
+        showSideDishList()
         viewModel.openDetail.observe(viewLifecycleOwner, EventObserver {
             openDetail()
         })
+    }
+
+    private fun showSideDishList() {
+        val ceh = CoroutineExceptionHandler { _, exception ->
+            Toast.makeText(requireContext(), "반찬의 일부를 가져오지 못했습니다", Toast.LENGTH_SHORT).show()
+            Log.d(TAG, "$exception")
+        }
+        viewLifecycleOwner.lifecycleScope.launch(ceh) {
+            supervisorScope {
+                launch() {
+                    adapter.updateCategoryItems(FoodCategory.MAIN)
+                }.join()
+                launch() {
+                    throw Exception()
+                    adapter.updateCategoryItems(FoodCategory.SOUP)
+                }.join()
+                launch() {
+                    adapter.updateCategoryItems(FoodCategory.SIDE)
+                }.join()
+            }
+
+        }
     }
 
     private fun openDetail() {
@@ -59,5 +85,9 @@ class FoodListFragment : Fragment() {
             addToBackStack(null)
             replace(R.id.container, ProductDetailFragment())
         }
+    }
+
+    companion object {
+        const val TAG = "FoodListFragment"
     }
 }
