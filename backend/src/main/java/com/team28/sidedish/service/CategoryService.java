a0ef4df2602bfc5a6e266506578d689245ba720a
ak2j38 <k2j38@naver.com>
2022-04-26 17:10:04 +0900

    feature: 카테고리 서비스 작업
    1. Mock 카테고리 서비스 삭제
    2. CategoryService 추가
    3. Entity를 DTO로 변환하기 위한 정적 팩토리 메소드 추가
    4. 의존성 변경
    
    end

--- backend/src/main/java/com/team28/sidedish/service/CategoryService.java
+++ backend/src/main/java/com/team28/sidedish/service/CategoryService.java
@@ -1,10 +1,23 @@
 package com.team28.sidedish.service;
 
 import com.team28.sidedish.controller.dto.CategoryResponse;
+import com.team28.sidedish.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-public interface CategoryService {
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
 
-    List<CategoryResponse> findAll();
+    private final CategoryRepository categoryRepository;
+
+    public List<CategoryResponse> findAll() {
+        return categoryRepository.findAll().stream()
+                .map(categoryEntity -> CategoryResponse.of(categoryEntity.getId(), categoryEntity.getName()))
+                .collect(Collectors.toUnmodifiableList());
+    }
 }
