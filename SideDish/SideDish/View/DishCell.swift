4a8897ea324bc07190dae9602a6ad79e23815a27
Damagucci-Juice <jsr9874@gmail.com>
2022-04-26 10:55:20 +0900

    [#17] feat: 서버에서 데이터 가져와서 뷰에 그리기

--- SideDish/SideDish/View/DishCell.swift
+++ SideDish/SideDish/View/DishCell.swift
@@ -17,6 +17,38 @@ class DishCell: UICollectionViewCell {
         super.awakeFromNib()
     }
 
+    func configure(with product: Product) {
+        thumbNailImageView.image = product.image
+        titleLabel.text = product.title
+        descriptionLabel.text = product.description
+        finalPriceLabel.text = "\(product.salePrice)원"
+        normalPriceLabel.text = ""
+        if let normalPrice = product.normalPrice {
+            let attributedStyle = NSUnderlineStyle.single
+            let attributedString = NSMutableAttributedString(string: "\(normalPrice)원")
+            attributedString.addAttribute(NSAttributedString.Key.strikethroughStyle,
+                                          value: attributedStyle.rawValue,
+                                          range: NSRange(0..<attributedString.length))
+
+            normalPriceLabel.attributedText = attributedString
+        }
+        if let discountType = product.discountType {
+            badgeView.isHidden = false
+            discountType.forEach {
+                switch $0 {
+                case .event:
+                    self.eventBadgeLabel.isHidden = false
+                case .launching:
+                    self.launchingBadgeLabel.isHidden = false
+                }
+            }
+
+            if self.eventBadgeLabel.isHidden == true && self.launchingBadgeLabel.isHidden == false {
+                self.launchingBadgeLabel.leadingAnchor.constraint(equalTo: self.badgeView.leadingAnchor).isActive = true
+            }
+        }
+    }
+
     func configure(image: UIImage, title: String, description: String, finalPrice: Int, normalPrice: Int?, discountType: [DiscountType]?) {
         thumbNailImageView.image = image
         titleLabel.text = title
