7d73fd6eb3607517336bc1989c8ec8ae686ce520
Minseok-Choi <cms05041@gmail.com>
2022-04-26 10:37:18 +0900

    Feat: CORS mapping을 위한 WebConfig 추가

--- BE/src/main/java/kr/codesquad/sidedish/config/WebConfig.java
+++ BE/src/main/java/kr/codesquad/sidedish/config/WebConfig.java
@@ -1,28 +1,15 @@
 package kr.codesquad.sidedish.config;
 
-import kr.codesquad.sidedish.interceptor.OrderInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowedHeaders("*")
-            .exposedHeaders("*")
-            .allowedMethods("GET", "POST");
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new OrderInterceptor())
-            .addPathPatterns("/orders/{dishId}");
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST");
     }
 }
