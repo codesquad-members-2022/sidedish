cc81dd5e72b147d6b73623d1a16da1f82fdda3d6
Sol <hskim6521@naver.com>
2022-04-26 16:49:44 +0900

    [#21] feat: DetailViewController의 init() 메소드 구현 및 ViewModel의 onUpdate, onUpdateWithTopImages, onUpdateWithDetailImages 변수 설정해주는 메소드 추가

--- SideDish/SideDish/View/DetailViewController.swift
+++ SideDish/SideDish/View/DetailViewController.swift
@@ -2,7 +2,29 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    private let detailView = DetailView()
+    private let detailView: DetailView
+    private var viewModel: DishDetailViewModel?
+
+    init(viewModel: DishDetailViewModel) {
+        detailView = DetailView()
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+
+        setViewModel()
+    }
+
+    required init?(coder: NSCoder) {
+        detailView = DetailView()
+        self.viewModel = nil
+        super.init(coder: coder)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        viewModel?.fetchDetailData()
+        view = detailView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -13,6 +35,57 @@ class DetailViewController: UIViewController {
         navigationItem.title = "오리 주물럭_반조리"
 
         detailView.setLayout()
-        view = detailView
+    }
+
+    private func setViewModel() {
+        viewModel?.onUpdate = {
+            DispatchQueue.main.async {
+                self.detailView.titleLabel.text = self.viewModel?.title
+                self.detailView.descriptionLabel.text = self.viewModel?.description
+                self.detailView.finalPriceLabel.text = self.viewModel?.finalPrice
+                self.detailView.normalPriceLabel.text = self.viewModel?.normalPrice
+                self.detailView.pointLabel.text = self.viewModel?.point
+                self.detailView.shippingInfoLabel.text = self.viewModel?.deliveryInfo
+                self.detailView.shippingFeeLabel.text = self.viewModel?.deliveryFee
+
+                if let normalPrice = self.viewModel?.normalPrice {
+                    let attributedStyle = NSUnderlineStyle.single
+                    let attributedString = NSMutableAttributedString(string: normalPrice)
+                    attributedString.addAttribute(NSAttributedString.Key.strikethroughStyle,
+                                                  value: attributedStyle.rawValue,
+                                                  range: NSRange(0..<attributedString.length))
+                    self.detailView.normalPriceLabel.attributedText = attributedString
+                }
+            }
+        }
+
+        viewModel?.onUpdateWithTopImages = {
+            DispatchQueue.main.async {
+                self.viewModel?.topImages.forEach { image in
+                    let topImageView = UIImageView(image: image)
+                    topImageView.contentMode = .scaleAspectFit
+                    topImageView.widthAnchor.constraint(equalTo: topImageView.heightAnchor).isActive = true
+
+                    self.detailView.productImageContentView.addArrangedSubview(topImageView)
+                }
+            }
+        }
+
+        viewModel?.onUpdateWithDetailImages = {
+            DispatchQueue.main.async {
+                self.viewModel?.detailImages.forEach { image in
+                    let ratio = image.size.height / image.size.width
+                    let detailImageView = UIImageView(image: image)
+                    let newWidth = self.detailView.detailImageStackView.frame.width
+                    let newHeight = newWidth * ratio
+
+                    detailImageView.contentMode = .scaleAspectFit
+                    detailImageView.translatesAutoresizingMaskIntoConstraints = false
+                    detailImageView.heightAnchor.constraint(equalToConstant: newHeight).isActive = true
+
+                    self.detailView.detailImageStackView.addArrangedSubview(detailImageView)
+                }
+            }
+        }
     }
 }
