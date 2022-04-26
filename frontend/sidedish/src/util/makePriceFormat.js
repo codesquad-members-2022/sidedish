b8451a672583ecd90f02bbdee74d347de94291e7
jthw1005 <skl0105@naver.com>
2022-04-26 14:47:28 +0900

    Feat: 가격 표시 변경 util 함수 추가

--- frontend/sidedish/src/util/makePriceFormat.js
+++ frontend/sidedish/src/util/makePriceFormat.js
@@ -1,3 +1,3 @@
 export const makePriceFormat = (priceString) => {
-  return (Number(priceString) || "").toLocaleString();
+  return priceString ? Number(priceString).toLocaleString() + "원" : "";
 };
