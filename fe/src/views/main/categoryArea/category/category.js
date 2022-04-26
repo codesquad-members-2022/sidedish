8b7b91c578d7868d74c02ecf9fd65e1d55ce45dc
youryu0212 <youryu0212@naver.com>
2022-04-26 17:37:37 +0900

    [fe#42] feat: 카테고리 슬라이드 기능 구현

--- fe/src/views/main/categoryArea/category/category.js
+++ fe/src/views/main/categoryArea/category/category.js
@@ -1,17 +1,67 @@
+import { useState } from 'react';
 import { ProductCard } from '../../../../components/productCard/ProductCard';
 import { buttonIcons } from '../../../../helper/constants';
-import { CategoryWrapper, CategoryTitle, CategoryCardWrapper, LeftButton, RightButton } from './category.styled';
+import {
+  CategoryWrapper,
+  CategoryTitle,
+  CategoryCardWrapper,
+  LeftButton,
+  RightButton,
+  CardArea,
+  MoveArea,
+} from './category.styled';
 
 export function Category({ title, sideDishCardResponses }) {
+  const [positionState, setPositionState] = useState({
+    left: 0,
+    right: Math.min(sideDishCardResponses.length, 3),
+    diff: 0,
+  });
+  const [transitionState, setTransitionState] = useState(true);
+
+  function handleRightClick() {
+    const diff = Math.min(sideDishCardResponses.length - 1 - positionState.right, 4);
+    if (!transitionState || diff === 0) {
+      return;
+    }
+    setTransitionState(false);
+    setPositionState({ left: positionState.left + diff, right: positionState.right + diff, diff: diff });
+  }
+
+  function handleLeftClick() {
+    const diff = Math.min(positionState.left, 4);
+    if (!transitionState || diff === 0) {
+      return;
+    }
+    setTransitionState(false);
+    setPositionState({ left: positionState.left - diff, right: positionState.right - diff, diff: diff });
+  }
+
+  function handleTransitionEnd() {
+    setTransitionState(true);
+  }
+
   return (
     <CategoryWrapper>
       <CategoryTitle>{title}</CategoryTitle>
       <CategoryCardWrapper flex>
-        <LeftButton>{buttonIcons.left}</LeftButton>
-        {sideDishCardResponses.map((product, idx) => {
-          return <ProductCard product={product} size="medium" key={idx}></ProductCard>;
-        })}
-        <RightButton>{buttonIcons.right}</RightButton>
+        <LeftButton leftPosition={positionState.left} targetPosition={0} onClick={handleLeftClick}>
+          {buttonIcons.left}
+        </LeftButton>
+        <CardArea>
+          <MoveArea flex position={positionState} onTransitionEnd={handleTransitionEnd}>
+            {sideDishCardResponses.map((product, idx) => {
+              return <ProductCard product={product} size="medium" key={idx}></ProductCard>;
+            })}
+          </MoveArea>
+        </CardArea>
+        <RightButton
+          rightPosition={positionState.right}
+          targetPosition={sideDishCardResponses.length - 1}
+          onClick={handleRightClick}
+        >
+          {buttonIcons.right}
+        </RightButton>
       </CategoryCardWrapper>
     </CategoryWrapper>
   );
