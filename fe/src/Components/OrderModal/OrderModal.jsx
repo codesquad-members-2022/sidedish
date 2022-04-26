eab4b9356effe5141171cf509b01513584464414
godhyeongman <aaaa4000@naver.com>
2022-04-26 15:59:46 +0900

    feat: 모달창 닫기 버튼 구현

--- fe/src/Components/OrderModal/OrderModal.jsx
+++ fe/src/Components/OrderModal/OrderModal.jsx
@@ -1,10 +1,10 @@
+import { useState } from 'react';
 import styled, { css } from 'styled-components';
 
 import { Colors, Fonts } from '@/Constants';
 
+import { ProductInfo } from './ProductInfo';
 import { ProductThumbnail } from './ProductThumbnail';
-
-import { DiscountBadge } from '@/Components/Badge';
 
 const ModalBackGround = styled.div`
   position: fixed;
@@ -17,10 +17,19 @@ const ModalBackGround = styled.div`
   left: 0;
   top: 0;
   z-index: 100;
+
+  ${({ ModalOpen }) => {
+    if (!ModalOpen) {
+      return css`
+        display: none;
+      `;
+    }
+  }}
 `;
 
 const OrderModalWrapper = styled.div`
   display: flex;
+  position: relative;
   flex-direction: column;
   background-color: beige;
   display: flex;
@@ -33,102 +42,6 @@ const SelectedProduct = styled.section`
 
   margin: 48px;
   margin-top: 76px;
-`;
-
-const ProductInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-left: 32px;
-`;
-
-const Title = styled.span`
-  display: block;
-`;
-
-const OriginalPrice = styled.span`
-  display: block;
-  color: ${Colors.LIGHT_GREY};
-  margin-top: 16px;
-`;
-
-const ListPrice = styled.div`
-  display: block;
-  margin-top: 8px;
-`;
-
-const Price = styled.span`
-  display: inline-block;
-  margin-left: 8px;
-`;
-
-const RelatedProduct = styled.div``;
-
-const DeliveryInfo = styled.ul`
-  display: flex;
-  width: 440px;
-  margin-top: 24px;
-
-  border-top: 0.5px solid ${Colors.LIGHT_GREY};
-  border-bottom: 0.5px solid ${Colors.LIGHT_GREY};
-`;
-
-const ProductOrderSpec = styled.div`
-  width: 60px;
-  display: flex;
-  flex-direction: column;
-  color: ${Colors.LIGHT_GREY};
-
-  padding-top: 16px;
-  padding-bottom: 16px;
-  li {
-    margin-bottom: 8px;
-  }
-`;
-
-const ProductOrderInfo = styled.ul`
-  display: flex;
-  flex-direction: column;
-
-  padding-top: 16px;
-  padding-bottom: 16px;
-  li {
-    margin-bottom: 8px;
-
-    &:last-child {
-      margin-bottom: 0;
-    }
-  }
-`;
-
-const AmountButton = styled.button`
-  color: ${Colors.GREY};
-  width: 24px;
-  height: 24px;
-
-  i {
-    font-size: 20px;
-  }
-`;
-
-const TotalAmount = styled.div`
-  display: flex;
-  width: 60px;
-  justify-content: space-between;
-  align-items: center;
-  margin-left: 6px;
-`;
-
-const TotalCost = styled.span`
-  display: inline-block;
-`;
-
-const CurrentAmount = styled.span``;
-
-const OrderPrice = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 440px;
-  margin-top: 26px;
 `;
 
 const OrderButton = styled.button`
@@ -146,14 +59,28 @@ const OrderButton = styled.button`
   bottom: 20px;
 `;
 
-const Icon = styled.i`
+const RelatedProduct = styled.div``;
+
+const CloseButton = styled.button`
+  position: absolute;
   color: ${Colors.GREY};
+  top: 32px;
+  right: 48px;
+  width: 30px;
+  height: 26px;
 `;
 
 export const OrderModal = () => {
+  const [ModalOpen, setModalOpen] = useState(true);
+
+  function onClickCloseBtn() {
+    setModalOpen(false);
+  }
+
   return (
-    <ModalBackGround>
+    <ModalBackGround ModalOpen={ModalOpen}>
       <OrderModalWrapper>
+        <CloseButton onClick={onClickCloseBtn}>닫기</CloseButton>
         <SelectedProduct>
           <ProductThumbnail
             ProductThumbnailData={[
@@ -163,43 +90,7 @@ export const OrderModal = () => {
             ]}
           />
 
-          <ProductInfo>
-            <Title className={Fonts.LG}>오이피자</Title>
-            <OriginalPrice className={Fonts.SM}>15800 원</OriginalPrice>
-
-            <ListPrice>
-              <DiscountBadge />
-              <Price className={Fonts.LG}>12000 원</Price>
-            </ListPrice>
-
-            <DeliveryInfo>
-              <ProductOrderSpec>
-                <li className={Fonts.XS}>적립금</li>
-                <li className={Fonts.XS}>주소</li>
-                <li className={Fonts.XS}>배달금</li>
-              </ProductOrderSpec>
-
-              <ProductOrderInfo>
-                <li className={Fonts.XS}>180 원</li>
-                <li className={Fonts.XS}>이곳은 주소입니다</li>
-                <li className={Fonts.XS}>3000 원</li>
-              </ProductOrderInfo>
-            </DeliveryInfo>
-
-            <OrderPrice>
-              <TotalAmount>
-                <AmountButton>
-                  <Icon className={'ic-plus'} aria-label={'상품 추가'} />
-                </AmountButton>
-                <CurrentAmount>0</CurrentAmount>
-                <AmountButton>
-                  <Icon className={'ic-minus'} aria-label={'상품 '} />
-                </AmountButton>
-              </TotalAmount>
-
-              <TotalCost>최종 가격</TotalCost>
-            </OrderPrice>
-          </ProductInfo>
+          <ProductInfo />
 
           <OrderButton>주문하기</OrderButton>
         </SelectedProduct>
