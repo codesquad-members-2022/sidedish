74b6468febdf7528ae8d74331c41531c780fe9e7
bibi <bibi6666667@kakao.com>
2022-04-26 15:49:34 +0900

    feat : Badge 추가 및 메인특가 뱃지 추가 #3

--- sidedishApp/sidedishApp/model/Badge.swift
+++ sidedishApp/sidedishApp/model/Badge.swift
@@ -7,16 +7,19 @@
 
 import Foundation
 
-//enum Badge {
-//    case eventPrice
-//    case launchingPrice
-//    
-//    var description: String {
-//        switch self {
-//        case .eventPrice:
-//            return "이벤트특가"
-//        case .launchingPrice:
-//            return "런칭특가"
-//        }
-//    }
-//}
+enum Badge {
+    case eventPrice
+    case launchingPrice
+    case mainPrice
+    
+    var description: String {
+        switch self {
+        case .eventPrice:
+            return "이벤트특가"
+        case .launchingPrice:
+            return "런칭특가"
+        case .mainPrice:
+            return "메인특가"
+        }
+    }
+}
