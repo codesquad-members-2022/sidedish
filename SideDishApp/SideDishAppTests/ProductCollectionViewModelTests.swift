834cd9a88d88ee55fdbc731b0b0ede476db980da
BumgeunSong <isp1195@gmail.com>
2022-04-26 18:16:31 +0900

    [#26] Chore: Organize directory

--- SideDishApp/SideDishAppTests/ProductCollectionViewModelTests.swift
+++ SideDishApp/SideDishAppTests/ProductCollectionViewModelTests.swift
@@ -23,30 +23,4 @@ class ProductCollectionViewModelTests: XCTestCase {
 
         wait(for: [promise], timeout: 1)
     }
-
-    func testFetchImage() throws {
-        let promise = XCTestExpectation(description: "Image fetched")
-
-        // Prepare Stub
-        let fileName = "1155_ZIP_P_0081_T"
-        let fileExtension = "jpg"
-
-        guard let localImageURL = Bundle.main.url(forResource: fileName, withExtension: fileExtension),
-                      let localImageData = try? Data(contentsOf: localImageURL), let localImage = UIImage(data: localImageData) else {
-                    return XCTFail()
-      }
-
-        var components = URLComponents()
-        components.scheme = "https"
-        components.host = "public.codesquad.kr"
-        components.path = "/jk/storeapp/data/main/"
-        components.path += "\(fileName).\(fileExtension)"
-        guard let testURL = components.url else { return XCTFail() }
-
-        productCollectionViewModel.fetchImage(from: testURL) { image in
-            guard let image = image else { return }
-            XCTAssertEqual(localImage, image)
-            promise.fulfill()
-        }
-    }
 }
