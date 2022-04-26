3490d6e7e56b21a6b395797af5d9cb630bcf26ef
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-04-26 13:45:37 +0900

    [#7] Chore: MockRepository에도 RepositoryApplicable채택 후 필요한 메소드 선언 우선 추가
    - 추후 테스트 메소드 작성시 해당 mock 객체의 역할에 대한 고민 필요

--- SideDish/SideDish/Model+Repository/Mock/MockRepository.swift
+++ SideDish/SideDish/Model+Repository/Mock/MockRepository.swift
@@ -1,11 +1,16 @@
 import Foundation
 
 struct MockRepository: RepositoryApplicable{
+    
     var delegate: RepositoryDelegate?
     
     func fetchBackgroundData<T>(category: Category, dataType: T.Type) where T : Decodable, T : Encodable {
         let food = Food(detailHash: "hash", alt: "alt", foodDescription: "description", normalPrice: "normalPrice", specialPrice: "specialPrice", deliveryInformation: ["deliveryInformation"], title: "title", imageUrl: "imageUrl", badges: ["badge"])
         delegate?.fetchBackgroundData(category: .main, backgroundData: food)
+    }
+    
+    func requestData(completionHandler: @escaping (Data) -> Void, method: HttpMethod, contentType: ContentType, url: EndPoint) {
+    
     }
     
 }
