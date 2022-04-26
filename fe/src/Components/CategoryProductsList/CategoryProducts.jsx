15a88481aaf7e704026492116f79a9c4e4edbf84
Cola <bcad1591@gmail.com>
2022-04-26 16:39:37 +0900

    design: 슬라이더 레이아웃 및 스타일 추가,

--- fe/src/Components/CategoryProductsList/CategoryProducts.jsx
+++ fe/src/Components/CategoryProductsList/CategoryProducts.jsx
@@ -1,5 +1,7 @@
+import { useRef, useEffect } from 'react';
 import styled from 'styled-components';
 
+import { IconFonts, Fonts } from '@/Constants';
 import { useFetch } from '@/Hooks';
 
 import { RetryButton } from '@/Components/Button';
@@ -8,7 +10,11 @@ import { ProductCard } from '@/Components/ProductCard';
 
 const CategoryProductsWrapper = styled.li`
   width: 1280px;
-  margin-top: 56px;
+  margin-top: 80px;
+
+  &:first-child {
+    margin-top: 56px;
+  }
 
   &:last-child {
     margin-bottom: 80px;
@@ -20,9 +26,61 @@ const Header = styled.header`
 `;
 
 const ProductCardList = styled.ul`
-  display: flex;
+  display: inline-flex;
   justify-content: flex-start;
 `;
+
+/* 슬라이더 */
+const SLIDE = 'slide-container';
+const SLIDER_PREV_BUTTON = 'slider-prev-button';
+const SLIDER_NEXT_BUTTON = 'slider-next-button';
+
+const SliderWrapper = styled.div`
+  width: 100%;
+  position: relative;
+`;
+
+const Slider = styled.div`
+  width: 100%;
+  overflow: hidden;
+
+  .${SLIDE} {
+    transition: transform 250ms ease-in-out;
+    
+    > * {
+      margin-right: ${({ margin = 24 }) => margin}px;
+    }
+  }
+`;
+
+const SliderButton = styled.button`
+  position: absolute;
+  top: 50%;
+  width: 32px;
+  height: 32px;
+  transform: translateY(-50%);
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  &:active {
+    opacity: 0.4;
+  }
+
+  &.${SLIDER_PREV_BUTTON} {
+    left: -60px;
+  }
+
+  &.${SLIDER_NEXT_BUTTON} {
+    right: -60px;
+  }
+`;
+
+const Icon = styled.i`
+  font-size: 24px;
+`;
+/* ****** */
 
 const parse = categoryProductsData => {
   const title = categoryProductsData.content[0].mainCategory;
@@ -37,10 +95,16 @@ export const CategoryProducts = props => {
   const [categoryProductsData, isLoaded, isError, setRetry] = useFetch(
     `/category/${categoryId}`
   );
+  const productCardRef = useRef(null);
 
   const handleClickRetryButton = () => {
     setRetry(true);
   };
+
+  useEffect(() => {
+    if (!isLoaded) return;
+    console.log(productCardRef.current.clientWidth);
+  }, [isLoaded]);
 
   if (isError) return <RetryButton onClick={handleClickRetryButton} />;
   if (!isLoaded) return <LoadingSpinner />;
@@ -50,19 +114,28 @@ export const CategoryProducts = props => {
 
   return (
     <CategoryProductsWrapper>
-      <Header className={'fonts-xl-bold'}>
+      <Header className={Fonts.XL_BOLD}>
         {parsedCategoryProductsData.title}
       </Header>
-      <ProductCardList>
-        {parsedCategoryProductsData.content.map(categoryProductData => (
-          <ProductCard
-            size={'md'}
-            data={categoryProductData}
-            key={categoryProductData.id}
-          />
-        ))}
-      </ProductCardList>
+      <SliderWrapper>
+        <Slider margin={24}>
+          <ProductCardList className={SLIDE} ref={productCardRef}>
+            {parsedCategoryProductsData.content.map(categoryProductData => (
+              <ProductCard
+                size={'md'}
+                data={categoryProductData}
+                key={categoryProductData.id}
+              />
+            ))}
+          </ProductCardList>
+          <SliderButton className={SLIDER_PREV_BUTTON}>
+            <Icon className={IconFonts.PREV_BUTTON} />
+          </SliderButton>
+          <SliderButton className={SLIDER_NEXT_BUTTON}>
+            <Icon className={IconFonts.NEXT_BUTTON} />
+          </SliderButton>
+        </Slider>
+      </SliderWrapper>
     </CategoryProductsWrapper>
   );
 };
-
