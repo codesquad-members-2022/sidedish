c199820ec5c35f67053d203fcc979ef4f45cc000
sally-ksh <sally.devz@gmail.com>
2022-04-26 20:26:45 +0900

    feat: Event @Id, SalePolicy 와 1:N 맵핑

--- BE/BE-sidedish/src/main/java/team14/sidedish/event/Event.java
+++ BE/BE-sidedish/src/main/java/team14/sidedish/event/Event.java
@@ -1,15 +1,26 @@
 package team14.sidedish.event;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.MappedCollection;
 import org.springframework.data.relational.core.mapping.Table;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import team14.sidedish.salepolicy.SalePolicy;
 
 @Table("SIDEDISH_EVENTS")
 public class Event {
+	@Id
 	@Column("EVENT_ID")
 	private final Long eventId;
+	@Column("EVENT_TITLE")
 	private final String eventTitle;
+
+	@MappedCollection(idColumn = "EVENT_ID", keyColumn = "EVENT_ID")
+	private List<SalePolicy> salePolicies = new ArrayList<>();
 
 	protected static Event of(String eventTitle) {
 		return new Event(null, eventTitle);
@@ -20,8 +31,16 @@ public class Event {
 		this.eventTitle = eventTitle;
 	}
 
+	public Long getEventId() {
+		return eventId;
+	}
+
 	public String title() {
 		return eventTitle;
+	}
+
+	public List<SalePolicy> getSalePolicies() {
+		return salePolicies;
 	}
 
 	public boolean isEquals(Long eventId) {
