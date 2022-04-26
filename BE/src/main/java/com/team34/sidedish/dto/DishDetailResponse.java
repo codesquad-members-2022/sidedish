6ca21ff6f73629de0d5f50f83c412af4543f476c
who-hoo <who.ho3ov@gmail.com>
2022-04-27 03:39:38 +0900

    update: 반찬 상세 응답 DTO(DishDetailResponse) 수정
    - 변수명 통일(mileageRatio -> mileageRate)
    - List<String> badges -> String badge로 변경
    - AllArgsConstructor 추가

--- BE/src/main/java/com/team34/sidedish/dto/DishDetailResponse.java
+++ BE/src/main/java/com/team34/sidedish/dto/DishDetailResponse.java
@@ -7,14 +7,32 @@ public class DishDetailResponse {
     private int originPrice;
     private int discountPrice;
     private int deliveryFee;
-    private double mileageRatio;
+    private double mileageRate;
     private boolean earlyDeliverable;
     private Integer freeShippingAmount;
     private String title;
     private String content;
     private List<String> images;
-    private List<String> badges;
+    private String badge;
     private List<DishResponse> recommendedItems;
+
+    public DishDetailResponse(int originPrice, int discountPrice, int deliveryFee,
+        double mileageRate,
+        boolean earlyDeliverable, Integer freeShippingAmount, String title, String content,
+        List<String> images, String badge,
+        List<DishResponse> recommendedItems) {
+        this.originPrice = originPrice;
+        this.discountPrice = discountPrice;
+        this.deliveryFee = deliveryFee;
+        this.mileageRate = mileageRate;
+        this.earlyDeliverable = earlyDeliverable;
+        this.freeShippingAmount = freeShippingAmount;
+        this.title = title;
+        this.content = content;
+        this.images = images;
+        this.badge = badge;
+        this.recommendedItems = recommendedItems;
+    }
 
     public int getOriginPrice() {
         return originPrice;
@@ -28,8 +46,8 @@ public class DishDetailResponse {
         return deliveryFee;
     }
 
-    public double getMileageRatio() {
-        return mileageRatio;
+    public double getMileageRate() {
+        return mileageRate;
     }
 
     public boolean isEarlyDeliverable() {
@@ -52,8 +70,8 @@ public class DishDetailResponse {
         return images;
     }
 
-    public List<String> getBadges() {
-        return badges;
+    public String getBadge() {
+        return badge;
     }
 
     public List<DishResponse> getRecommendedItems() {
