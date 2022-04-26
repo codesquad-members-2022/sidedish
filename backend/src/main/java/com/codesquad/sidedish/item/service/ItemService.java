f837a706a7d9120de70138e3d3728ba74514ec2e
seyeong <seyoung7555@naver.com>
2022-04-26 16:09:18 +0900

    refactor: ItemService 동작 방식 수정 (#40)
    - 특정 Item을 불러올 때 category를 불러온 다음 item을 찾는다.
    - Transactional(readonly = true) 설정을 하였다.

--- backend/src/main/java/com/codesquad/sidedish/item/service/ItemService.java
+++ backend/src/main/java/com/codesquad/sidedish/item/service/ItemService.java
@@ -8,14 +8,15 @@ import com.codesquad.sidedish.item.dto.CategoryItemsDto;
 import com.codesquad.sidedish.item.dto.DetailItemDto;
 import com.codesquad.sidedish.item.dto.ItemDto;
 import com.codesquad.sidedish.item.exception.CategoryIdNotFoundException;
-import com.codesquad.sidedish.item.exception.ItemIdNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Transactional(readOnly = true)
 @Service
 public class ItemService {
     private final CategoryRepository categoryRepository;
@@ -39,9 +40,15 @@ public class ItemService {
     }
 
     public DetailItemDto findById(int id) {
-        return categoryRepository.findByItemId(id)
+        Category category = categoryRepository.findByItemId(id)
+                .orElseThrow(() -> new CategoryIdNotFoundException("존재하지 않는 카테고리입니다."));
+
+        return category.getItems()
+                .stream()
+                .filter(item -> item.getId() == id)
+                .findAny()
                 .map(item -> new DetailItemDto(item.getId(), item.getDiscountPolicy(), item.getDiscountRate(), item.getDescription(), item.getName(), item.getPrice(), item.getMainImageLink(), item.getItemImages()))
-                .orElseThrow(() -> new ItemIdNotFoundException("존재하지 않는 아이템입니다."));
+                .orElseThrow(() -> new RuntimeException("존재하지 않는 아이템입니다."));
     }
 
     public CategoryItemDto findByCategory(int categoryId) {
