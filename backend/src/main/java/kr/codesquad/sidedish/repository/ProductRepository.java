8a32aea62036b3962e1f617afe1053ec04878ffa
Seung-Wan Jeong <sjeong4776@gmail.com>
2022-04-28 00:09:22 +0900

    Feat: ORDER BY 조건을 패러미터로 지정할 수 있도록 SQL문 수정

--- backend/src/main/java/kr/codesquad/sidedish/repository/ProductRepository.java
+++ backend/src/main/java/kr/codesquad/sidedish/repository/ProductRepository.java
@@ -11,7 +11,8 @@ public interface ProductRepository extends CrudRepository<Product, Long> {
     @Query("SELECT DISTINCT p.* FROM product p LEFT JOIN product_theme pt on p.id = pt.product_id " +
             "WHERE IF(ISNULL(:categoryId), true, p.category_id = :categoryId) " +
             "AND IF(ISNULL(:themeId), true, pt.theme_id = :themeId) " +
-            "ORDER BY :sort LIMIT :offset, :pageSize")
+            "ORDER BY (CASE WHEN :sort = 'RAND()' THEN RAND() ELSE p.id END) " +
+            "LIMIT :offset, :pageSize")
     List<Product> findAllByCategoryIdAndThemeId(Long categoryId, Long themeId,
                                                 String sort, Long offset, Integer pageSize);
 }
