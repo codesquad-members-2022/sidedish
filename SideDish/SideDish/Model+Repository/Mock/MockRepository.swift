6ac180edfea64b931f4f0936040a1f6996dd7688
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-04-28 02:59:12 +0900

    [#9] Test: MockNetworkHandler, MockRepository 수정 및 RepositoryTest에 모델 데이터 요청하는 테스트 메소드 추가

--- SideDish/SideDish/Model+Repository/Mock/MockRepository.swift
+++ SideDish/SideDish/Model+Repository/Mock/MockRepository.swift
@@ -20,10 +20,21 @@ final class MockRepository: RepositoryApplicable{
         delegate?.fetchBackgroundData(category: .main, backgroundData: food)
     }
     
-    func requestData(method: HttpMethod, contentType: ContentType, url: EndPoint, completionHandler: @escaping (Result<Data,Error>)->Void) {
+    func requestBinaryData(method: HttpMethod, contentType: ContentType, url: EndPoint, completionHandler: @escaping (Result<Data,Error>)->Void) {
         networkHandler.request(url: url, method: method, contentType: contentType, completionHandler: completionHandler)
     }
     
+    func requestModelData<T>(method: HttpMethod, contentType: ContentType, url: EndPoint, completionHandler: @escaping (Result<T, Error>) -> Void) where T : Decodable, T : Encodable {
+        networkHandler.request(url: url, method: method, contentType: contentType) { result in
+            switch result {
+            case .success(let data):
+                guard let model = self.jsonHandler.convertJSONToObject(from: data, to: T.self) else { return }
+                completionHandler(.success(model))
+            case .failure(let error):
+                completionHandler(.failure(error))
+            }
+        }
+    }
 }
 
 extension MockRepository: NetworkHandlerDelegate{
--- SideDish/SideDishTests/RepositoryTest.swift
+++ SideDish/SideDishTests/RepositoryTest.swift
@@ -3,12 +3,12 @@ import XCTest
 
 class RepositoryTest: XCTestCase {
     
-    private var dataCache: DataCacheable!
+    private var dataCache: DataCachingManagable!
     private var networkHandler: NetworkHandlable!
     private var jsonHandler: JSONHandlable!
     private var repository: RepositoryApplicable!
     private var food: Food!
-
+    
     
     override func setUp(){
         dataCache = MockDataCachingManager()
@@ -20,9 +20,9 @@ class RepositoryTest: XCTestCase {
     }
     
     //네트워크 요청 테스트 -> 성공을 가정한 후, 데이터 캐시 처리까지 잘 되었는 지 캐싱된 데이터 개수를 확인하는 방식으로 테스트
-    func testRequestingData() throws {
+    func testRequestingBinaryData() throws {
         
-        repository.requestData(method: .get, contentType: .image, url: .mainImage(rawUrl: food.imageUrl)){ result in
+        repository.requestBinaryData(method: .get, contentType: .image, url: .mainImage(rawUrl: food.imageUrl)){ result in
             switch result{
             case .success(let data):
                 XCTAssertNotNil(data)
@@ -33,4 +33,21 @@ class RepositoryTest: XCTestCase {
         
         XCTAssertGreaterThan(dataCache.count, 0)
     }
+    
+    //네트워크 요청 테스트 -> 성공을 가정한 후, 데이터 캐시 처리까지 잘 되었는 지 캐싱된 데이터 개수를 확인하는 방식으로 테스트
+    func testRequestingModelData() throws {
+        let completionHandler: (Result<DetailResponse<FoodDetail>,Error>) -> Void = { result in
+            switch result {
+            case .success(let model):
+                XCTAssertNotNil(model)
+            case .failure(_):
+                XCTFail()
+            }
+        }
+        
+        repository.requestModelData(method: .get, contentType: .json, url: .detail(detailHash: food.detailHash), completionHandler: completionHandler)
+        
+        XCTAssertGreaterThan(dataCache.count, 0)
+  }
+                                        
 }
