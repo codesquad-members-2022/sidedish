749a9f724a9a46a888766eec4c3456fa0b14794c
ju-kkim <wndus8198@naver.com>
2022-04-27 14:46:28 +0900

    fix: card item 간격 수정

--- src/Components/main/food/List.jsx
+++ src/Components/main/food/List.jsx
@@ -4,20 +4,26 @@ import Card from './Card';
 
 const List = styled.div`
   ${F_basic}
-  width: calc(100% + 24px);
-  margin-left: -12px;
+  width: calc(100% + ${(props) => props.itemGap});
+  margin-left: calc(-${(props) => props.itemGap} / 2);
 
   > div {
     flex-shrink: 0;
-    width: calc((100% / ${(props) => props.vieewItemCount}) - 24px);
-    margin: 0 12px;
+    width: calc(
+      (100% / ${(props) => props.viewItemCount}) - ${(props) => props.itemGap}
+    );
+    margin: 0 calc(${(props) => props.itemGap} / 2);
   }
 `;
 
-export default function FoodList({ food, vieewItemCount }) {
+export default function FoodList({ food, viewItemCount, itemGap }) {
   const card = food.map((data) => (
     <Card key={data.detail_hash} cardData={data} />
   ));
 
-  return <List vieewItemCount={vieewItemCount}>{card}</List>;
+  return (
+    <List viewItemCount={viewItemCount} itemGap={itemGap}>
+      {card}
+    </List>
+  );
 }
