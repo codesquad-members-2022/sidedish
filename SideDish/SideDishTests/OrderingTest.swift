d32bd5db32d26c59f7cee0e91d4ce70cfea2229d
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-04-27 18:18:52 +0900

    [#11] Test: Ordering, Repository 클래스의 단위테스트 작성 및 주석 추가

--- SideDish/SideDishTests/OrderingTest.swift
+++ SideDish/SideDishTests/OrderingTest.swift
@@ -5,43 +5,79 @@ class OrderingTests: XCTestCase {
     
     private var ordering: Ordering!
     private var food: Food!
+    private var networkHandler: NetworkHandlable!
+    private var jsonHandler: JSONHandlable!
+    private var dataCache: DataCacheable!
     
-    override func setUp() {
-        ordering = Ordering(repository: MockRepository())
+    override func setUp(){
+        networkHandler = MockNetworkHandler()
+        jsonHandler = JSONHandler()
+        dataCache = MockDataCache()
+        let repository = MockRepository(networkHandler: networkHandler, jsonHandler: jsonHandler, dataCache: dataCache)
+        ordering = Ordering(repository: repository)
         food = Food(detailHash: "hash2", alt: "alt", foodDescription: "description", normalPrice: "normalPrice", specialPrice: "specialPrice", deliveryInformation: ["deliveryInformation"], title: "title", imageUrl: "imageUrl", badges: ["badge"])
         super.setUp()
     }
     
-    func testOrderingInitialization() {
+    //Ordering 인스턴스 초기화 테스트
+    func testOrderingInitialization(){
         XCTAssertNotNil(ordering)
         XCTAssertEqual(ordering.foodCount, 1)
     }
     
-    func testAddingFood() {
+    //addFood 메소드 테스트 - Food 인스턴스가 잘 추가되는 지 확인
+    func testAddingFood(){
         ordering.addFood(food: food, category: .main)
         XCTAssertEqual(ordering.foodCount, 2)
     }
     
-    func testSelectingFood() {
+    //selectFood 테스트 - Food 인스턴스 추가 후, 선택이 잘 되는지 확인
+    func testSelectingFood(){
         ordering.addFood(food: food, category: .main)
         XCTAssertNil(ordering.selectedMenu)
         ordering.selectFood(foodHash: "hash", category: .main)
         XCTAssertNotNil(ordering.selectedMenu)
     }
     
-    func testIncresingCount() {
-        ordering.addFood(food: food, category: .main)
-        ordering.selectFood(foodHash: "hash", category: .main)
-        ordering.increaseCount(foodHash: "hash")
-        XCTAssertEqual(ordering.sum, 5000)
-    }
-    
-    func testReturningSum() {
+    //increaseCount 테스트 - Food 인스턴스 추가 및 선택 후, orderingCount에 맞춰 주문 총액 sum 값이 잘 변경되는 지 테스트
+    func testIncresingCount(){
         ordering.addFood(food: food, category: .main)
         XCTAssertLessThan(ordering.sum, 0)
 
         ordering.selectFood(foodHash: "hash", category: .main)
         ordering.increaseCount(foodHash: "hash")
         XCTAssertGreaterThan(ordering.sum, 0)
+    }
+    
+    //requestFoodImage 테스트1 - 성공적인 응답 수신 상황을 가정한 테스트
+    func testRequstingFoodImage_success() throws{
+        let expectation = XCTestExpectation()
+
+        ordering.requesetFoodImage(imageUrl: food.imageUrl){ result in
+            switch result{
+            case .success(let data):
+                XCTAssertNotNil(data)
+                expectation.fulfill()
+            case .failure(_):
+                XCTFail()
+            }
+        }
+    }
+    
+    //requestFoodImage 테스트2 - 응답 수신 실패 상황을 가정한 테스트
+    func testRequstingFoodImage_failure() throws{
+        let expectation = XCTestExpectation()
+        let networkHandler = try XCTUnwrap(networkHandler) as? MockNetworkHandler
+        networkHandler?.isResponseFailed = true
+        
+        ordering.requesetFoodImage(imageUrl: food.imageUrl){ result in
+            switch result{
+            case .success(_):
+                XCTFail()
+            case .failure(let error):
+                XCTAssertEqual("\(error)", "Unknown Error")
+                expectation.fulfill()
+            }
+        }        
     }
 }
