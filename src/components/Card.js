e8c5745b134ac35406365e4dc3c168c0280bbfff
seungchanHan <seungchanhan00@gmail.com>
2022-04-28 03:19:37 +0900

    feat: [#17] flex 유동적으로 적용

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,20 +4,24 @@ import styled from 'styled-components';
 import Infos from './Infos';
 import { maxWidthBody } from '../css/variables';
 
-const Card = ({ cardInfo, cardNum }) => {
+const Card = ({ cardInfo, cardNum, isFlex }) => {
   const { title, image } = cardInfo;
 
   return (
-    <>
+    <StyledDiv isFlex={isFlex}>
       <Img src={image} alt={title} cardNum={cardNum} />
       <Infos cardInfo={cardInfo} />
-    </>
+    </StyledDiv>
   );
 };
 
 const Img = styled.img`
   width: ${({ cardNum }) => Math.floor(maxWidthBody.width / cardNum) - 43}px;
   height: ${({ cardNum }) => Math.floor(maxWidthBody.width / cardNum) - 43}px;
+`;
+
+const StyledDiv = styled.div`
+  display: ${({ isFlex }) => (isFlex ? 'flex' : '')};
 `;
 
 export default Card;
