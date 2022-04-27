be4f47a20b19116537453f9036412e7581789a26
ju-kkim <wndus8198@naver.com>
2022-04-27 16:20:35 +0900

    feat: 모든 카테고리 보기 버튼 이벤트 추가

--- src/Components/main/Category.jsx
+++ src/Components/main/Category.jsx
@@ -4,6 +4,7 @@ import { FOOD_DATA } from '../../Store/foodData';
 import { Inner } from '../../Assets/CommonStyle';
 import Title from '../Title';
 import SlideButton from '../SlideButton';
+import { useState } from 'react';
 
 const CategoryContainer = styled.section`
   padding-top: 56px;
@@ -22,12 +23,40 @@ const ListContainer = styled.section`
   overflow: hidden;
 `;
 
+const CategoryButton = styled.button`
+  display: block;
+  width: 203px;
+  height: 62px;
+  margin: 56px auto;
+  border: 1px solid ${({ theme }) => theme.Color.gray4};
+  font-size: ${({ theme }) => theme.FontSize.Large};
+  font-weight: ${({ theme }) => theme.FontWeight.Regular};
+  color: ${({ theme }) => theme.Color.gray1};
+`;
+
 export function Category({ info }) {
-  const CATEGORIES = info.map((category) => {
+  const [isViewAll, setView] = useState(false);
+  const [viewCategory, setViewCategory] = useState([info[0]]);
+
+  const categoryViewHandler = () => {
+    setView(true);
+    setViewCategory(info);
+  };
+
+  const CATEGORIES = viewCategory.map((category) => {
     return <CategoryItem category={category} key={category.path} />;
   });
 
-  return <CategoryContainer>{CATEGORIES}</CategoryContainer>;
+  return (
+    <CategoryContainer>
+      {CATEGORIES}
+      {!isViewAll ? (
+        <CategoryButton onClick={categoryViewHandler}>
+          모든 카테고리 보기
+        </CategoryButton>
+      ) : null}
+    </CategoryContainer>
+  );
 }
 
 function CategoryItem({ category }) {
