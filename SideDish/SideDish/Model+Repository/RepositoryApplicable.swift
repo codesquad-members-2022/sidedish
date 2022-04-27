35d588a8ddbb70a11b450301a68be998e3ec6cd0
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-04-27 18:12:54 +0900

    [#11] Refactor: NetworkHandlable, RepositoryApplicable 프로토콜 메소드의 파라미터 타입 변경
    - Data 타입을 파라미터로 받는 클로저가 아닌, Result<Data,Error> 타입을 파라미터로 받는 클로저를 사용하도록 변경

--- SideDish/SideDish/Model+Repository/RepositoryApplicable.swift
+++ SideDish/SideDish/Model+Repository/RepositoryApplicable.swift
@@ -2,6 +2,7 @@ import Foundation
 
 protocol RepositoryApplicable {
     var delegate: RepositoryDelegate? { get set }
+    
     func fetchBackgroundData<T: Codable>(category: Category, dataType: T.Type)
-    func requestData(completionHandler: @escaping (Data) -> Void, method: HttpMethod, contentType: ContentType, url: EndPoint)
+    func requestData(method: HttpMethod, contentType: ContentType, url: EndPoint, completionHandler: @escaping (Result<Data,Error>)->Void)
 }
