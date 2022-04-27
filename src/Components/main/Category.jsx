cc6445f920f602489b8c3bef50e547bf902d1b71
ju-kkim <wndus8198@naver.com>
2022-04-27 13:06:18 +0900

    feat: 카테고리 스타일 추가

--- src/Components/main/Category.jsx
+++ src/Components/main/Category.jsx
@@ -1,21 +1,50 @@
+import styled from 'styled-components';
 import FoodList from './food/List';
 import { FOOD_DATA } from '../../Store/foodData';
+import { Inner } from '../../Assets/CommonStyle';
+import Title from '../Title';
+import SlideButton from '../SlideButton';
+
+const CategoryContainer = styled.section`
+  padding-top: 56px;
+`;
+
+const CategoryArticle = styled.article`
+  margin-bottom: 79px;
+`;
+
+const SlideWrapper = styled.div`
+  position: relative;
+  margin-top: 40px;
+`;
+
+const ListContainer = styled.section`
+  overflow: hidden;
+`;
 
 export function Category({ info }) {
   const CATEGORIES = info.map((category) => {
     return <CategoryItem category={category} key={category.path} />;
   });
 
-  return <section className="category_container">{CATEGORIES}</section>;
+  return <CategoryContainer>{CATEGORIES}</CategoryContainer>;
 }
 
 function CategoryItem({ category }) {
   const Food = FOOD_DATA[category.path].body;
 
   return (
-    <article>
-      <h2>{category.title}</h2>
-      <FoodList food={Food} />
-    </article>
+    <CategoryArticle>
+      <Inner>
+        <Title title={category.title} fontSize="XLarge" fontWeight="Bold" />
+        <SlideWrapper>
+          <SlideButton direction="prev" />
+          <ListContainer>
+            <FoodList food={Food} vieewItemCount={category.viewItemCount} />
+          </ListContainer>
+          <SlideButton direction="next" />
+        </SlideWrapper>
+      </Inner>
+    </CategoryArticle>
   );
 }
