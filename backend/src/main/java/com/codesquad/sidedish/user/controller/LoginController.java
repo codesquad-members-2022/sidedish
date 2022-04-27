1cc9547b98e68768735c2bddb74ef4153daeb222
leejohy-0223 <leejohy@naver.com>
2022-04-27 14:50:15 +0900

    chore: 테스트 용 코드 제거 (#41)

--- backend/src/main/java/com/codesquad/sidedish/user/controller/LoginController.java
+++ backend/src/main/java/com/codesquad/sidedish/user/controller/LoginController.java
@@ -11,6 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.codesquad.sidedish.user.dto.LoginResponse;
 import com.codesquad.sidedish.user.service.LoginService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "login", description = "로그인 API")
 @RestController
 public class LoginController {
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
@@ -21,16 +30,14 @@ public class LoginController {
         this.loginService = loginService;
     }
 
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "JWT 토큰 정상 반환", content = @Content(schema = @Schema(implementation = LoginResponse.class))),
+        @ApiResponse(responseCode = "4xx", description = "발급 중 오류 발생", content = @Content)
+    })
+    @Operation(summary = "로그인", description = "code를 전달하여 OAuth 로그인을 진행하고, JWT 토큰을 발급받습니다.", parameters = @Parameter(name = "code", description = "GitHub 서버로부터 받은 code"))
     @GetMapping("/login/oauth")
     public LoginResponse githubLogin(@RequestParam("code") String code) {
         log.debug("code : {}", code);
         return loginService.login(code);
-    }
-
-    @GetMapping("/testapi")
-    public String testApi(HttpServletRequest request) {
-        Integer userId = (Integer)request.getAttribute("userId");
-        log.debug("userId is {}", userId);
-        return String.valueOf(userId);
     }
 }
