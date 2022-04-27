f30f9d35c14864d6cc01839c1bbfe345beffc226
KTH-96 <taehyun960@gmail.com>
2022-04-27 15:55:42 +0900

    feat: AccessToken 받아오기 기능 구현

--- be/src/main/java/sidedish/web/dto/AccessToken.java
+++ be/src/main/java/sidedish/web/dto/AccessToken.java
@@ -1,5 +1,24 @@
 package sidedish.web.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AccessToken {
 
+	private String access_token;
+	private String scope;
+	private String token_type;
+
+	@Override
+	public String toString() {
+		return "AccessToken{" +
+			"access_token='" + access_token + '\'' +
+			", scope='" + scope + '\'' +
+			", token_type='" + token_type + '\'' +
+			'}';
+	}
 }
