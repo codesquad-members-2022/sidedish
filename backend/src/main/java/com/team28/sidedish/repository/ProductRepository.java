0eee0862663be246b3bc6bcc79c9551b17b0cf78
ak2j38 <k2j38@naver.com>
2022-04-27 16:12:10 +0900

    fix: 불필요 주석 제거
    1. 사용하지 않는 주석 제거
    
    end

--- backend/src/main/java/com/team28/sidedish/repository/ProductRepository.java
+++ backend/src/main/java/com/team28/sidedish/repository/ProductRepository.java
@@ -1,15 +1,8 @@
 package com.team28.sidedish.repository;
 
 import com.team28.sidedish.repository.entity.ProductEntity;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends CrudRepository<ProductEntity, Long> {
-
-//    @Query("UPDATE PRODUCT SET STOCK_QUANTITY = :quantity WHERE ID = :productId")
-//    void updateStockQuantity(
-//            @Param("productId") Long productId,
-//            @Param("quantity") int stockQuantity);
 
 }
