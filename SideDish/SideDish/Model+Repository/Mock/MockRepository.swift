6bf68cf6881a32bda63bd8161526349a43098a2b
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-04-27 18:18:19 +0900

    [#11] Feat: MockRepository 수정, MockNetworkHandler, MockDataCache 추가
    - 단위 테스트에 사용할 Mock Obejct 생성
    - MockRepository는 Ordering을 테스트하기 위해 사용
    - MockNetworkHandler, MockDataCache는 Ordering 및 Repository를 테스트하기 위해 사용

--- SideDish/SideDish/Model+Repository/Mock/MockRepository.swift
+++ SideDish/SideDish/Model+Repository/Mock/MockRepository.swift
@@ -1,16 +1,34 @@
 import Foundation
 
-struct MockRepository: RepositoryApplicable {
+final class MockRepository: RepositoryApplicable{
     
     var delegate: RepositoryDelegate?
+    private var networkHandler: NetworkHandlable
+    private let jsonHandler: JSONHandlable
+    private let dataCache: DataCacheable
+    
+    init(networkHandler: NetworkHandlable, jsonHandler: JSONHandlable, dataCache: DataCacheable){
+        self.networkHandler = networkHandler
+        self.jsonHandler = jsonHandler
+        self.dataCache = dataCache
+        
+        self.networkHandler.delegate = self
+    }
     
     func fetchBackgroundData<T>(category: Category, dataType: T.Type) where T : Decodable, T : Encodable {
         let food = Food(detailHash: "hash", alt: "alt", foodDescription: "description", normalPrice: "normalPrice", specialPrice: "specialPrice", deliveryInformation: ["deliveryInformation"], title: "title", imageUrl: "imageUrl", badges: ["badge"])
         delegate?.fetchBackgroundData(category: .main, backgroundData: food)
     }
     
-    func requestData(completionHandler: @escaping (Data) -> Void, method: HttpMethod, contentType: ContentType, url: EndPoint) {
-    
+    func requestData(method: HttpMethod, contentType: ContentType, url: EndPoint, completionHandler: @escaping (Result<Data,Error>)->Void) {
+        networkHandler.request(url: url, method: method, contentType: contentType, completionHandler: completionHandler)
     }
     
+}
+
+extension MockRepository: NetworkHandlerDelegate{
+    
+    func cachingDataRequested(url: EndPoint, data: Data) {
+        dataCache.addCacheData(data: data, key: url.urlString)
+    }
 }
