8e7a0f8684b8c87e8dde203736e915078a799682
Seung-Wan Jeong <sjeong4776@gmail.com>
2022-04-27 14:24:14 +0900

    Feat: Product 도메인 모델 및 레포지토리 작성

---
+++ backend/src/main/java/kr/codesquad/sidedish/domain/VariantImage.java
@@ -0,0 +1,13 @@
+package kr.codesquad.sidedish.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@RequiredArgsConstructor
+@ToString
+public class VariantImage {
+
+    private final String url;
+}
