356c5672e9ae677f3599bd6a45113f60d0784b19
hsjang <hsjang0405@gmail.com>
2022-04-27 14:45:32 +0900

    [BE] feat: login 시 session으로 access token 저장

--- BE/src/main/java/sidedish/jbc/controller/OAuthController.java
+++ BE/src/main/java/sidedish/jbc/controller/OAuthController.java
@@ -1,7 +1,11 @@
 package sidedish.jbc.controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Objects;
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,28 +16,44 @@ import sidedish.jbc.domain.OauthDto;
 @Controller
 public class OAuthController {
 
+	private static final String REDIRECT_GITHUB_URL = "https://github.com/login/oauth/authorize?client_id=" + System.getenv("CLIENT_ID") + "&redirect_uri=http://localhost:8080/afterlogin";
+	private static final String GITHUB_ACCESS_TOKEN = "https://github.com/login/oauth/access_token";
+
+	@GetMapping("/login")
+	public ResponseEntity<Object> login() throws URISyntaxException {
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setLocation(new URI(REDIRECT_GITHUB_URL));
+		return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
+	}
+
 	@GetMapping("/afterlogin")
-	public ResponseEntity<String> afterLogin(@RequestParam String code, HttpServletRequest request) {
+	public ResponseEntity<String> afterLogin(@RequestParam String code, HttpSession session)
+		throws URISyntaxException {
+
 		if (Objects.isNull(code)) {
 			throw new IllegalStateException("Github 로그인이 실패했습니다.");
 		}
+
 		RestTemplate restTemplate = new RestTemplate();
-		OauthDto oauthDto = new OauthDto();
-		oauthDto.setCode(code);
-		oauthDto.setClient_id(System.getenv("CLIENT_ID"));
-		oauthDto.setClient_secret(System.getenv("SECRET_CODE"));
-		String accessToken = restTemplate.postForObject("https://github.com/login/oauth/access_token", oauthDto, String.class);
-		if (accessToken.contains("error")) {
-			//로그인 실패
+		OauthDto oauthDto = new OauthDto(code, System.getenv("CLIENT_ID"), System.getenv("SECRET_CODE"));
+		String queryParam = restTemplate.postForObject(GITHUB_ACCESS_TOKEN, oauthDto, String.class);
 
-			// 리다이렉트
-
+		if (queryParam.contains("error")) {
+			HttpHeaders httpHeaders = new HttpHeaders();
+			httpHeaders.setLocation(new URI(REDIRECT_GITHUB_URL));
+			return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
 		}
-		// 로그인 성공
+
+		String accessToken = extractAccessToken(queryParam);
+		session.setAttribute("accessToken", accessToken);
 
 		System.out.println(accessToken);
 
 		return ResponseEntity.ok().build();
+	}
+
+	private String extractAccessToken(String queryParam) {
+		return queryParam.split("&")[0].split("=")[1];
 	}
 
 }
--- BE/src/main/java/sidedish/jbc/domain/OauthDto.java
+++ BE/src/main/java/sidedish/jbc/domain/OauthDto.java
@@ -6,27 +6,21 @@ public class OauthDto {
 	private String client_id;
 	private String client_secret;
 
-	public String getCode() {
-		return code;
+	public OauthDto(String code, String client_id, String client_secret) {
+		this.code = code;
+		this.client_id = client_id;
+		this.client_secret = client_secret;
 	}
 
-	public void setCode(String code) {
-		this.code = code;
+	public String getCode() {
+		return code;
 	}
 
 	public String getClient_id() {
 		return client_id;
 	}
 
-	public void setClient_id(String client_id) {
-		this.client_id = client_id;
-	}
-
 	public String getClient_secret() {
 		return client_secret;
-	}
-
-	public void setClient_secret(String client_secret) {
-		this.client_secret = client_secret;
 	}
 }
