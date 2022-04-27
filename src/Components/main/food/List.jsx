a69f87160cd106bd99f254a02b6d5c04e99ce370
ju-kkim <wndus8198@naver.com>
2022-04-27 13:06:42 +0900

    feat: 카테고리 list 스타일 추가

--- src/Components/main/food/List.jsx
+++ src/Components/main/food/List.jsx
@@ -1,9 +1,23 @@
+import styled from 'styled-components';
+import { F_basic } from '../../../Assets/CommonStyle';
 import Card from './Card';
 
-export default function FoodList({ food }) {
+const List = styled.div`
+  ${F_basic}
+  width: calc(100% + 24px);
+  margin-left: -12px;
+
+  > div {
+    flex-shrink: 0;
+    width: calc((100% / ${(props) => props.vieewItemCount}) - 24px);
+    margin: 0 12px;
+  }
+`;
+
+export default function FoodList({ food, vieewItemCount }) {
   const card = food.map((data) => (
     <Card key={data.detail_hash} cardData={data} />
   ));
 
-  return <ul>{card}</ul>;
+  return <List vieewItemCount={vieewItemCount}>{card}</List>;
 }
