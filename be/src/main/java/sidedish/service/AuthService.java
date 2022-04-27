79649b7aec7eb4f23ec0ca2e39e8cdb0c481c36b
KTH-96 <taehyun960@gmail.com>
2022-04-27 15:55:42 +0900

    feat: AccessToken 받아오기 기능 구현

--- be/src/main/java/sidedish/service/AuthService.java
+++ be/src/main/java/sidedish/service/AuthService.java
@@ -1,8 +1,18 @@
 package sidedish.service;
 
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import sidedish.domain.member.Member;
 import sidedish.domain.member.MemberRepository;
 import sidedish.web.dto.AccessToken;
@@ -10,8 +20,12 @@ import sidedish.web.dto.ResponseDto;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class AuthService {
-
+	@Value("${auth.client_id}")
+	private String clientId;
+	@Value("${auth.client_secret}")
+	private String clientSecret;
 	private final MemberRepository memberRepository;
 
 	public ResponseDto userLogin(String code) {
@@ -23,9 +37,20 @@ public class AuthService {
 
 	private AccessToken getAccessToken(String code) {
 		//code를 가지고 body만들고 ->클라아이디, 시크릿 , 코드
+		String url = "https://github.com/login/oauth/access_token";
 		//post 요청으로 github페이지에 요청 -> https://github.com/login/oauth/access_token
+		HttpHeaders header = new HttpHeaders();
+		header.add("Accept", "application/vnd.github.v3+json");
+		Map<String, String> params = new HashMap<>();
+		params.put("client_id", clientId);
+		params.put("client_secret", clientSecret);
+		params.put("code", code);
 		//응답으로 accessToken받아서 리턴
-		return null;
+		ResponseEntity<AccessToken> response = new RestTemplate().exchange(url, HttpMethod.POST,
+			new HttpEntity<>(params, header), AccessToken.class);
+
+		log.info("log = {}", response.getBody());
+		return response.getBody();
 	}
 
 	private Member getUserInfo(AccessToken accessToken) {
