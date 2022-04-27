f06ec07ddb31385f862857f7362119f7d0bdbc17
soralee <sora2821@gmail.com>
2022-04-27 14:17:31 +0900

    [#39] Chore:  styled-components, babel-plugin-styled-components 설치

--- FE/babel.config.js
+++ FE/babel.config.js
@@ -8,10 +8,14 @@ module.exports = {
     ["@babel/preset-react", { runtime: "automatic" }],
   ],
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 9e68d79 ([#39] Chore:  styled-components, babel-plugin-styled-components 설치)
   plugins: [
     "@babel/plugin-transform-runtime",
     "babel-plugin-styled-components",
   ],
+<<<<<<< HEAD
 =======
   presets: ["@babel/preset-env", "@babel/preset-react"],
 <<<<<<< HEAD
@@ -21,4 +25,6 @@ module.exports = {
 >>>>>>> b3671d9 ([#6] Chore: babel config에 `runtime: automatic' 추가)
   plugins: ["@babel/plugin-transform-runtime"],
 >>>>>>> 6a903a6 ([#13] Chore: babel transform-runtime-plugin 설치, 설정)
+=======
+>>>>>>> 9e68d79 ([#39] Chore:  styled-components, babel-plugin-styled-components 설치)
 };
