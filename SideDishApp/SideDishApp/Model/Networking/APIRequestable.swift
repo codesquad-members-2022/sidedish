e8827da40bf1637ebbd8252f0e9514fd814330a0
BumgeunSong <isp1195@gmail.com>
2022-04-28 17:55:01 +0900

    [#26] Refactor: Movee execution logic from NetworkManager to APIRequestable (delete NetworkManager)

--- SideDishApp/SideDishApp/Model/Networking/APIRequestable.swift
+++ SideDishApp/SideDishApp/Model/Networking/APIRequestable.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Alamofire
+import UIKit
 
 protocol APIRequestable {
     associatedtype Response
@@ -24,9 +25,37 @@ extension APIRequestable {
     // Header, queryItem이 없을 경우 구현하지 않아도 됨
     var headers: [String: String] { [:] }
     var queryItems: [String: String] { [:] }
+
+    // Request 생성 로직은 모든 구체 타입에서 동일하므로 default로 구현
+    func buildRequest() -> DataRequest {
+        return AF.request(self.url,
+                          method: self.method,
+                          parameters: self.queryItems,
+                          headers: HTTPHeaders(self.headers)
+        )
+    }
+}
+
+extension APIRequestable {
+    // execute 로직은 현재 모든 구체 타입에서 동일하므로 default로 구현
+    func execute(completion: @escaping (Response?) -> Void) {
+        self.buildRequest()
+            .validate()
+            .responseData { response in
+                switch response.result {
+                case .success(let data):
+                    return completion(decode(data))
+                case .failure(let error):
+                    SystemLog.fault(error.localizedDescription)
+                    return completion(nil)
+                }
+            }
+    }
 }
 
 extension APIRequestable where Response: Decodable {
+    // Decoding 로직은 Response type이 Decodable한지, 그냥 Binary Data인지에 따라 달라짐.
+    // type constraint를 사용해서 default를 다르게 구현
     func decode(_ data: Data) -> Response? {
         do {
             return try JSONDecoder().decode(Response.self, from: data)
