d447b576771dbb162c723000d1564c20430e88a8
jinan159 <wlsdhks0423@naver.com>
2022-04-28 19:56:08 +0900

    refactor: GlobalExceptionHandler 에서 처리하는 예외를 debug 로그로 출력하도록 개선

--- backend/src/main/java/com/team28/sidedish/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/team28/sidedish/exception/GlobalExceptionHandler.java
@@ -1,16 +1,22 @@
 package com.team28.sidedish.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Arrays;
+
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = SidedishRuntimeException.class)
     public ResponseEntity<ErrorResponse> handleSidedishRuntimeException(SidedishRuntimeException e) {
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+
+        log.error(e.getMessage(), e);
 
         return new ResponseEntity<>(errorResponse, e.getStatus());
     }
@@ -19,12 +25,16 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
 
+        log.error(e.getMessage(), e);
+
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+
+        log.error(e.getMessage(), e);
 
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
