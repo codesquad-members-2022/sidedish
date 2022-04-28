2dcb49393ce2df8d047e7e59072fa3dbf349da55
ju-kkim <wndus8198@naver.com>
2022-04-28 21:40:14 +0900

    feat: 슬라이드 ref 설정 및 transition 스타일 추가

--- src/Components/main/food/List.jsx
+++ src/Components/main/food/List.jsx
@@ -6,6 +6,7 @@ const List = styled.div`
   ${F_basic}
   width: calc(100% + ${(props) => props.itemGap});
   margin-left: calc(-${(props) => props.itemGap} / 2);
+  transition: 0.3s ease-out;
 
   > div {
     flex-shrink: 0;
@@ -16,13 +17,13 @@ const List = styled.div`
   }
 `;
 
-export default function FoodList({ food, viewItemCount, itemGap }) {
+export default function FoodList({ food, viewItemCount, itemGap, slider }) {
   const card = food.map((data) => (
     <Card key={data.detail_hash} cardData={data} />
   ));
 
   return (
-    <List viewItemCount={viewItemCount} itemGap={itemGap}>
+    <List viewItemCount={viewItemCount} itemGap={itemGap} ref={slider}>
       {card}
     </List>
   );
