33f2dd06c46b465be51888764cf900c3113b3211
jm <jm.dev.study@gmail.com>
2022-04-29 01:19:05 +0900

    Fix: 에러 컴포넌트에 스타일 추가
        Co-authored-by: Muffin <Muffin9@users.noreply.github.com>

--- fe/src/common/Error.jsx
+++ fe/src/common/Error.jsx
@@ -1,5 +1,14 @@
+import styled from 'styled-components';
+
+const Error = styled.div`
+  margin: 0 auto;
+  padding: 100px 0;
+  text-align: center;
+  ${({ theme }) => theme.fontStyles.logo};
+`;
+
 const ErrorComponent = () => {
-  return <div>데이터 불러오기 실패...</div>;
+  return <Error>데이터 불러오기 실패...🥲</Error>;
 };
 
 export default ErrorComponent;
