fbbafbdc59c44453844f726fd7f432210641f018
kimdongjune <jun36817@naver.com>
2022-04-28 15:01:41 +0900

    feat - producttAPI 추가 #140

---
+++ SideDishProject/SideDishProject/repository/ProductAPI.swift
@@ -0,0 +1,31 @@
+//
+//  CategoryAPI.swift
+//  SideDishProject
+//
+//  Created by 김동준 on 2022/04/28.
+//
+
+import Foundation
+
+enum ProductAPI{
+    static let baseURL = "http://3.38.240.18:8080/"
+    
+    case categories
+    case products
+    
+    var url: URL? {
+        switch self{
+        case .categories:
+            return URL(string: Self.baseURL + "categories")
+        case .products:
+            return URL(string: Self.baseURL + "products")
+        }
+    }
+    
+    var method: String {
+        switch self{
+        case .categories, .products:
+            return "GET"
+        }
+    }
+}
