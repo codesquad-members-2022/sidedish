431d2f6f867ed4ac550045fc7b8b082180f7c6e0
sally-ksh <sally.devz@gmail.com>
2022-04-29 02:16:05 +0900

    refactor: SpecialMenuModel 글자 수정

--- BE/BE-sidedish/src/main/java/team14/sidedish/specialmenu/enums/SpecialMenuModel.java
+++ BE/BE-sidedish/src/main/java/team14/sidedish/specialmenu/enums/SpecialMenuModel.java
@@ -11,8 +11,9 @@ public enum SpecialMenuModel {
 	ONE("풍성한 고기 반찬", 1),
 	TWO("편리한 반찬 세트", 2),
 	THREE("맛있는 제철 요리", 3),
-	FOUR("우리아이 영양 반찬", 4);
+	FOUR("우리 아이 영양 반찬", 4);
 
+	public static final String ERROR_OF_SPECIAL_MENU_MODEL_PARAM = "error of specialMenuModel param";
 	private final String title;
 	private final int id;
 
@@ -20,8 +21,6 @@ public enum SpecialMenuModel {
 		return Arrays.stream(values())
 			.filter(it -> it.id == id)
 			.findAny()
-			.orElseThrow(() -> new IllegalArgumentException("error of specialMenuModel param"));
+			.orElseThrow(() -> new IllegalArgumentException(ERROR_OF_SPECIAL_MENU_MODEL_PARAM));
 	}
-
-
 }
