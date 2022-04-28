0aa51a20c84a69015e01d974b42047fb8cab3416
yongseongjeon <yongseong.jeon95@gmail.com>
2022-04-28 19:45:38 +0900

    ✨ MainCategory 에서 데이터 fetch
    - 배열 추출하는 함수 추가
    - promise.all로 데이터 fetch
    
    closed #15
    
    Co-authored-by: Dony <rsefa94@gmail.com>

--- src/components/MainCategory/MainCategory.jsx
+++ src/components/MainCategory/MainCategory.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Text from 'components/utils/Text';
 import TabBar from 'components/MainCategory/Tab/TabBar';
@@ -6,10 +6,18 @@ import Contents from 'components/MainCategory/Contents';
 import sideDishDummyData from './sideDishDummyData';
 
 const MAIN_CATEGORY_TITLE = '한 번 주문하면 두 번 반하는 반찬';
-const TAB_NAMES = ['풍성한 고기반찬', '편리한 반찬 세트', '맛있는 제철 요리', '우리 아이 영양 반찬'];
+const TAB_NAMES = ['정갈한 밑반찬', '뜨끈뜨끈 국물 요리', '든든한 메인 요리'];
+const END_POINT = 'https://api.codesquad.kr/onban/';
+const PATHs = ['main', 'soup', 'side'];
 
 export default function MainCategory() {
   const [selectedTabNum, setSelectedTabNum] = useState(0);
+  const [sideDishes, setSideDishes] = useState(sideDishDummyData);
+
+  useEffect(() => {
+    fetchCategoryData();
+  }, []);
+
   return (
     <Wrap>
       <Title>
@@ -17,9 +25,51 @@ export default function MainCategory() {
         <Text size="40px" weight="BOLD" value={MAIN_CATEGORY_TITLE} />
       </Title>
       <TabBar tabNames={TAB_NAMES} selectedTabNum={selectedTabNum} setSelectedTabNum={setSelectedTabNum} />
-      <Contents sideDishes={sideDishDummyData[selectedTabNum]} />
+      <Contents sideDishes={sideDishes[selectedTabNum] || []} />
     </Wrap>
   );
+
+  function fetchCategoryData() {
+    const URLs = PATHs.map(path => `${END_POINT}${path}`);
+    const sideDishData = [];
+
+    Promise.all(URLs.map(fetchRandomSideDish));
+
+    function fetchRandomSideDish(URL) {
+      fetch(URL)
+        .then(res => res.json())
+        .then(saveRandomSideDish)
+        .then(() => setSideDishes(sideDishData), handleError);
+    }
+
+    function saveRandomSideDish(res) {
+      const MAX_LENGTH = 3;
+      const randomSideDish = extractRandomLenInArr(res.body, MAX_LENGTH);
+      sideDishData.push(randomSideDish);
+    }
+
+    function extractRandomLenInArr(arr, length) {
+      if (length > arr.length) {
+        throw new Error('추출하는 길이보다 배열의 길이가 작습니다.');
+      }
+      const extractedSet = new Set();
+      const arrLen = arr.length;
+      while (extractedSet.size < length) {
+        const idx = getRandomNumber(0, arrLen - 1);
+        extractedSet.add(arr[idx]);
+      }
+      return Array.from(extractedSet);
+    }
+
+    function getRandomNumber(min, max) {
+      return Math.floor(min + Math.random() * (max - min + 1));
+    }
+
+    function handleError(error) {
+      setSideDishes([]);
+      throw new Error(`${error}: 데이터를 성공적으로 불러오지 못했습니다.`);
+    }
+  }
 }
 
 const Wrap = styled.div({
