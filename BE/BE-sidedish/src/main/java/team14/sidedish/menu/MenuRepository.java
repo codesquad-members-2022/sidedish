ce9cd8f5603cb88b3cc77e1c566ed74672567567
sally-ksh <sally.devz@gmail.com>
2022-04-29 02:19:20 +0900

    feat: MenuRepository PageRequest 추가

--- BE/BE-sidedish/src/main/java/team14/sidedish/menu/MenuRepository.java
+++ BE/BE-sidedish/src/main/java/team14/sidedish/menu/MenuRepository.java
@@ -1,9 +1,10 @@
 package team14.sidedish.menu;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface MenuRepository extends CrudRepository<Menu, Long> {
-	List<Menu> findByCategory(Menu.Category category);
+	List<Menu> findByCategory(Menu.Category category, PageRequest pageable);
 }
