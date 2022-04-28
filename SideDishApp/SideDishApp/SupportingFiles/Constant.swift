d68320aa705389c12a91b52b2ce0713e996f2637
P_SeoB <akdlsjtm21@naver.com>
2022-04-28 16:42:53 +0900

    [Refactor][#22] HeaderConstant 제거

--- SideDishApp/SideDishApp/SupportingFiles/Constant.swift
+++ SideDishApp/SideDishApp/SupportingFiles/Constant.swift
@@ -18,12 +18,6 @@ enum Constant {
         static let sectionHeaderView = "sectionHeaderViewIdentifier"
     }
     
-    enum SectionHeaderText {
-        static let main = "모두가 좋아하는 든든한 메인 요리"
-        static let soup = "정성이 담긴 뜨끈뜨끈 국물 요리"
-        static let side = "식탁을 풍성하게 하는 정갈한 밑반찬"
-    }
-    
     enum SubInfoText: String, CaseIterable, CustomStringConvertible {
         case point
         case deliveryInfo
