519bb719c6a5775bbd940f23e6f2bbd078244aeb
nak253 <nwon4413@naver.com>
2022-04-28 15:39:05 +0900

    feat: Spring boot Content Type ISO-8859-1 문제 해결

--- BE/src/main/java/com/team34/sidedish/config/WebMvcConfig.java
+++ BE/src/main/java/com/team34/sidedish/config/WebMvcConfig.java
@@ -1,7 +1,11 @@
 package com.team34.sidedish.config;
 
 import com.team34.sidedish.interceptor.CorsInterceptor;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
@@ -19,5 +23,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new CorsInterceptor()).addPathPatterns("/**");
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        StringHttpMessageConverter converter = new StringHttpMessageConverter(
+            StandardCharsets.UTF_8);
+        converter.setWriteAcceptCharset(false);
+        converters.add(converter);
+        WebMvcConfigurer.super.configureMessageConverters(converters);
     }
 }
