0a4f10a84531bc1bd0005867013f7b555c0ea189
함석호 <kalejuice02@gmail.com>
2022-04-28 14:28:52 +0900

    fix : cors설정에 ec2 인스턴스 주소 추가

--- be/src/main/java/sidedish/config/WebConfig.java
+++ be/src/main/java/sidedish/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000","http://ec2-3-105-209-84.ap-southeast-2.compute.amazonaws.com")
                 .allowedMethods("GET", "POST");
     }
 }
