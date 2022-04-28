d9b8cd2eea10bd62feacbc760bf895bad69dbf0e
Millie <jay.lee.2280@gmail.com>
2022-04-28 14:34:14 +0900

    Refactor: URL을 파일로 분리
    Co-authored-by: Hemudi <hemudi@users.noreply.github.com>

--- FE/src/constants/api.js
+++ FE/src/constants/api.js
@@ -1,3 +1,4 @@
-const TEST_URL = `http://3.39.42.204:8080/api/dishes`;
+const API_URL = `http://3.39.42.204:8080/api/`;
+const MOCK_URL = 'http://localhost:4000/';
 
-export { TEST_URL };
+export { API_URL, MOCK_URL };
