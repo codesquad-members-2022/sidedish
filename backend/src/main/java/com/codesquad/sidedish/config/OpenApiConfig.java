06e4f8e77081e1ec8ee50dfa8e29ede8cdf2bdae
seyeong <seyoung7555@naver.com>
2022-04-28 15:24:51 +0900

    refactor: 중복된 class를 없애고 패키지 import (#50)

--- backend/src/main/java/com/codesquad/sidedish/config/OpenApiConfig.java
+++ backend/src/main/java/com/codesquad/sidedish/config/OpenApiConfig.java
@@ -1,15 +1,14 @@
 package com.codesquad.sidedish.config;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@io.swagger.v3.oas.annotations.security.SecurityScheme(
+@SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
         bearerFormat = "JWT",
@@ -19,12 +18,9 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        SecurityScheme bearerAuth = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT");
         SecurityRequirement securityItem = new SecurityRequirement().addList("bearerAuth");
 
         return new OpenAPI()
-                .components(new Components().addSecuritySchemes("bearer-key", bearerAuth))
                 .addSecurityItem(securityItem);
     }
 }
