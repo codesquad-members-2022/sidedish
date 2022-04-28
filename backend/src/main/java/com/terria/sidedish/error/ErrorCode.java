63bb03fe03afd10beb3c44b0fdbfb40722a4bc90
mybloom <baejungeun11@gmail.com>
2022-04-28 11:00:21 +0900

    [#52]fix: NO_SUCH_SIDE_DISH_ERROR 에러 문구 수정

--- backend/src/main/java/com/terria/sidedish/error/ErrorCode.java
+++ backend/src/main/java/com/terria/sidedish/error/ErrorCode.java
@@ -6,7 +6,7 @@ public enum ErrorCode {
 
     EXHIBITION_VALIDATION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "기획전 아이디가 유효하지 않습니다."),
     NO_SUCH_MEMBER_ERROR(HttpStatus.NOT_FOUND, "해당 사용자가 존재하지 않습니다."),
-    NO_SUCH_SIDE_DISH_ERROR(HttpStatus.NOT_FOUND, "해당 사용자가 존재하지 않습니다."),
+    NO_SUCH_SIDE_DISH_ERROR(HttpStatus.NOT_FOUND, "해당 반찬이 존재하지 않습니다."),
     NOT_AVAILABLE_ORDER_FOR_NO_STOCK(HttpStatus.NOT_ACCEPTABLE, "반찬 재고 수량이 부족합니다.");
 
     final HttpStatus status;
