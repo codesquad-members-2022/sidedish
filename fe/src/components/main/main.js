eb23e883d54963007a51f1578b94ab6e331a8cef
youryu0212 <youryu0212@naver.com>
2022-04-28 16:19:16 +0900

    [fe#62] refactor: main 컴포넌트 분리

---
+++ fe/src/components/main/main.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { BestProduct } from './bestProduct/BestProduct';
+import { CategoryArea } from './categoryArea/CategoryArea';
+import { ProductDetailModal } from './productDetailModal/productDetailModal';
+
+export function Main() {
+  const [modal, setModal] = useState(null);
+  return (
+    <>
+      <BestProduct setModal={setModal} />
+      <CategoryArea setModal={setModal} />
+    </>
+  );
+}
