b2875d383047a5c7fc857d6452fef031b6aea295
godhyeongman <aaaa4000@naver.com>
2022-04-28 11:56:34 +0900

    refactor: useEffect 리팩토링 & 컨텍스 Provider arr-> obj 수정

--- fe/src/Components/OrderModal/ModalContext.jsx
+++ fe/src/Components/OrderModal/ModalContext.jsx
@@ -4,8 +4,31 @@ export const ModalContext = createContext(null);
 
 export const ModalStore = props => {
   const [ModalDisplay, setModalDisplay] = useState(false);
+  const [productID, setProductID] = useState(1);
+  const [productDetail, setProductDetail] = useState(null);
+  const [currentAmount, setCurrentAmount] = useState(1);
+
+  useEffect(() => {
+    if (!productID) {
+      return;
+    }
+
+    fetchData(`/items/${productID}`)
+      .then(productData => setProductDetail(productData))
+      .catch(err => console.error(err));
+  }, [productID]);
+
   return (
-    <ModalContext.Provider value={[ModalDisplay, setModalDisplay]}>
+    <ModalContext.Provider
+      value={{
+        ModalDisplay,
+        setModalDisplay,
+        setProductID,
+        productDetail,
+        currentAmount,
+        setCurrentAmount,
+      }}
+    >
       {props.children}
     </ModalContext.Provider>
   );
