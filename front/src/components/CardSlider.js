d37b7f9a91ff4d250f6056f33276b62d82ae4be3
ldldz <qkrqudrjs4350@gmail.com>
2022-04-28 22:11:33 +0900

    Refact: CardSlider gap 상태에서 상수로 변경

--- front/src/components/CardSlider.js
+++ front/src/components/CardSlider.js
@@ -1,11 +1,9 @@
 import styled, { useTheme } from 'styled-components';
 import Card from './Card';
-import { useState } from 'react';
 
 function CardSlider({ items, showModal, startCardIndex, NUMBER_OF_CARDS, width, cardSize }) {
   const theme = useTheme();
-  const [gap] = useState((width - NUMBER_OF_CARDS * theme.cardSizes[cardSize].width) / (NUMBER_OF_CARDS - 1));
-  // gap을 변수로 선언했을때 적용이 안되서 상태로 변경함 물어보기!
+  const gap = (width - NUMBER_OF_CARDS * theme.cardSizes[cardSize].width) / (NUMBER_OF_CARDS - 1);
   return (
     <CardSliderWrap>
       <CardListWrap width={width}>
