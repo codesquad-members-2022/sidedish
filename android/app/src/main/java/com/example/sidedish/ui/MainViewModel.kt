7a787d33ec8260b8ecd60005d9fc5fd416940add
hwj <jminie@naver.com>
2022-04-28 19:06:36 +0900

    refactor: PR 피드백 2차 반영

--- android/app/src/main/java/com/example/sidedish/ui/MainViewModel.kt
+++ android/app/src/main/java/com/example/sidedish/ui/MainViewModel.kt
@@ -1,12 +1,16 @@
 package com.example.sidedish.ui
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.sidedish.model.Products
 import com.example.sidedish.repository.MainRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlin.system.measureTimeMillis
 
 class MainViewModel(private val mainRepository: MainRepository) : ViewModel() {
 
@@ -25,14 +29,20 @@ class MainViewModel(private val mainRepository: MainRepository) : ViewModel() {
 
     private fun loadMenu() {
         viewModelScope.launch {
-            mainRepository.loadMainMenu().let {
-                _mainMenu.value = it?.products
+            CoroutineScope(Dispatchers.Main).launch {
+                mainRepository.loadMainMenu().let {
+                    _mainMenu.value = it?.products
+                }
             }
-            mainRepository.loadSoupMenu().let {
-                _soupMenu.value = it?.products
+            CoroutineScope(Dispatchers.Main).launch {
+                mainRepository.loadSoupMenu().let {
+                    _soupMenu.value = it?.products
+                }
             }
-            mainRepository.loadSideDish().let {
-                _sideDish.value = it?.products
+            CoroutineScope(Dispatchers.Main).launch {
+                mainRepository.loadSideDish().let {
+                    _sideDish.value = it?.products
+                }
             }
         }
     }
