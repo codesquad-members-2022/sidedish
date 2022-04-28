10a885588ad33581baee613c08363dce192fd854
함석호 <kalejuice02@gmail.com>
2022-04-28 15:44:58 +0900

    refactor : EventRepository에 있는 item 조회 메서드를 ItemRepository로 이동

--- be/src/main/java/sidedish/domain/item/ItemRepository.java
+++ be/src/main/java/sidedish/domain/item/ItemRepository.java
@@ -12,4 +12,8 @@ public interface ItemRepository extends CrudRepository<Item, Long> {
 
     @Query("select * from item where main_category_id = :category_id")
     List<Item> findItemsById(@Param("category_id") Long categoryId);
+
+    @Query("select i.id, i.title, i.description, i.price, i.quantity, i.discount_policy, i.morning_delivery"
+            + " from item_event AS ie join item AS i on ie.item_id = i.id WHERE ie.event_id = :event_id")
+    List<Item> findItemsByEventId(@Param("event_id") Long eventId);
 }
