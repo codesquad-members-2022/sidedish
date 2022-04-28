1b11a40262e516f87e5af850fdd94c9cb09cb015
Minseok-Choi <cms05041@gmail.com>
2022-04-28 23:31:36 +0900

    Fix: CORS 맵핑 header 빠진거 추가

--- BE/src/main/java/kr/codesquad/sidedish/config/WebConfig.java
+++ BE/src/main/java/kr/codesquad/sidedish/config/WebConfig.java
@@ -15,6 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
+            .allowedHeaders("*")
             .exposedHeaders("*")
             .allowedMethods("GET", "POST");
     }
