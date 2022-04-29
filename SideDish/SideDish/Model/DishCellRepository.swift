1867476d8dcab357ecd99b15f4ae0821e9ca6ba8
Sol <hskim6521@naver.com>
2022-04-29 15:55:35 +0900

    [#26] NetworkTarget 열거형 생성
    네트워크 통신에서 사용되는 url을 각각의 경우마다 불러서 사용할 수 있는 열거형 생성

--- SideDish/SideDish/Model/DishCellRepository.swift
+++ SideDish/SideDish/Model/DishCellRepository.swift
@@ -6,11 +6,7 @@ protocol DishCellRepositoryProtocol {
 
 class DishCellRepository: DishCellRepositoryProtocol {
     func fetchInfo(sort: ProductSort, _ completion: @escaping (Result<[DishCellInfo], NetworkError>) -> Void) {
-        var urlComponent = URLComponents()
-        urlComponent.scheme = "https"
-        urlComponent.host = "api.codesquad.kr"
-        urlComponent.path = "/onban\(sort.rawValue)"
-        guard let url = urlComponent.url else { return }
+        guard let url = NetworkTarget.loadDishes(productSort: sort).url else { return }
         URLSession.shared.dataTask(with: url) { data, _, error in
             guard let data = data, error == nil else {
                 completion(.failure(.noData))
