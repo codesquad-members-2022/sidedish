6daf8d616024846ed7f6c6fa9c554c5fc3dfd9bc
Millie <jay.lee.2280@gmail.com>
2022-04-29 12:46:00 +0900

    Refactor: 할인율, 뱃지 이름 constants 폴더로 분리
    Co-authored-by: Hemudi <hemudi@users.noreply.github.com>

--- FE/src/components/Card/Badge.jsx
+++ FE/src/components/Card/Badge.jsx
@@ -1,12 +1,8 @@
+import { badgeName } from 'constants/productInfo';
 import React from 'react';
 import { BadgeStyle } from './Badge.style';
 
 const Badge = ({ type }) => {
-  const badgeName = {
-    LAUNCHING: '런칭특가',
-    EVENT: '이벤트특가',
-  };
-
   return <BadgeStyle type={type}>{badgeName[type]}</BadgeStyle>;
 };
 
---
+++ FE/src/constants/productInfo.js
@@ -0,0 +1,12 @@
+const discountRate = {
+  NONE: 0.8,
+  LAUNCHING: 0.9,
+  EVENT: 0.5,
+};
+
+const badgeName = {
+  LAUNCHING: '런칭특가',
+  EVENT: '이벤트특가',
+};
+
+export { discountRate, badgeName };
