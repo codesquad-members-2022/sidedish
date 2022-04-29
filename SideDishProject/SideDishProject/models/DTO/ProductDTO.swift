cdcfe5060e5a39dccb8a4d6688f7a1b27dbbdd1a
kimdongjune <jun36817@naver.com>
2022-04-29 12:06:51 +0900

    feat- 네트워크 fetchall 구현 완료 #144

--- SideDishProject/SideDishProject/models/DTO/ProductDTO.swift
+++ SideDishProject/SideDishProject/models/DTO/ProductDTO.swift
@@ -4,6 +4,10 @@ struct CategoryDTO: Codable {
     let id: Int
     let name: String
     let count: Int
+    
+    func convertDTOtoEntity() -> DishCategory{
+        return DishCategory(id: id, name: name, count: count)
+    }
 }
 
 struct ProductDTO: Codable {
@@ -15,8 +19,20 @@ struct ProductDTO: Codable {
     let discountPrice: Int
     let events: [String]
     
-    enum CodingKeys: String, CodingKey {
-        case id, name, imageUrl, description, price, events
-        case discountPrice = "discount_price"
+    func convertDTOtoEntity(category: DishCategory) -> Product{
+        var productEvents: [ProductEvent] = []
+        for event in events {
+            var eventState = ProductEvent.none(disCount: 1)
+            switch event{
+            case "런칭특가":
+                eventState = ProductEvent.launchingPrice(disCount: 0.15)
+            case "이벤트특가":
+                eventState = ProductEvent.eventPrice(disCount: 0.2)
+            default :
+                eventState = ProductEvent.none(disCount: 1)
+            }
+            productEvents.append(eventState)
+        }
+        return Product(name: name, price: price, description: description, stock: 1, category: category, imageUrl: imageUrl, event: productEvents)
     }
 }
