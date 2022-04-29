72b3a3ab19ae4f00626fd9823050a9fa767b633d
jun <devjun1023@gmail.com>
2022-04-29 16:01:55 +0900

    Feat: Category 컨트롤러, 서비스 추가
    - Category 컨트롤러 서비스 추가

---
+++ BE/src/main/java/com/example/be/business/category/controller/dto/PlanningDataResponse.java
@@ -0,0 +1,74 @@
+package com.example.be.business.category.controller.dto;
+
+import com.example.be.business.dish.domain.Badge;
+import com.example.be.business.dish.domain.DeliveryPriceOption;
+import com.example.be.business.dish.domain.Dish;
+import com.example.be.business.dish.domain.DishStatus;
+
+import java.math.BigDecimal;
+
+public class PlanningDataResponse {
+
+    private Long dishId;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private String badge;
+    private String deliveryPriceOption;
+    private String thumbnail;
+    private String dishStatus;
+
+    public PlanningDataResponse(Long dishId, String name, String description, BigDecimal price, Badge badge, DeliveryPriceOption deliveryPriceOption, String thumbnail, DishStatus dishStatus) {
+        this.dishId = dishId;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.badge = badge.name();
+        this.deliveryPriceOption = deliveryPriceOption.name();
+        this.thumbnail = thumbnail;
+        this.dishStatus = dishStatus.name();
+    }
+
+    public PlanningDataResponse (Dish dish){
+        this.dishId = dish.getDishId();
+        this.name = dish.getName();
+        this.description = dish.getDescription();
+        this.price = dish.getPrice();
+        this.badge = dish.getBadge().name();
+        this.deliveryPriceOption = dish.getDeliveryPriceOption().name();
+        this.thumbnail = dish.getThumbnail();
+        this.dishStatus = dish.getDishStatus().name();
+    }
+
+    public Long getDishId() {
+        return dishId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public String getBadge() {
+        return badge;
+    }
+
+    public String getDeliveryPriceOption() {
+        return deliveryPriceOption;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public String getDishStatus() {
+        return dishStatus;
+    }
+}
---
+++ BE/src/main/java/com/example/be/business/category/service/CategoryService.java
@@ -0,0 +1,43 @@
+package com.example.be.business.category.service;
+
+import com.example.be.business.DishInformationResponse;
+import com.example.be.business.category.domain.CategoryType;
+import com.example.be.business.category.repository.CategoryRepository;
+import com.example.be.business.dish.service.CategoryType;
+import com.example.be.business.dish.service.DishInformationResponse;
+import com.example.be.business.dish.service.InMemogryDatabase;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final InMemogryDatabase inMemogryDatabase;
+
+    public CategoryService(CategoryRepository categoryRepository, InMemogryDatabase inMemogryDatabase) {
+        this.categoryRepository = categoryRepository;
+        this.inMemogryDatabase = inMemogryDatabase;
+    }
+
+    @Transactional(readOnly = true)
+    public Map<CategoryType, List<DishInformationResponse>> getDishesByCategory() {
+        if (inMemogryDatabase.planningDataisEmpty()) {
+            inMemogryDatabase.savePlanningData(categoryRepository.getDishesByCategory());
+            return inMemogryDatabase.getPlanningDataByCategory();
+        }
+        return inMemogryDatabase.getPlanningDataByCategory();
+    }
+
+    @Transactional(readOnly = true)
+    public Map<Long, List<DishInformationResponse>> getDishesBySubCategory(Long id) {
+        if (inMemogryDatabase.planningDataisEmpty()) {
+            inMemogryDatabase.savePlanningData(categoryRepository.getDishesByCategory());
+            return inMemogryDatabase.getPlanningDataBySubCategory(id);
+        }
+        return inMemogryDatabase.getPlanningDataBySubCategory(id);
+    }
+}
